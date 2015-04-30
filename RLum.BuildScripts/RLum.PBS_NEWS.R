459c6a861847f29c5ec95ab996e91073fa90aa86
tzerk <christoph.burow@gmx.net>
2015-04-30 17:18:28 +0200

    force UTF-8 encoding

--- RLum.BuildScripts/RLum.PBS_NEWS.R
+++ RLum.BuildScripts/RLum.PBS_NEWS.R
@@ -7,6 +7,6 @@
 
 require(tools)
 
-NEWS.txt <- capture.output(tools:::Rd2txt("inst/NEWS.Rd"))
+NEWS.txt <- capture.output(tools:::Rd2txt("inst/NEWS.Rd", outputEncoding = "UTF-8"))
 NEWS.txt <- gsub( "_\b","", NEWS.txt) #remove unwanted characters
 write(NEWS.txt,"NEWS")
