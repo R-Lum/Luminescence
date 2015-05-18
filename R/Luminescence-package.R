8a4552d9dcbb39467041b440df0ef51a95a2cb7a
Sebastian Kreutzer <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2015-05-18 11:01:49 +0200

    Prepare for CRAN submission

--- R/Luminescence-package.R
+++ R/Luminescence-package.R
@@ -6,7 +6,7 @@
 #' plotting of equivalent dose distributions.
 #'
 #' \tabular{ll}{ Package: \tab Luminescence\cr Type: \tab Package\cr Version:
-#' \tab 0.4.4 [upcoming]\cr Date: \tab 2015-05-XX\cr License: \tab GPL-3\cr }
+#' \tab 0.4.4\cr Date: \tab 2015-05-19\cr License: \tab GPL-3\cr }
 #'
 #' @name Luminescence-package
 #' @aliases Luminescence-package Luminescence
