67acb611bb42064704a93d78104ccb7ffda83462
Sebastian Kreutzer <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2015-09-08 19:51:41 +0200

    Update after the merging, also fix problem with NAMESPACE file during checking

--- R/plot_VolcanoPlot.R
+++ R/plot_VolcanoPlot.R
@@ -213,9 +213,3 @@ plot_VolcanoPlot <- function(
   }
 
 }
-## read example data set
-data(ExampleData.DeValues, envir = environment())
-ExampleData.DeValues <- Second2Gray(ExampleData.DeValues$BT998, c(0.0438,0.0019))
-
-## create plot straightforward
-plot_VolcanoPlot(data = ExampleData.DeValues)
