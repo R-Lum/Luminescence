ca5b703ea6b493d759e0605d0d71e693b89409be
Sebastian Kreutzer <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2015-09-19 16:08:57 +0200

    Pass the right answer ...

---
+++ R/get_rightAnswer.R
@@ -0,0 +1,17 @@
+#' Function to get the right answer
+#'
+#' This function returns the right answer
+#'
+#' @param ... you can pass an infinite number of further arguments
+#' @return Returns the right answer
+#' @section Function version: 0.1.0
+#' @author RLum.Team
+#' @examples
+#'
+#' ## ask for an arbitrary qoute
+#' get_Quote()
+#'
+#' @export
+get_rightAnswer <- function(...) {
+  return(46)
+}
