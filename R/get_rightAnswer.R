7dff0be8c1ffa9189af4ff38cf8671a7f4af147e
Sebastian Kreutzer <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2015-09-19 16:38:41 +0200

    update the right answer

--- R/get_rightAnswer.R
+++ R/get_rightAnswer.R
@@ -1,11 +1,11 @@
 #' Function to get the right answer
 #'
-#' This function returns the right answer
+#' This function returns just the right answer
 #'
 #' @param ... you can pass an infinite number of further arguments
 #' @return Returns the right answer
 #' @section Function version: 0.1.0
-#' @author NA
+#' @author inspired by R.G.
 #' @examples
 #'
 #' ## you really want to know?
