e27c241f3452e89f4dc34b3ec3cac7c2bfc8e637
Sebastian Kreutzer <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2016-01-29 17:39:48 +0100

    Add .pid to RLum objects

--- R/RLum-class.R
+++ R/RLum-class.R
@@ -15,16 +15,19 @@ NULL
 #'
 #' @slot info Object of class \code{\link{list}} for additional information on the object itself
 #'
-#' @slot .uid Object of class \code{\link{character}} containing a unique object identifier (md5 hash)
-#' calculated using the internal function \code{.create_UID()}. This id is calculated everytime and
-#' object is created.
+#' @slot .uid Object of class \code{\link{character}} for a unique object identifier. This id is
+#' usually calculated using the internal function \code{.create_UID()} if the funtion \code{\link{set_RLum}}
+#' is called.
+#'
+#' @slot .pid Object of class \code{\link{character}} for a parent id. This allows nesting RLum-objects
+#' at will. The parent id can be the uid of another object.
 #'
 #' @note \code{RLum} is a virtual class.
 #'
 #' @section Objects from the Class: A virtual Class: No objects can be created
 #' from it.
 #'
-#' @section Class version: 0.3.0
+#' @section Class version: 0.4.0
 #'
 #' @author Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne (France)
 #'
@@ -41,12 +44,15 @@ setClass("RLum",
            slots = list(
              originator = "character",
              info = "list",
-             .uid = "character"),
+             .uid = "character",
+             .pid = "character"
+             ),
            contains = "VIRTUAL",
            prototype = prototype(
              originator = NA_character_,
              info = list(),
-             .uid = NA_character_
+             .uid = NA_character_,
+             .pid = NA_character_
            )
          )
 
