3999b2de249ec42e528bbb50194e7712a8d4d8c4
Sebastian Kreutzer <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2016-01-29 12:21:09 +0100

    Fix NAMESPACE issue ...

--- R/Luminescence-package.R
+++ R/Luminescence-package.R
@@ -90,7 +90,7 @@
 #' @importFrom raster nlayers raster contour plotRGB brick
 #' @importFrom graphics abline mtext text lines par layout lines arrows axTicks axis barplot box boxplot contour curve grconvertX grconvertY hist legend persp points polygon rug segments title grid
 #' @importFrom grDevices adjustcolor axisTicks colorRampPalette gray.colors rgb topo.colors
-#' @importFrom stats approx as.formula complete.cases density dnorm glm lm median na.exclude na.omit nls nls.control pchisq pnorm quantile rnorm runif sd smooth smooth.spline spline t.test uniroot var weighted.mean
+#' @importFrom stats approx as.formula complete.cases density dnorm glm lm median na.exclude na.omit nls nls.control pchisq pnorm quantile rnorm runif sd smooth smooth.spline spline t.test uniroot var weighted.mean setNames
 #' @importFrom parallel parLapply makeCluster stopCluster
 #' @importFrom Rcpp evalCpp
 #' @useDynLib Luminescence
