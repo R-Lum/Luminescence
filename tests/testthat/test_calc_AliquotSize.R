87944b0230461f085b307e9ba9a0258464af46ed
johannes.friedrich <johannes.friedrich@uni-bayreuth.de>
2016-07-17 21:05:10 +0200

    First tests for using 'testthat'

---
+++ tests/testthat/test_calc_AliquotSize.R
@@ -0,0 +1,35 @@
+context("calc_AliquotSize")
+
+set.seed(1)
+temp <- calc_AliquotSize(grain.size = c(100,150), sample.diameter = 1, MC.iter = 100, plot = FALSE)
+
+test_that("check class and length of output", {
+  
+  expect_equal(is(temp), c("RLum.Results", "RLum"))
+  expect_equal(length(temp), 4)
+  
+})
+
+test_that("check summary output", {
+  
+  expect_is(temp, c("RLum.Results", "RLum"))
+  expect_equal(temp$args$grain.size, 125)
+  expect_equal(temp$args$sample.diameter, 1)
+  expect_equal(temp$args$packing.density, 0.65)
+  expect_true(temp$args$MC)
+  expect_equal(temp$args$grains.counted, NA)
+  expect_equal(temp$args$MC.iter, 100)
+})
+
+test_that("check MC run", {
+  
+  expect_equal(round(temp$MC$statistics$n), 100)
+  expect_equal(round(temp$MC$statistics$mean), 43)
+  expect_equal(round(temp$MC$statistics$median), 39)
+  expect_equal(round(temp$MC$statistics$sd.abs), 20)
+  expect_equal(round(temp$MC$statistics$sd.rel), 45)
+  expect_equal(round(temp$MC$statistics$se.abs), 2)
+  expect_equal(round(temp$MC$statistics$se.rel), 5)
+  expect_length(temp$MC$kde$x, 10000)
+  expect_length(temp$MC$kde$y, 10000)
+})
---
+++ tests/testthat/test_calc_CentralDose.R
@@ -0,0 +1,34 @@
+context("calc_CentralDose")
+
+data(ExampleData.DeValues, envir = environment())
+temp <- calc_CentralDose(ExampleData.DeValues$CA1, plot = FALSE)
+
+
+test_that("check class and length of output", {
+  
+  expect_equal(is(temp), c("RLum.Results", "RLum"))
+  expect_equal(length(temp), 5)
+  
+})
+
+test_that("check values from output", {
+  
+   
+  results <- get_RLum(temp)
+  
+  expect_equal(round(results$de, digits = 5), 65.70929)
+  expect_equal(round(results$de_err, digits = 6), 3.053443)
+  expect_equal(round(results$OD, digits = 5), 34.69061)
+  expect_equal(round(results$OD_err, digits = 6), 3.458774)
+  expect_equal(round(results$Lmax, digits = 5), 31.85046)
+  expect_equal(temp@data$args$log, "TRUE")
+  expect_equal(temp@data$args$sigmab, 0)
+
+})
+
+test_that("check values from output", {
+  
+  temp <- calc_CentralDose(ExampleData.DeValues$CA1, sigmab = 0.2)
+  
+  expect_equal(temp@data$args$sigmab, 0.2)
+})
