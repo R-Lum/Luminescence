20533eafe91e12c1aceafa0a1133d619ce0f5fa8
coffeemuggler <mdietze@gfz-potsdam.de>
2016-08-26 10:34:35 +0200

    Bug fixed/patched, cex is restored before function exit to allow alike plots in multicolumn figures.

--- R/plot_AbanicoPlot.R
+++ R/plot_AbanicoPlot.R
@@ -519,6 +519,7 @@ plot_AbanicoPlot <- function(
 
   ## save original plot parameters and restore them upon end or stop
   par.old.full <- par(no.readonly = TRUE)
+  cex_old <- par()$cex
 
   ## this ensures par() is respected for several plots on one page
   if(sum(par()$mfrow) == 2 & sum(par()$mfcol) == 2){
@@ -3658,6 +3659,9 @@ plot_AbanicoPlot <- function(
     print(IAP)
   }
 
+  ## restore initial cex
+  par(cex = cex_old)
+  
   ## create and return numeric output
   if(output == TRUE) {
     return(plot.output)
