249d61a346f9fed15e9ca461141559b77f0d966c
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2016-11-14 19:33:02 +0100

    Remove

--- RLum.BuildResults/RLumModel.Rcheck/00_pkg_src/RLumModel/tests/testthat/test-set_pars.R
+++
@@ -1,9 +0,0 @@
-context("set_pars")
-
-test_that("check length of output",{
-  expect_equal(length(.set_pars("Bailey2001")), 12)
-  expect_equal(length(.set_pars("Bailey2002")), 12)
-  expect_equal(length(.set_pars("Bailey2004")), 12)
-  expect_equal(length(.set_pars("Pagonis2007")), 12)
-  expect_equal(length(.set_pars("Pagonis2008")), 12)
-})
