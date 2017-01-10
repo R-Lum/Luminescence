1fc65a8a34e6fd52be0f17bcaa7a7a528175420a
tzerk <christoph.burow@gmx.net>
2017-01-10 14:52:27 +0100

    RLum.PBS: greatly simplify arguments list
    When we move the compilation of available functions and their arguments after building and installing the package we can get the list of arguments much easier directly from R. We hence do not need to parse the .Rd files anymore. @RLumSK
    
    [skip ci]

--- RLum.BuildScripts/RLum.PBS_Function_Arguments.R
+++ RLum.BuildScripts/RLum.PBS_Function_Arguments.R
@@ -1,86 +1,26 @@
 ### ===============================================================================================
 ### R package Luminescence BUILDSCRIPTS
 ### Function_Arguments
-### sebastian.kreutzer@u-bordeaux-montaigne.fr
-### 2015-04-29
-### Note: Previous version based on code of Michal Dietze
+### christoph.burow@uni-koeln.de
+### 2017-01-10
+### Note: Previous versions were written by Michal Dietze and Sebastian Kreutzer.
+### This version programmatically fetches all functions and arguments and does
+### not rely on parsing the .Rd files with regular expressions.
 ### ===============================================================================================
-library(tools)
+library(Luminescence)
+temp.version <- packageVersion("Luminescence")
 
+args <- sapply(lsf.str("package:Luminescence"), formalArgs)
+M <- matrix(NA, ncol = length(args), nrow = max(sapply(args, length)))
 
-##get version number
-temp <- readLines("DESCRIPTION")
-temp <- temp[grep("Version", temp)]
-temp.version <- sub(" ","",unlist(strsplit(temp,":"))[2])
+for (i in 1:ncol(M)) 
+  M[1:length(args[[i]]), i] <- args[[i]]
 
-## list files in rd-directory
-rd.files <- list.files("man/")
-
-## remove file extensions
-functions <- unlist(strsplit(x = rd.files, split = ".Rd"))
-
-## remove non-functions and unwanted stuff
-functions <- functions[!grepl(x = functions, pattern = "-class")]
-functions <-
-  functions[!grepl(x = functions, pattern = "ExampleData")]
-functions <- functions[!functions == "BaseDataSet.CosmicDoseRate"]
-functions <- functions[!functions == "Luminescence-package"]
-
-## create output matrix
-M <- matrix(nrow = 100, ncol = length(functions))
-colnames(M) <- functions
-
-## parse rd-files for argument names
-for (i in 1:ncol(M)) {
-  rd.file.i <- readLines(con = paste0("man/",functions[i], ".Rd"))
-
-  line.ID.i <- seq(from = 1, to = length(rd.file.i))
-
-  arguments.start <- line.ID.i[rd.file.i == "\\arguments{"]
-
-  arguments.end <- line.ID.i[which(rd.file.i == "}")]
-  arguments.end <- arguments.end[which(arguments.end > arguments.start)][1]
-
-  rd.arguments.i <- rd.file.i[arguments.start:arguments.end]
-
-  arguments.i <- rd.arguments.i[grepl(x = rd.arguments.i,
-                                      pattern = "\\item",
-                                      fixed = TRUE)]
-
-  arguments.i <- sub(
-    x = arguments.i,
-    pattern = "\\item{",
-    replacement = "",
-    fixed = TRUE
-  )
-
-  arguments.i <- strsplit(x = arguments.i,
-                          split = "}{",
-                          fixed = TRUE)
-
-  arguments.j <- character(length = length(arguments.i))
-
-  for (j in 1:length(arguments.i)) {
-    arguments.j[j] <- arguments.i[[j]][1]
-  }
-
-  arguments.i <- sub(
-    x = arguments.j,
-    pattern = "\\",
-    replacement = "",
-    fixed = TRUE
-  )
-
-
-  M[1:length(arguments.i),i] <- arguments.i
-
-}
-
-n.max <- max(100 - colSums(apply(X = M, MARGIN = 2, FUN = is.na)))
-
-M <- M[1:n.max,]
+colnames(M) <- names(args)
 
 write.table(x = M,
             row.names = FALSE,
             file = paste0("RLum.BuildResults/Luminescence_",temp.version,"-Function_Arguments.csv"),
             sep = ",")
+
+
