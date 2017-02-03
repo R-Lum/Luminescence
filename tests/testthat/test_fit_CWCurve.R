3233552a8d98b0ee5ed10299dbde734a520e3d6c
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2017-02-03 11:16:51 +0100

    correct typo leading to wrong results

--- tests/testthat/test_fit_CWCurve.R
+++ tests/testthat/test_fit_CWCurve.R
@@ -4,7 +4,8 @@ data(ExampleData.CW_OSL_Curve, envir = environment())
 fit <- fit_CWCurve(values = ExampleData.CW_OSL_Curve,
                    main = "CW Curve Fit",
                    n.components.max = 4,
-                   log = "x")
+                   log = "x",
+                   plot = FALSE)
 
 test_that("check class and length of output", {
 
@@ -17,5 +18,7 @@ test_that("check values from output example", {
 
   expect_equal(fit$data$n.components, 3)
   expect_equal(round(fit$data$I01, digits = 3), 2387.617)
+  expect_equal(round(fit$data$lambda1, digits = 5), 4.59054)
+  expect_equal(round(fit$data$`pseudo-R^2`, digits = 4), 0.9995)
 
 })
