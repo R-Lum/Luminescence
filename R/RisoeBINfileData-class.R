1afb7b5932d8243d7afaa4848799e4ebae28ef7a
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2017-02-04 14:14:31 +0100

    Minor adjustment for the Risoe.BINfileData-class

--- R/RisoeBINfileData-class.R
+++ R/RisoeBINfileData-class.R
@@ -143,7 +143,7 @@ NULL
 #' @section Objects from the Class: Objects can be created by calls of the form
 #' \code{new("Risoe.BINfileData", ...)}.
 #'
-#' @section Function version: 0.3.0
+#' @section Function version: 0.3.1
 #'
 #' @author Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne
 #' (France)
@@ -180,41 +180,48 @@ setClass("Risoe.BINfileData",
 setMethod("show", signature(object = "Risoe.BINfileData"),
           function(object){
 
-            version<-paste(unique(object@METADATA[,"VERSION"]), collapse = ", ")
-            systemID<-paste(unique(object@METADATA[,"SYSTEMID"]), collapse = ", ")
-            filename <- as.character(object@METADATA[1,"FNAME"])
-            records.overall<-length(object@DATA)
-            records.type<-table(object@METADATA[,"LTYPE"])
-            user<-paste(unique(as.character(object@METADATA[,"USER"])), collapse = ", ")
-            date<-paste(unique(as.character(object@METADATA[,"DATE"])), collapse = ", ")
-            run.range<-range(object@METADATA[,"RUN"])
-            set.range<-range(object@METADATA[,"SET"])
-            grain.range <- range(object@METADATA[,"GRAIN"])
-            pos.range<-range(object@METADATA[,"POSITION"])
+            if(length(object@METADATA) != 0){
+              version<-paste(unique(object@METADATA[,"VERSION"]), collapse = ", ")
+              systemID<-paste(unique(object@METADATA[,"SYSTEMID"]), collapse = ", ")
+              filename <- as.character(object@METADATA[1,"FNAME"])
+              records.overall<-length(object@DATA)
+              records.type<-table(object@METADATA[,"LTYPE"])
+              user<-paste(unique(as.character(object@METADATA[,"USER"])), collapse = ", ")
+              date<-paste(unique(as.character(object@METADATA[,"DATE"])), collapse = ", ")
+              run.range<-range(object@METADATA[,"RUN"])
+              set.range<-range(object@METADATA[,"SET"])
+              grain.range <- range(object@METADATA[,"GRAIN"])
+              pos.range<-range(object@METADATA[,"POSITION"])
 
-            records.type.count <- sapply(1:length(records.type),
-              function(x){paste(
-              names(records.type)[x],"\t(n = ",records.type[x],")",sep="")
-              })
+              records.type.count <- sapply(1:length(records.type),
+                function(x){paste(
+                names(records.type)[x],"\t(n = ",records.type[x],")",sep="")
+                })
 
-            records.type.count <- paste(records.type.count,
-                                        collapse="\n\t                      ")
+              records.type.count <- paste(records.type.count,
+                                          collapse="\n\t                      ")
 
-            ##print
-            cat("\n[Risoe.BINfileData object]")
-            cat("\n\n\tBIN/BINX version     ", version)
-            if(version>=6){
-              cat("\n\tFile name:           ", filename)
-            }
-            cat("\n\tObject date:         ", date)
-            cat("\n\tUser:                ", user)
-            cat("\n\tSystem ID:           ", ifelse(systemID == 0,"0 (unknown)", systemID))
-            cat("\n\tOverall records:     ", records.overall)
-            cat("\n\tRecords type:        ", records.type.count)
-            cat("\n\tPosition range:      ",pos.range[1],":",pos.range[2])
-            cat("\n\tGrain range:         ",grain.range[1],":",grain.range[2])
-            cat("\n\tRun range:           ",run.range[1],":",run.range[2])
-            cat("\n\tSet range:           ",set.range[1],":",set.range[2])
+              ##print
+              cat("\n[Risoe.BINfileData object]")
+              cat("\n\n\tBIN/BINX version     ", version)
+              if(version>=6){
+                cat("\n\tFile name:           ", filename)
+              }
+              cat("\n\tObject date:         ", date)
+              cat("\n\tUser:                ", user)
+              cat("\n\tSystem ID:           ", ifelse(systemID == 0,"0 (unknown)", systemID))
+              cat("\n\tOverall records:     ", records.overall)
+              cat("\n\tRecords type:        ", records.type.count)
+              cat("\n\tPosition range:      ",pos.range[1],":",pos.range[2])
+              cat("\n\tGrain range:         ",grain.range[1],":",grain.range[2])
+              cat("\n\tRun range:           ",run.range[1],":",run.range[2])
+              cat("\n\tSet range:           ",set.range[1],":",set.range[2])
+
+            }else{
+              cat("\n[Risoe.BINfileData object]")
+              cat("\n\n >> This object is empty!<<")
+
+             }
           }#end function
           )#end setMethod
 
