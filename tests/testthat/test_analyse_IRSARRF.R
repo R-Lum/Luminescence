d602812c6f87524f4f86dd8e061cee43815e1576
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2017-02-04 23:38:21 +0100

    enhance unit test for analyse_IRSAR.RF()

--- tests/testthat/test_analyse_IRSARRF.R
+++ tests/testthat/test_analyse_IRSARRF.R
@@ -1,10 +1,21 @@
 context("analyse_IRSAR.RF")
 
-data(ExampleData.RLum.Analysis, envir = environment())
-results_fit <- analyse_IRSAR.RF(object = IRSAR.RF.Data, plot = FALSE, method = "FIT")
-results_slide <- analyse_IRSAR.RF(object = IRSAR.RF.Data, plot = FALSE, method = "SLIDE", n.MC = NULL)
-
 test_that("check class and length of output", {
+
+  set.seed(1)
+  data(ExampleData.RLum.Analysis, envir = environment())
+  results_fit <- analyse_IRSAR.RF(object = IRSAR.RF.Data, plot = FALSE, method = "FIT")
+  results_slide <- analyse_IRSAR.RF(object = IRSAR.RF.Data, plot = FALSE, method = "SLIDE", n.MC = NULL)
+  results_slide_alt <-
+    analyse_IRSAR.RF(
+      object = IRSAR.RF.Data,
+      plot = FALSE,
+      method = "SLIDE",
+      n.MC = 10,
+      method.control = list(vslide_range = 'auto'),
+      txtProgressBar = FALSE
+    )
+
 
   expect_equal(is(results_fit), c("RLum.Results", "RLum"))
   expect_equal(length(results_fit), 5)
@@ -13,12 +24,11 @@ test_that("check class and length of output", {
   expect_is(results_slide$fit, class = "nls", info = NULL, label = NULL)
   expect_length(results_slide$slide, 10)
 
-})
-
-test_that("check values from output example", {
 
   expect_equal(results_fit$data$DE, 623.25)
   expect_equal(results_fit$data$DE.LOWER, 600.63)
   expect_equal(results_slide$data$DE, 610.17)
+  expect_equal(round(results_slide_alt$data$DE, digits = 0), 384)
+
 
 })
