17df7902261183cd23d55fabd5fffac0ff337774
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2017-02-12 22:24:11 +0100

    Add test for analyse_SARTL()

---
+++ tests/testthat/test_analyse_SARTL.R
@@ -0,0 +1,24 @@
+context("analyse_SAR.TL")
+
+##Full check
+test_that("Test examples", {
+  skip_on_cran()
+
+
+  ##load data
+  data(ExampleData.BINfileData, envir = environment())
+
+  ##transform the values from the first position in a RLum.Analysis object
+  object <- Risoe.BINfileData2RLum.Analysis(TL.SAR.Data, pos=3)
+
+  ##perform analysis
+  ##TODO ... there is a warning
+  expect_is(analyse_SAR.TL(object,
+                 signal.integral.min = 210,
+                 signal.integral.max = 220,
+                 log = "y",
+                 fit.method = "EXP OR LIN",
+                 sequence.structure = c("SIGNAL", "BACKGROUND")), "RLum.Results")
+
+})
+
