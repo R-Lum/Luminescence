ad165c1158b6df3f882f0c6e207abd57fad9c77e
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2017-02-12 21:21:23 +0100

    Extent test for RisoeBINfileData-class

--- tests/testthat/test_RisoeBINfileData-class.R
+++ tests/testthat/test_RisoeBINfileData-class.R
@@ -6,6 +6,10 @@ test_that("Check the example and the numerical values", {
   temp <-
     set_Risoe.BINfileData(METADATA = data.frame(), DATA = list(), .RESERVED = list())
 
+  ##get function and check whether we get NULL
+  expect_null(get_Risoe.BINfileData(temp))
+
+
   ##check object
   expect_is(temp, class = "Risoe.BINfileData")
 
