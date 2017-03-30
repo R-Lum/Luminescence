02b70b7005a6091249b199381b8088e997cbb668
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2017-03-30 23:24:42 +0200

    CRAN submission build

--- RLum.BuildScripts/RLum.PBS_ReverseDependencies.R
+++ RLum.BuildScripts/RLum.PBS_ReverseDependencies.R
@@ -6,6 +6,7 @@
 ### ===============================================================================================
 ## This script should be run manually after the package has been built
 
+
 devtools::revdep_check()
 devtools::revdep_check_save_summary()
 devtools::revdep_check_print_problems()
