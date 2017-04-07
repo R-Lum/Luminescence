8fda8040d71c0dbbadeefbe8035a27da5c57e51f
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2017-04-07 12:26:49 +0200

    Rebuild after cherry picking + NEWS + fix testthat for calc_AverageDose()

--- tests/testthat/test_calc_AverageDose.R
+++ tests/testthat/test_calc_AverageDose.R
@@ -17,7 +17,7 @@ test_that("check summary output", {
   testthat::skip_on_cran()
   results <- get_RLum(temp)
 
-  expect_equal(round(results$Average_DOSE, digits = 4), 65.3597)
+  expect_equal(round(results$AVERAGE_DOSE, digits = 4), 65.3597)
   expect_equal(round(results$SIGMA_D, digits = 4), 0.3092)
   expect_equal(round(results$L_MAX, digits = 5), -19.25096)
 })
