f266ab9763954ec304bcefa58182612853c2e464
Christoph Burow <christoph.burow@gmx.net>
2017-06-22 09:01:45 +0200

    more detail work in calc_funs

--- R/calc_HomogeneityTest.R
+++ R/calc_HomogeneityTest.R
@@ -4,27 +4,37 @@
 #'
 #' For details see Galbraith (2003).
 #'
-#' @param data [RLum.Results-class] or [data.frame]
-#' (**required**): for [data.frame]: two columns with De
-#' \code{(data[,1])} and De error \code{(values[,2])}
-#' @param log [logical] *(with default)*: peform the homogeniety test
-#' with (un-)logged data
+#' @param data [RLum.Results-class] or [data.frame] (**required**): 
+#' for [data.frame]: two columns with De `(data[,1])` and De error `(values[,2])`
+#' 
+#' @param log [logical] *(with default)*: 
+#' peform the homogeneity test with (un-)logged data
+#' 
 #' @param ... further arguments (for internal compatibility only).
-#' @return Returns a terminal output. In addition an
-#' [RLum.Results-class] object is returned containing the
-#' following element:
+#' 
+#' @return 
+#' Returns a terminal output. In addition an
+#' [RLum.Results-class]-object is returned containing the
+#' following elements:
 #'
 #' \item{summary}{[data.frame] summary of all relevant model results.}
-#' \item{data}{[data.frame] original input data} \item{args}{[list]
-#' used arguments} \item{call}{[call] the function call}
+#' \item{data}{[data.frame] original input data} 
+#' \item{args}{[list] used arguments} 
+#' \item{call}{[call] the function call}
 #'
-#' The output should be accessed using the function
-#' [get_RLum]
+#' The output should be accessed using the function [get_RLum]
+#' 
 #' @section Function version: 0.2
+#' 
 #' @author Christoph Burow, University of Cologne (Germany)
+#' 
 #' @seealso [pchisq]
-#' @references Galbraith, R.F., 2003. A simple homogeneity test for estimates
+#' 
+#' @references 
+#' Galbraith, R.F., 2003. A simple homogeneity test for estimates
 #' of dose obtained using OSL. Ancient TL 21, 75-77.
+#' 
+#' 
 #' @examples
 #'
 #' ## load example data
