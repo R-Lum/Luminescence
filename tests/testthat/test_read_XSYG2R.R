c77204182aae8f423d252fecb1cd554174b9b8dc
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2017-07-12 15:48:34 +0200

    Empty test for read_XSYG2R()

---
+++ tests/testthat/test_read_XSYG2R.R
@@ -0,0 +1,11 @@
+context("read_XSYG2R")
+
+test_that("test import of XSYG files", {
+  testthat::skip_on_cran()
+
+  ##error actions
+  ##ex read_XSYG2R("https://raw.githubusercontent.com/R-Lum/rxylib/master/inst/extdata/TLSpectrum.xsyg", fastForward = TRUE)
+
+
+
+})
