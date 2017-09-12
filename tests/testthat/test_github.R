6531645ab6d8b68e1939ebd8b0945a94991fa53f
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2017-09-12 11:52:38 +0200

    Add test for github interface

---
+++ tests/testthat/test_github.R
@@ -0,0 +1,22 @@
+context("GitHub Interface")
+
+test_that("Check github_commits()", {
+  testthat::skip_on_cran()
+
+  expect_is(github_commits(), 'data.frame')
+
+})
+
+test_that("Check github_branches()", {
+  testthat::skip_on_cran()
+
+  expect_is(github_branches(), 'data.frame')
+
+})
+
+test_that("Check github_issues()", {
+  testthat::skip_on_cran()
+
+  expect_output(github_issues())
+
+})
