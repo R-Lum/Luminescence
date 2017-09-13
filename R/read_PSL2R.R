1aa3942046cff53e52403cf81ea3fcbf35d9052d
Christoph Burow <christoph.burow@gmx.net>
2017-09-13 11:11:12 +0200

    Include PSL example file #56
    + update example in read_PSL2R

--- R/read_PSL2R.R
+++ R/read_PSL2R.R
@@ -53,11 +53,10 @@
 #' 
 #' # (1) Import PSL file to R
 #' 
-#' \dontrun{
-#' FILE <- file.choose()
-#' temp <- read_PSL2R(FILE)
-#' temp
-#' }
+#' file <- system.file("extdata", "DorNie_0016.psl", package = "Luminescence")
+#' psl <- read_PSL2R(file, drop_bg = FALSE, as_decay_curve = TRUE, smooth = TRUE, merge = FALSE)
+#' print(str(psl, max.level = 3))
+#' plot(psl, combine = TRUE)
 #' 
 #' @md
 #' @export
