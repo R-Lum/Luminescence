a259db1618f348c4333da9cb3f7c41d81076084b
Christoph Burow <christoph.burow@gmx.net>
2017-10-18 14:45:57 +0200

    surfexp: more testthat cases #59

--- tests/testthat/test_fit_SurfaceExposure.R
+++ tests/testthat/test_fit_SurfaceExposure.R
@@ -23,6 +23,17 @@ test_that("check values from output example", {
   expect_equal(round(fit$summary$age_error), 369)
 })
 
+# Sub-test - weighted fitting
+fit <- fit_SurfaceExposure(data = d1, sigmaphi = 5e-10, mu = 0.9, weights = TRUE,
+                           plot = FALSE, verbose = FALSE)
+
+
+test_that("check values from output example", {
+  testthat::skip_on_cran()
+  expect_equal(round(fit$summary$age), 9624)
+  expect_equal(round(fit$summary$age_error), 273)
+})
+
 
 ## Example data 2
 fit <- fit_SurfaceExposure(data = d2, age = 1e4, sigmaphi = 5e-10, Ddot = 2.5, D0 = 40,
@@ -89,6 +100,20 @@ test_that("not enough parameters provided", {
   )
   expect_equal(
     is(fit_SurfaceExposure(data = d2, plot = FALSE, verbose = FALSE)$fit),
-    "simpleError")
+    "simpleError"
+  )
+  expect_warning(
+    fit_SurfaceExposure(data = d4, age = c(1e2, 1e3, 1e4, 1e5, 1e6), sigmaphi = 5e-10,
+                        Ddot = 1.0, D0 = 40, weights = TRUE,
+                        plot = FALSE, verbose = TRUE),
+    "is not supported when"
+  )
+  expect_error(
+    fit_SurfaceExposure(data = d4, age = 1e4, sigmaphi = 5e-10,
+                        Ddot = 1.0, D0 = 40,
+                        plot = FALSE, verbose = FALSE),
+    "'age' must be of the same length"
+  )
+  
 })
 
