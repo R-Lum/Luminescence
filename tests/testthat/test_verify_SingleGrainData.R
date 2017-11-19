078640a1e96d4cd70710546f75b247373e83f59e
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2017-11-19 20:24:47 +0100

    Change unit tests for verify_SingleGrainData()

--- tests/testthat/test_verify_SingleGrainData.R
+++ tests/testthat/test_verify_SingleGrainData.R
@@ -4,11 +4,21 @@ test_that("Various function test", {
   testthat::skip_on_cran()
 
   data(ExampleData.XSYG, envir = environment())
-  output <- verify_SingleGrainData(OSL.SARMeasurement$Sequence.Object)
+  object <- get_RLum(
+    OSL.SARMeasurement$Sequence.Object, recordType = "OSL (UVVIS)", drop = FALSE)
+
+  ##initial
+  output <- verify_SingleGrainData(object)
 
   ##return value
   expect_is(output, "RLum.Results")
   expect_is(output$selection_full, "data.frame")
+
+  ##check options
+  expect_silent(suppressWarnings(verify_SingleGrainData(object, plot = TRUE)))
+  expect_silent(suppressWarnings(verify_SingleGrainData(object, threshold = 100)))
+  expect_silent(suppressWarnings(verify_SingleGrainData(object, verbose = FALSE)))
+  expect_silent(suppressWarnings(verify_SingleGrainData(object, cleanup = TRUE)))
 
 })
 
