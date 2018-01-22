f9070388a0f4c26193d2e1fc7bf7a82ce998963f
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2018-01-22 15:03:05 +0100

    Add another test to complete RLum-class

--- tests/testthat/test_replicate_RLum.R
+++ tests/testthat/test_replicate_RLum.R
@@ -5,6 +5,7 @@ test_that("Test replication of RLum-objects", {
 
   data(ExampleData.RLum.Analysis, envir = environment())
   expect_silent(results <- rep(IRSAR.RF.Data[[1]], 5))
+  expect_silent(rep(IRSAR.RF.Data[[1]]))
 
   ##check
   expect_equal(length(results),5)
