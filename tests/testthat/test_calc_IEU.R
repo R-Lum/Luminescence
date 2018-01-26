b4eb70dfb3907761deba6020609bfd3d73a16793
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2018-01-26 09:38:23 +0100

    Complete tests for calc_IEU()

--- tests/testthat/test_calc_IEU.R
+++ tests/testthat/test_calc_IEU.R
@@ -29,6 +29,15 @@ test_that("Test general behaviour", {
     verbose = TRUE, plot = TRUE
   ))
 
+  ##verbose without setting
+  expect_message(calc_IEU(
+    ExampleData.DeValues$CA1,
+    a = 0.2,
+    b = 1.9,
+    interval = 1,
+    plot = FALSE
+  ))
+
   ##cause stop
   expect_error(calc_IEU(
     "ExampleData.DeValues$CA1",
