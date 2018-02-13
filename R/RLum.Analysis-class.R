R-Lum/Luminescence

commit 31d1c209f0c7f0a492fb63666dc06725d34f717c

Fix an uncontrolled function break if get_RLum() was called with a subset expression which was not of class logical.

R/RLum.Analysis-class.R
@@ -25,7 +25,7 @@ NULL
 #' @section Objects from the Class:
 #' Objects can be created by calls of the form `set_RLum("RLum.Analysis", ...)`.
 #'
-#' @section Class version: 0.4.12
+#' @section Class version: 0.4.13
 #'
 #' @author
 #' Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne (France)
@@ -375,9 +375,13 @@ setMethod("get_RLum",
 
             if (!is.null(substitute(subset))) {
 
+              ##check for a logical expression, to avoid problems afterwards
+              if(class(eval(substitute(subset))) != "logical")
+                stop("[get_RLum] The argument 'subset' does not contain a logical expression!", call. = FALSE)
+
               # To account for different lengths and elements in the @info slot we first
-              # check all unique elements
-              info_el <- unique(unlist(sapply(object@records, function(el) names(el@info))))
+              # check all unique elements (in all records)
+              info_el <- unique(unlist(lapply(object@records, function(el) names(el@info))))
 
               envir <- as.data.frame(do.call(rbind,
                                              lapply(object@records, function(el) {
@@ -387,9 +391,10 @@ setMethod("get_RLum",
                                                if (any(!info_el %in% names(val))) {
                                                  val_new <- setNames(rep(NA, length(info_el[!info_el %in% names(val)])), info_el[!info_el %in% names(val)])
                                                  val <- c(val, val_new)
+
                                                }
 
-                                               # order the named char vector by its names so we dont mix up the columns
+                                               # order the named char vector by its names so we don't mix up the columns
                                                val <- val[order(names(val))]
                                                return(val)
                                                })
@@ -423,7 +428,6 @@ setMethod("get_RLum",
             else if(!is.null(info.object)) {
 
               if(info.object %in% names(object@info)){
-
                 unlist(object@info[info.object])
 
               }else{

man/RLum.Analysis-class.Rd
@@ -183,7 +183,7 @@ Objects can be created by calls of the form \code{set_RLum("RLum.Analysis", ...)
 }
 
 \section{Class version}{
- 0.4.12
+ 0.4.13
 }
 
 \examples{

commit d7363b2f5f901f59367e3625d90e622ebc12187a

rlum.analysis: fix subset method
Commit 31d1c209f0c7f0a492fb63666dc06725d34f717c broke the whole subsetting functionality of RLum.Analysis objects by evaluating the subset expression in the current environment rather than within the environment of the data.frame that is created only after the check; this always causes an error.

This fix first creates the data.frame and then checks if the provided expression is logical.. within the proper environment.

@RLumSK: Unfortunately, the bug was introduced shortly prior to the release of 0.8.0. Given the severity I would argue that 0.8.1 should be released rather sooner than later.

R/RLum.Analysis-class.R
@@ -374,31 +374,32 @@ setMethod("get_RLum",
                    protocol = "UNKNOWN", get.index = NULL, drop = TRUE, recursive = TRUE, info.object = NULL, subset = NULL, env = parent.frame(2)) {
 
             if (!is.null(substitute(subset))) {
-
-              ##check for a logical expression, to avoid problems afterwards
-              if(class(eval(substitute(subset))) != "logical")
-                stop("[get_RLum] The argument 'subset' does not contain a logical expression!", call. = FALSE)
-
+              
               # To account for different lengths and elements in the @info slot we first
               # check all unique elements (in all records)
               info_el <- unique(unlist(lapply(object@records, function(el) names(el@info))))
-
+              
               envir <- as.data.frame(do.call(rbind,
                                              lapply(object@records, function(el) {
                                                val <- c(curveType = el@curveType, recordType = el@recordType, unlist(el@info))
-
+                                               
                                                # add missing info elements and set NA
                                                if (any(!info_el %in% names(val))) {
                                                  val_new <- setNames(rep(NA, length(info_el[!info_el %in% names(val)])), info_el[!info_el %in% names(val)])
                                                  val <- c(val, val_new)
-
+                                                 
                                                }
-
+                                               
                                                # order the named char vector by its names so we don't mix up the columns
                                                val <- val[order(names(val))]
                                                return(val)
-                                               })
-                                             ), stringsAsFactors = FALSE)
+                                             })
+              ), stringsAsFactors = FALSE)
+              
+              ##check for a logical expression, to avoid problems afterwards
+              print(class(eval(substitute(subset), envir = envir, enclos = env)))
+              if(class(eval(substitute(subset), envir = envir, enclos = env)) != "logical")
+                stop("[get_RLum] The argument 'subset' does not contain a logical expression!", call. = FALSE)
 
               ##select relevant rows
               sel <- tryCatch(eval(