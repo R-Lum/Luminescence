9e418434a56cc681ca2c9458647b99c1fc8306b7
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2018-02-14 14:15:28 +0100

    Complete tests for RLum.Data.Curve()

--- tests/testthat/test_RLum.Data.Curve.R
+++ tests/testthat/test_RLum.Data.Curve.R
@@ -12,4 +12,14 @@ test_that("check class", {
   object <- set_RLum(class = "RLum.Data.Curve")
   expect_s4_class(set_RLum(class = "RLum.Data.Curve", data = object), class = "RLum.Data.Curve")
 
+  ##check get_RLum
+  object <- set_RLum(class = "RLum.Data.Curve", data = object, info = list(a = "test"))
+  expect_warning(get_RLum(object, info.object = "est"), regexp = "Invalid info.object name")
+
+  ##test names
+  expect_type(names(object), "character")
+
+  ##test bin
+  expect_warning(bin_RLum.Data(object, bin_size = -2), "Argument 'bin_size' invald, nothing was done!")
+
 })
