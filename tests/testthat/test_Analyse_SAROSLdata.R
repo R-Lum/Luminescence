3bb29db4d488b8e4f3998c223e082c4ffbc65255
Christoph Burow <christoph.burow@gmx.net>
2018-02-28 12:28:48 +0100

    test: Analyse_SAR.OSLdata()
    Mainly the plotting code was totally untested.

--- tests/testthat/test_Analyse_SAROSLdata.R
+++ tests/testthat/test_Analyse_SAROSLdata.R
@@ -8,10 +8,34 @@ test_that("full example test", {
                                 background.integral = c(900:1000),
                                 position = c(1:1),
                                 output.plot = FALSE)
-
+  
+  
   ##checks
   expect_is(output, "list")
   expect_length(output, 3)
-
-
+  
+  ## errors
+  expect_error({ Analyse_SAR.OSLdata() }, 
+               regexp = "No input data given")
+  expect_error({ Analyse_SAR.OSLdata(input.data = CWOSL.SAR.Data) }, 
+               regexp = "No signal integral is given")
+  expect_error({ Analyse_SAR.OSLdata(input.data = CWOSL.SAR.Data, signal.integral = 1:3) }, 
+               regexp = "No background integral is given")
+  expect_error({ Analyse_SAR.OSLdata(input.data = subset(CWOSL.SAR.Data, LTYPE == "IRSL"), 
+                                     signal.integral = 1:3,
+                                     background.integral = 200:250) }, 
+               regexp = "No 'OSL' curves found")
+  
+  ## should work
+  expect_is({ Analyse_SAR.OSLdata(input.data = CWOSL.SAR.Data, signal.integral = 1:3,
+                                  background.integral = 200:250, position = 1,
+                                  background.count.distribution = "non-poisson",
+                                  sigmab = 0.1, output.plot = TRUE) }, "list")
+  expect_is({
+    tmp <- subset(CWOSL.SAR.Data, LTYPE == "OSL" & POSITION == 1 & ID <= 457)
+    Analyse_SAR.OSLdata(tmp, 1:3, 200:250, output.plot = TRUE, output.plot.single = TRUE)
+  }, "list")
+  
+  
+  
 })
