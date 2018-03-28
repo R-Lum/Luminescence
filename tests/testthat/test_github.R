297f41c778acaaec8107c997f537923555a72af5
Christoph Burow <christoph.burow@gmx.net>
2018-03-28 14:16:29 +0200

    test: catch 403 github response
    Unauthenticated requests to the GiHub APIv3 are limited to 60 requests per hour
    (associated with the originating request). Exceeding the rate limit results in a
    403 Forbidden reply. Since CIs make multiple requests when testing the rate limit
    is easily reached. We check whether we either get a valid response, or at least
    a 403 response.

--- tests/testthat/test_github.R
+++ tests/testthat/test_github.R
@@ -1,25 +1,50 @@
 context("GitHub Interface")
 
+## NOTE:
+# Unauthenticated requests to the GiHub APIv3 are limited to 60 requests per hour
+# (associated with the originating request). Exceeding the rate limit results in a
+# 403 Forbidden reply. Since CIs make multiple requests when testing the rate limit
+# is easily reached. We check whether we either get a valid response, or at least
+# a 403 response.
+
 test_that("Check github_commits()", {
   testthat::skip_on_cran()
   testthat::skip_on_os("mac")
 
-  expect_is(github_commits(), 'data.frame')
+  response <- tryCatch(github_commits(), error = function(e) return(e))
+  
+  if (inherits(response, "error"))
+    expect_output(print(response), regexp = "status code 403")
+  else
+    expect_is(response, "data.frame")
 
+  rm(response)
 })
 
 test_that("Check github_branches()", {
   testthat::skip_on_cran()
   testthat::skip_on_os("mac")
 
-  expect_is(github_branches(), 'data.frame')
+  response <- tryCatch(github_branches(), error = function(e) return(e))
+  
+  if (inherits(response, "error"))
+    expect_output(print(response), regexp = "status code 403")
+  else
+    expect_is(response, "data.frame")
 
+  rm(response)
 })
 
 test_that("Check github_issues()", {
   testthat::skip_on_cran()
   testthat::skip_on_os("mac")
 
-  expect_output(github_issues())
+  response <- tryCatch(github_issues(), error = function(e) return(e))
+  
+  if (inherits(response, "error"))
+    expect_output(print(response), regexp = "status code 403")
+  else
+    expect_is(response, "list")
 
+  rm(response)
 })
