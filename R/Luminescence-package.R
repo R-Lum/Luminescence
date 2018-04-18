81ec922862a9a9fee314a9450b0b9872d94a2ba6
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2018-04-18 10:12:38 +0200

    Prepare next CRAN submission

--- R/Luminescence-package.R
+++ R/Luminescence-package.R
@@ -9,7 +9,7 @@
 #' **Package:** \tab Luminescence \cr
 #' **Type:** \tab Package \cr
 #' **Version:** \tab 0.8.3 \cr
-#' **Date:** \tab 2018-03-11 \cr
+#' **Date:** \tab 2018-04-20 \cr
 #' **License:** \tab GPL-3 \cr
 #' }
 #'
