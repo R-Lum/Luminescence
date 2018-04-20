c5f0295f21e2a49ae816404b404f7197f2f902f3
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2018-04-20 09:17:51 +0200

    Fix test for app_RLum() @tzerk. The test could not succeed if 'RLumShiny' was missing, but crashed. Apart from it, all tests should have the line 'skip_on_cran()', we do not want to overuse the CRAN resources.

--- tests/testthat/test_app_RLum.R
+++ tests/testthat/test_app_RLum.R
@@ -1,3 +1,14 @@
 context("app_RLum")
 
-expect_message(app_RLum(app = "abc"), regexp = "Invalid app name")
+test_that("Simply check app frame", {
+  skip_on_cran()
+
+  if("RLumShiny" %in% installed.packages()){
+    expect_message(app_RLum(app = "abc"), regexp = "Invalid app name")
+
+  }else{
+    expect_error(app_RLum(app = "abc"), regexp = "Shiny applications require")
+
+  }
+
+})
