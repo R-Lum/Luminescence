6f7ab7c7024ba1568ce1f4ec5ef453439958b07b
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2018-07-22 12:46:27 +0200

    Update again ps file

--- RLum.CIscripts/AppVeyor_Script.ps1
+++ RLum.CIscripts/AppVeyor_Script.ps1
@@ -13,13 +13,14 @@ Function InstallJAGS {
   Param()
 
 if ((Test-Path "C:\projects\JAGS-4.3.0.exe")) {
-    Progress "Running JAGS installer"
-    & 'C:\projects\JAGS-4.3.0.exe'
+    Progress "JAGS found in cache, running JAGS installer ..."
+    Start-Process -FilePath C:\projects\JAGS-4.3.0.exe
 
   } Else {
-    Progress "JAGS not yet available, trying to download  ..."
+    Progress "JAGS not available, attempting download ..."
     Start-FileDownload 'https://sourceforge.net/projects/mcmc-jags/files/JAGS/4.x/Windows/JAGS-4.3.0.exe' -FileName 'C:\projects\JAGS-4.3.0.exe'
-    & 'C:\projects\JAGS-4.3.0.exe'
+    Progress "Running JAGS installer ... "
+    Start-Process -FilePath C:\projects\JAGS-4.3.0.exe
   }
 
 }
