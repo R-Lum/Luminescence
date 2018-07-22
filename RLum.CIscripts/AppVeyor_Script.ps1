05a2ec045f679b8bad35d1472fc8f3a3d47c476d
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2018-07-22 12:40:53 +0200

    Update PS script

--- RLum.CIscripts/AppVeyor_Script.ps1
+++ RLum.CIscripts/AppVeyor_Script.ps1
@@ -1,17 +1,25 @@
-##Additional AppVeyor script to overcome problems while installing JAGS on the VM
-
-Function InstallJags {
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## Title:   AppVeyor powershell script to overcome problems while installing JAGS on the VM
+## Author:  Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne (France)
+## Contact: sebastian.kreutzer@u-bordeaux-montaigne.fr
+## Date:    Sun Jul 22 12:34:53 2018
+## Info:    The successful download of JAGS from the remote server very fragile and is often enough
+##          pure luck. If the download fails, the build fails. This script provides a function
+##          that first tries to use JAGS from the internal cache before attempting to dowload
+##          JAGS from the remove server.
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+Function InstallJAGS {
   [CmdletBinding()]
   Param()
 
 if ((Test-Path "C:\projects\JAGS-4.3.0.exe")) {
     Progress "Running JAGS installer"
-    Start-Process -FilePath 'C:\projects\JAGS-4.3.0.exe'
+    & 'C:\projects\JAGS-4.3.0.exe'
 
   } Else {
     Progress "JAGS not yet available, trying to download  ..."
     Start-FileDownload 'https://sourceforge.net/projects/mcmc-jags/files/JAGS/4.x/Windows/JAGS-4.3.0.exe' -FileName 'C:\projects\JAGS-4.3.0.exe'
-    Start-Process -FilePath 'C:\projects\JAGS-4.3.0.exe'
+    & 'C:\projects\JAGS-4.3.0.exe'
   }
 
 }
