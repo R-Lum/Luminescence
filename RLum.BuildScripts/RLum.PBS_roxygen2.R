5ab7c0b97222069f42e62d665d35c2cc860f59c5
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2018-11-25 16:45:11 +0100

    Refit RLum.PBS_roxygen2 build script for the newest roxygen2 package version.

--- RLum.BuildScripts/RLum.PBS_roxygen2.R
+++ RLum.BuildScripts/RLum.PBS_roxygen2.R
@@ -2,11 +2,19 @@
 ### R package Luminescence BUILDSCRIPTS
 ### roxygen2
 ### sebastian.kreutzer@u-bordeaux-montaigne.fr
-### 2015-09-30
+### 2018-11-25
 ### ===============================================================================================
+##updated using the suggestions in
+##https://github.com/klutometis/roxygen/issues/822
 
+
+# Load packages -------------------------------------------------------------------------------
 if(!require("devtools"))
   install.packages("devtools")
 
-library(devtools)
-document(pkg = ".", roclets = NULL)
+if(!require("pkgbuild"))
+  install.packages("pkgbuild")
+
+# Create documentation  -----------------------------------------------------------------------
+pkgbuild::compile_dll()
+devtools::document()
