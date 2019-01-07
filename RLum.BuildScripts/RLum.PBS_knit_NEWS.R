5ee1c1a2f49372c831c7e0b802d5e70e536bdf9b
Christoph Burow <christoph.burow@gmx.net>
2019-01-07 17:00:54 +0100

    Add comment to new NEWS script
    ..to prevent future troubles on windows computers.

--- RLum.BuildScripts/RLum.PBS_knit_NEWS.R
+++ RLum.BuildScripts/RLum.PBS_knit_NEWS.R
@@ -5,6 +5,13 @@
 ### 2019-01-02
 ### ===============================================================================================
 
+# IMPORTANT NOTE ------------------------------------------------------------------------------
+# On WINDOWS machines make sure to include pandoc in the PATH environment. 
+# Pandoc is usually not (pre-)installed, which is why RStudio uses its bundled
+# version found in 'C:\Program Files\RStudio\bin\pandoc'. Running this build 
+# script from CMD will not find the pandoc.exe unless it's been added to the
+# search path.
+
 # Knit NEWS -----------------------------------------------------------------------------------
 rmarkdown::render("NEWS.Rmd", output_format = "github_document")
 
