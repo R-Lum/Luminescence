7e6027cea388adb88e0cf8ce5d90a6cc94e9f05c
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2019-06-04 18:17:28 -0400

    Update tests for apply_EfficiencyCorrection()

--- tests/testthat/test_apply_EfficiencyCorrection.R
+++ tests/testthat/test_apply_EfficiencyCorrection.R
@@ -6,8 +6,32 @@ test_that("check function", {
   ##load data
   data(ExampleData.XSYG, envir = environment())
 
+  ##create efficiency data
+  eff_data <- data.frame(WAVELENGTH = 1:1000, runif(1000))
+
+  ##break function
+  expect_error(apply_EfficiencyCorrection(object = "ERROR"),
+               regexp = "Input object is not of type RLum.Data.Spectrum")
+
+  expect_error(apply_EfficiencyCorrection(object = TL.Spectrum, spectral.efficiency = "ERROR"),
+               regexp = "'spectral.efficiency' is not of type data.frame")
+
+  eff_data_false <- eff_data
+  eff_data_false[1,2] <- 2
+  expect_error(apply_EfficiencyCorrection(
+    object = TL.Spectrum,
+    spectral.efficiency = eff_data_false),
+               regexp = "Relative quantum efficiency values > 1 are not allowed.")
+
+
   ##run tests
-  expect_silent(apply_EfficiencyCorrection(TL.Spectrum,spectral.efficiency = as.data.frame(TL.Spectrum[,1:2])))
+  expect_s4_class(apply_EfficiencyCorrection(TL.Spectrum,spectral.efficiency = eff_data), "RLum.Data.Spectrum")
+
+  ##run loop test
+  expect_warning(
+    apply_EfficiencyCorrection(list(a = "test", TL.Spectrum), spectral.efficiency = eff_data),
+    regexp = "Skipping character object in input list.")
+
 
 })
 
