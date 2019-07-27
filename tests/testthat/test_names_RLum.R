1ac22b952133369ec825c2211e3f3cb3b4d30287
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2019-07-27 13:45:57 +0200

    Enhance test for names_RLum()

--- tests/testthat/test_names_RLum.R
+++ tests/testthat/test_names_RLum.R
@@ -7,4 +7,7 @@ test_that("Test whether function works", {
   expect_silent(names_RLum(IRSAR.RF.Data))
   expect_is(names_RLum(IRSAR.RF.Data), "character")
 
+  ##test a list of such elements
+  expect_is(names_RLum(list(IRSAR.RF.Data,IRSAR.RF.Data, "a")), "character")
+
 })
