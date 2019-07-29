acfe887cd52a78627286260fcc4149a44cad9fdd
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2019-07-29 10:15:11 +0200

    Full rebuild

--- R/RLum.Results-class.R
+++ R/RLum.Results-class.R
@@ -23,7 +23,7 @@ NULL
 #' @section Class version: 0.5.2
 #'
 #' @author
-#' Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne (France)
+#' Sebastian Kreutzer, IRAMAT-CRP2A, UMR 5060, CNRS - Université Bordeaux Montaigne (France)
 #'
 #' @seealso [RLum-class], [plot_RLum], [merge_RLum]
 #'
