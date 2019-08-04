cfa5b0e7b52755e8e844d6b2d30eef1f838a75cb
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2019-08-04 11:19:04 +0200

    Rebuild with codemeta

--- CHECK_AND_BUILD_macOS_Linux.sh
+++ CHECK_AND_BUILD_macOS_Linux.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 R -q -e "RLumBuild::build_package(
-  exclude = c('module_check_ReverseDependencies'),
+  exclude = c(
+  'module_check_ReverseDependencies',
+  'module_write_codemetar'
+  ),
   as_cran = TRUE,
   write_Rbuildignore = TRUE
 )"
