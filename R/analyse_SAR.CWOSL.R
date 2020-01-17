7c58be39d3dafb8902946dfe5267994e432344e3
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2020-01-17 12:40:10 +0000

    Remove test code from analyse_SAR.CWOSL()

--- R/analyse_SAR.CWOSL.R
+++ R/analyse_SAR.CWOSL.R
@@ -1793,30 +1793,3 @@ if(is.list(object)){
   }
 
 }
-
-##load data
-##ExampleData.BINfileData contains two BINfileData objects
-##CWOSL.SAR.Data and TL.SAR.Data
-data(ExampleData.BINfileData, envir = environment())
-
-##transform the values from the first position in a RLum.Analysis object
-object <- Risoe.BINfileData2RLum.Analysis(CWOSL.SAR.Data, pos=1)
-
-##perform SAR analysis and set rejection criteria
-results <- analyse_SAR.CWOSL(
-  object = object,
-  signal.integral.min = 1,
-  signal.integral.max = 2,
-  background.integral.min = 900,
-  background.integral.max = 1000,
-  log = "x",
-  main = "test",
-  plot_onePage = TRUE,
-  fit.method = "EXP",
-  rejection.criteria = list(
-    recycling.ratio = 10,
-    recuperation.rate = 10,
-    testdose.error = 10,
-    palaeodose.error = 10,
-    exceed.max.regpoint = TRUE)
-)
