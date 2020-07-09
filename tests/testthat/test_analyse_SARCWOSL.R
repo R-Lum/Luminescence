c1ab6b82d6b19c94c760a21f444bb310f2ac51ea
RLumSK <sebastian.kreutzer@aber.ac.uk>
2020-07-09 23:16:45 +0200

    Extent test on analyse_SAR.CWOSL()

--- tests/testthat/test_analyse_SARCWOSL.R
+++ tests/testthat/test_analyse_SARCWOSL.R
@@ -1,29 +1,63 @@
 context("analyse_SAR.CWOSL")
 
-set.seed(1)
-data(ExampleData.BINfileData, envir = environment())
-object <- Risoe.BINfileData2RLum.Analysis(CWOSL.SAR.Data, pos=1)
+  ##prepare test file
+  set.seed(1)
+  data(ExampleData.BINfileData, envir = environment())
+  object <- Risoe.BINfileData2RLum.Analysis(CWOSL.SAR.Data, pos = 1:2 )
 
-##perform SAR analysis and set rejection criteria
-results <- analyse_SAR.CWOSL(
-object = object,
-signal.integral.min = 1,
-signal.integral.max = 2,
-background.integral.min = 900,
-background.integral.max = 1000,
-log = "x",
-fit.method = "EXP",
-rejection.criteria = list(
-  recycling.ratio = 10,
-  recuperation.rate = 10,
-  testdose.error = 10,
-  palaeodose.error = 10,
-  exceed.max.regpoint = TRUE),
-plot = FALSE,
-verbose = FALSE
-)
+  results <- analyse_SAR.CWOSL(
+    object = object[[1]],
+    signal.integral.min = 1,
+    signal.integral.max = 2,
+    background.integral.min = 900,
+    background.integral.max = 1000,
+    log = "x",
+    fit.method = "EXP",
+    rejection.criteria = list(
+      recycling.ratio = 10,
+      recuperation.rate = 10,
+      testdose.error = 10,
+      palaeodose.error = 10,
+      exceed.max.regpoint = TRUE),
+    plot = FALSE,
+    verbose = FALSE
+  )
 
-test_that("check class and length of output", {
+test_that("simple run", {
+    testthat::skip_on_cran()
+
+  ##perform SAR tests with different parameters
+  ##verbose and plot off
+  expect_s4_class(
+    analyse_SAR.CWOSL(
+      object = object,
+      signal.integral.min = 1,
+      signal.integral.max = 2,
+      background.integral.min = 900,
+      background.integral.max = 1000,
+      fit.method = "EXP",
+      plot = FALSE,
+      verbose = FALSE
+    ),
+    class = "RLum.Results"
+  )
+
+  ##verbose and plot on
+  expect_s4_class(
+    analyse_SAR.CWOSL(
+      object = object[[1]],
+      signal.integral.min = 1,
+      signal.integral.max = 2,
+      background.integral.min = 900,
+      background.integral.max = 1000,
+      fit.method = "LIN"
+    ),
+    class = "RLum.Results"
+  )
+
+})
+
+test_that("tests class elements", {
   testthat::skip_on_cran()
 
     expect_is(results, "RLum.Results")
@@ -35,7 +69,7 @@ test_that("check class and length of output", {
 
 })
 
-test_that("check De values", {
+test_that("regression tests De values", {
   testthat::skip_on_cran()
 
   ##fix for different R versions
@@ -49,7 +83,7 @@ test_that("check De values", {
 
 })
 
-test_that("check LxTx table", {
+test_that("regression test LxTx table", {
   testthat::skip_on_cran()
 
    expect_equal(object = round(sum(results$LnLxTnTx.table$LxTx), digits = 5),  20.92051)
@@ -57,7 +91,7 @@ test_that("check LxTx table", {
 
 })
 
-test_that("check rejection criteria", {
+test_that("regression test - check rejection criteria", {
   testthat::skip_on_cran()
   ##fix for different R versions
   if(R.version$major == "3" && as.numeric(R.version$minor) < 6){
