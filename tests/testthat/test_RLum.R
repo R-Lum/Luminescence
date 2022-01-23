7e7e382ec001b8eb0902bab5c5e9c765efa36d4d
RLumSK <sebastian.kreutzer@aber.ac.uk>
2022-01-23 11:14:14 +0000

    plot_RLum(): update test

--- tests/testthat/test_RLum.R
+++ tests/testthat/test_RLum.R
@@ -5,7 +5,6 @@ test_that("check class", {
   object <- set_RLum(class = "RLum.Data.Curve")
   expect_length(rep(object, 10), 10)
 
-
   ## <RLum.Results>
   data(ExampleData.DeValues, envir = environment())
   temp <- calc_FuchsLang2001(ExampleData.DeValues$BT998, cvThreshold = 5, plot = FALSE)
