3f17837cb10f8be7feadc99c0ca964015818ae35
RLumSK <sebastian.kreutzer@uni-heidelberg.de>
2022-08-01 18:32:41 +0200

    plot_RadialPlot(): add a few tests

---
+++ tests/testthat/test_plot_RadialPlot.R
@@ -0,0 +1,41 @@
+test_that("dedicated test for the radialplot", {
+  testthat::skip_on_cran()
+  local_edition(3)
+
+  ##distribution plots
+  set.seed(12310)
+  x <- rnorm(30,5,0.5)
+  y <- x * runif(30, 0.05, 0.10)
+  df <- data.frame(x,y)
+
+  ## standard data
+  ## simple test
+  expect_silent(
+  plot_RadialPlot(
+    data = df,
+    centrality = 5))
+
+  ## simple test - unlogged
+  expect_silent(
+    plot_RadialPlot(
+      data = df,
+      centrality = 5,
+      log.z = FALSE))
+
+  ## simple test - unlogged
+  expect_silent(
+    plot_RadialPlot(
+      data = df,
+      centrality = -1,
+      log.z = FALSE))
+
+  ## trigger message
+  expect_output(
+    plot_RadialPlot(
+      data = data.frame(x = df$x, y = rep(0.0001, nrow(df))),
+      centrality = -1,
+      log.z = FALSE),
+    regexp = "Attention.*")
+
+
+})
