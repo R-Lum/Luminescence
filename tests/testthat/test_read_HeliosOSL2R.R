cc9218605099500deb8a967f2b504627c85a1ca3
RLumSK <sebastian.kreutzer@uni-heidelberg.de>
2024-07-11 08:28:26 +0200

    new function read_HeliosOSL2R() + ad NEWS + ad tests + up docu

---
+++ tests/testthat/test_read_HeliosOSL2R.R
@@ -0,0 +1,29 @@
+test_that("Test functionality", {
+  testthat::skip_on_cran()
+  local_edition(3)
+
+  ## crash function
+  expect_error(
+    object = read_HeliosOSL2R("error"),
+    regexp = "\\[read\\_HeliosOSL2R\\(\\)\\] File extension")
+  expect_error(
+    object = read_HeliosOSL2R("error.err"),
+    regexp = "\\[read\\_HeliosOSL2R\\(\\)\\] File extension")
+
+  ## standard input
+  file <- system.file("extdata/HeliosOSL_Example.osl", package = "Luminescence")
+  expect_s4_class(
+    object = read_HeliosOSL2R(file),
+    class = "RLum.Analysis")
+
+  ## no verbose
+  expect_silent(
+    object = read_HeliosOSL2R(file, verbose = FALSE))
+
+  ## list input
+  files <- list(file, file)
+  expect_type(
+    object = read_HeliosOSL2R(files),
+    type = "list")
+
+})
