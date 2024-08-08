bc438abe89536493cc898354f62c0932c4559371
Marco Colombo <marco.colombo@uni-heidelberg.de>
2024-08-08 12:50:21 +0200

    Complete coverage for PSL2Risoe.BINfileData.R.

--- tests/testthat/test_PSL2RisoeBINfiledata.R
+++ tests/testthat/test_PSL2RisoeBINfiledata.R
@@ -9,4 +9,19 @@ test_that("simple test", {
   expect_s4_class(bin, "Risoe.BINfileData")
   expect_equal(length(bin), 70)
 
+  ## input validation
+  expect_error(PSL2Risoe.BINfileData("wrong-class"),
+               "Only objects of class 'RLum.Analysis' are allowed")
+
+  ## manipulate the object to trigger other errors
+  fake <- merged
+  fake@records[20] <- "unexpected-subclass"
+  expect_error(PSL2Risoe.BINfileData(fake),
+               "must only contain objects of class 'RLum.Data.Curve'")
+
+  fake <- merged
+  fake@records[10][[1]]@originator <- "unexpected-originator"
+  expect_error(PSL2Risoe.BINfileData(fake),
+               "Only objects originating from 'read_PSL2R()' are allowed",
+               fixed=TRUE)
 })
