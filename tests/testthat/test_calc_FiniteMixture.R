6272a7bccc0b05cdf7e0c3a118c53c35f5450c1a
Marco Colombo <marco.colombo@uni-heidelberg.de>
2024-08-14 17:09:00 +0200

    Clean up input validation in calc_FiniteMixture().

--- tests/testthat/test_calc_FiniteMixture.R
+++ tests/testthat/test_calc_FiniteMixture.R
@@ -5,6 +5,26 @@ test_that("check class and length of output", {
   ## load example data
   data(ExampleData.DeValues, envir = environment())
 
+  ## input validation
+  expect_error(calc_FiniteMixture("test"),
+               "object has to be of type 'data.frame' or 'RLum.Results'")
+  expect_error(calc_FiniteMixture(data.frame(col = 1:10)),
+               "'data' object must have two columns")
+  expect_error(calc_FiniteMixture(),
+               "argument .* is missing, with no default")
+  expect_error(calc_FiniteMixture(ExampleData.DeValues$CA1),
+               "argument .* is missing, with no default")
+  expect_error(calc_FiniteMixture(ExampleData.DeValues$CA1, sigmab = 0.2),
+               "argument .* is missing, with no default")
+  expect_error(calc_FiniteMixture(ExampleData.DeValues$CA1, sigmab = -1),
+               "'sigmab' must be a value between 0 and 1")
+  expect_error(calc_FiniteMixture(ExampleData.DeValues$CA1, sigmab = 0.2,
+                                  n.components = 1),
+               "At least two components need to be fitted")
+  expect_error(calc_FiniteMixture(ExampleData.DeValues$CA1, sigmab = 0.2,
+                                  n.components = 2, pdf.sigma = "test"),
+               "Only 'se' or 'sigmab' allowed for the pdf.sigma argument")
+
   ## simple run
   temp <- expect_s4_class(calc_FiniteMixture(
     ExampleData.DeValues$CA1,
