10468942419b33a0ac90d1530f3165fa863eed70
Marco Colombo <marco.colombo@uni-heidelberg.de>
2024-08-20 10:19:34 +0200

    Increase coverage in calc_OSLLxTxDecomposed.R.

---
+++ tests/testthat/test_calc_OSLLxTxDecomposed.R
@@ -0,0 +1,20 @@
+data(ExampleData.LxTxOSLData, envir = environment())
+
+test_that("input validation", {
+  testthat::skip_on_cran()
+  local_edition(3)
+
+  expect_error(calc_OSLLxTxDecomposed(),
+               "is missing, with no default")
+  expect_error(calc_OSLLxTxDecomposed("test"),
+               "No valid component data.frame for Lx value")
+  expect_error(calc_OSLLxTxDecomposed(data.frame(col = integer(0))),
+               "No valid component data.frame for Lx value")
+  expect_error(calc_OSLLxTxDecomposed(Lx.data, "test"),
+               "No valid component data.frame for Tx value")
+  expect_error(calc_OSLLxTxDecomposed(Lx.data, data.frame(col = integer(0))),
+               "No valid component data.frame for Tx value")
+  expect_error(calc_OSLLxTxDecomposed(Lx.data, Tx.data,
+                                      OSL.component = NA),
+               "Type error! No valid data type for OSL.component")
+})
