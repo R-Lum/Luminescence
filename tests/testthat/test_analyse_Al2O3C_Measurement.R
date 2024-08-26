7b1bd0c49421608f174cad997e786094833b44de
Marco Colombo <marco.colombo@uni-heidelberg.de>
2024-08-26 16:07:09 +0200

    Improve coverage in analyse_Al2O3C_Measurement.R.

--- tests/testthat/test_analyse_Al2O3C_Measurement.R
+++ tests/testthat/test_analyse_Al2O3C_Measurement.R
@@ -1,23 +1,54 @@
-##Full check
-test_that("analyse_Al2O3C_Measurements", {
+## load data
+data(ExampleData.Al2O3C, envir = environment())
+
+test_that("input validation", {
   skip_on_cran()
 
-   ##load data
-   data(ExampleData.Al2O3C, envir = environment())
+  expect_error(analyse_Al2O3C_Measurement(),
+               "is missing, with no default")
+  expect_error(analyse_Al2O3C_Measurement("error"),
+               "'object' must be an 'RLum.Analysis' object or a list of such objects")
+  expect_error(analyse_Al2O3C_Measurement(list(data_CrossTalk, "error")),
+               "Elements in 'object' are not all of type 'RLum.Analysis'")
+  suppressWarnings(
+  expect_error(analyse_Al2O3C_Measurement(data_CrossTalk,
+                                          travel_dosimeter = "error"),
+               "Input for 'travel_dosimeter' is not numeric")
+  )
+  expect_error(analyse_Al2O3C_Measurement(data_CrossTalk,
+                                          irradiation_time_correction = 7),
+               "must have length 2")
+  expect_error(analyse_Al2O3C_Measurement(data_CrossTalk,
+                                          irradiation_time_correction = "a"),
+               "must be a numeric vector or an 'RLum.Results' object")
+  expect_error(analyse_Al2O3C_Measurement(data_CrossTalk,
+                                          cross_talk_correction = "a"),
+               "'cross_talk_correction' was created by an unsupported function")
 
-   ##00 - cause function breaks
-   expect_error(analyse_Al2O3C_Measurement())
-   expect_error(analyse_Al2O3C_Measurement(object = "test"))
-   expect_warning(Luminescence:::.warningCatcher(
-      analyse_Al2O3C_Measurement(object = data_CrossTalk, signal_integral = 1000)))
+  expect_warning(Luminescence:::.warningCatcher(
+                                     analyse_Al2O3C_Measurement(object = data_CrossTalk, signal_integral = 1000)))
+})
+
+test_that("analyse_Al2O3C_Measurements", {
+  skip_on_cran()
 
    ##run analysis
    expect_s4_class(suppressWarnings(analyse_Al2O3C_Measurement(data_CrossTalk)), "RLum.Results")
    expect_s4_class(suppressWarnings(analyse_Al2O3C_Measurement(data_CrossTalk, calculate_TL_dose = TRUE)),
                                     "RLum.Results")
+  expect_output(analyse_Al2O3C_Measurement(data_CrossTalk[[2]],
+                        test_parameter = list(stimulation_power = 0.01)))
+  expect_output(analyse_Al2O3C_Measurement(data_CrossTalk[[2]],
+                                           dose_points = list(3)))
 
-   ##run test without TL curves
-   temp <- get_RLum(data_CrossTalk, recordType = "OSL", drop = FALSE)
-   expect_s4_class(suppressWarnings(analyse_Al2O3C_Measurement(temp)), "RLum.Results")
-
+  ## tests without TL curves
+  temp <- get_RLum(data_CrossTalk, recordType = "OSL", drop = FALSE)
+  expect_s4_class(analyse_Al2O3C_Measurement(temp),
+                  "RLum.Results")
+  expect_output(analyse_Al2O3C_Measurement(temp, travel_dosimeter = 2),
+                "travel dosimeter correction applied")
+  expect_message(analyse_Al2O3C_Measurement(temp, travel_dosimeter = 1:2),
+                 "'travel_dosimeter' specifies every position")
+  expect_message(analyse_Al2O3C_Measurement(temp, travel_dosimeter = 2000),
+                 "Invalid position in 'travel_dosimeter', nothing corrected")
 })
