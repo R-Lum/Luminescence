dae7217aff78f9dfdafedf4291c21c40cebff5f9
Marco Colombo <marco.colombo@uni-heidelberg.de>
2024-08-29 18:31:55 +0200

    Increase coverage in read_SPE2R.R.

--- tests/testthat/test_read_SPE2R.R
+++ tests/testthat/test_read_SPE2R.R
@@ -1,18 +1,42 @@
+## path to the SPE file on github, as it's not included in the package
+github.url <- file.path("https://github.com/R-Lum/Luminescence",
+                        "raw/dev_0.9.x/tests/testthat/_data")
+
+test_that("input validation", {
+  testthat::skip_on_cran()
+
+  expect_message(expect_null(read_SPE2R("error")),
+                 "Error: File does not exist, NULL returned")
+  expect_error(read_SPE2R(file.path(github.url, "SPEfile.SPE"),
+                          output.object = "error"),
+               "'output.object' not supported, valid options are")
+
+  SW({
+  expect_message(expect_null(read_SPE2R("http://httpbun.org/status/404")),
+                 "Error: File does not exist, NULL returned")
+  })
+
+  wrong <- system.file("extdata/BINfile_V8.binx", package = "Luminescence")
+  expect_error(read_SPE2R(wrong),
+               "Unsupported file format")
+})
+
 test_that("Test general functionality", {
   testthat::skip_on_cran()
 
-  github.url <- file.path("https://github.com/R-Lum/Luminescence",
-                          "raw/dev_0.9.x/tests/testthat/_data")
-
-  ##crash function
-  expect_null(read_SPE2R(file = "text"))
-
   ## default values
+  SW({
   expect_s4_class(read_SPE2R(file.path(github.url, "SPEfile.SPE")),
                   "RLum.Data.Image")
+  })
 
-  ##test verbose
+  ## test output.object
   expect_s4_class(read_SPE2R(file.path(github.url, "SPEfile.SPE"),
+                             output.object = "RLum.Data.Spectrum",
                              verbose = FALSE),
-                  "RLum.Data.Image")
+                  "RLum.Data.Spectrum")
+  ret <- read_SPE2R(file.path(github.url, "SPEfile.SPE"),
+                    output.object = "matrix",
+                    verbose = FALSE)
+  expect_true(is.matrix(ret))
 })
