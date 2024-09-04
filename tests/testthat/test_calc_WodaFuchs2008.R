99607b9a359623eec12560c2713210b81a415008
Marco Colombo <marco.colombo@uni-heidelberg.de>
2024-09-04 14:44:38 +0200

    Complete coverage for calc_WodaFuchs2008.R.

--- tests/testthat/test_calc_WodaFuchs2008.R
+++ tests/testthat/test_calc_WodaFuchs2008.R
@@ -1,24 +1,34 @@
+test_that("input validation", {
+  testthat::skip_on_cran()
+
+  expect_warning(expect_null(calc_WodaFuchs2008("error")),
+                 "Input data must be one of 'data.frame', 'RLum.Results' or")
+})
+
 test_that("Test general functionality", {
   testthat::skip_on_cran()
 
-  ##load example data
-  ## read example data set
   data(ExampleData.DeValues, envir = environment())
 
   ##test arguments
-  expect_s4_class(suppressWarnings(calc_WodaFuchs2008(data = ExampleData.DeValues$CA1)), "RLum.Results")
-
-  ##test arguments
-  expect_s4_class(suppressWarnings(
-    calc_WodaFuchs2008(data = ExampleData.DeValues$CA1, plot = FALSE)), "RLum.Results")
-
-  ##test arguments
-  expect_s4_class(calc_WodaFuchs2008(data = ExampleData.DeValues$CA1, breaks = 20), "RLum.Results")
+  expect_s4_class(calc_WodaFuchs2008(ExampleData.DeValues$CA1),
+                  "RLum.Results")
+  expect_s4_class(calc_WodaFuchs2008(ExampleData.DeValues$CA1, plot = FALSE),
+                  "RLum.Results")
+  expect_s4_class(calc_WodaFuchs2008(ExampleData.DeValues$CA1, breaks = 20),
+                  "RLum.Results")
+  expect_warning(calc_WodaFuchs2008(ExampleData.DeValues$CA1[1:40, ]),
+                 "More than one maximum, fit may be invalid")
 
   ## issue #197
   set.seed(1)
   df <- data.frame(rnorm(20, 10), rnorm(20, 0.5))
   expect_silent(calc_WodaFuchs2008(df))
+
+  ## more coverage
+  expect_warning(calc_WodaFuchs2008(df, breaks = 3),
+                 "Fewer than 4 bins produced, 'breaks' set to 4")
+  expect_output(calc_WodaFuchs2008(df, trace = TRUE))
 
   ## numeric vector
   expect_message(calc_WodaFuchs2008(df[, 1]),
@@ -27,4 +37,8 @@ test_that("Test general functionality", {
   ## single-column data.frame
   expect_message(calc_WodaFuchs2008(df[, 1, drop = FALSE]),
                  "No errors provided")
+
+  ## RLum.Results object
+  obj <- calc_CommonDose(ExampleData.DeValues$BT998, verbose = FALSE)
+  expect_silent(calc_WodaFuchs2008(obj))
 })
