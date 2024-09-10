R-Lum/Luminescence

commit bb8cf6903270abd0365e22853a65962aaabb4f6f

Update .RisoeBINfileData2RLum.Data.Curve()

R/Risoe.BINfileData2RLum.Data.Curve.R
@@ -36,7 +36,7 @@
 #' @note Due to changes in the BIN-file (version 3 to version 4) format the recalculation of TL-curves might be not
 #' overall correct for cases where the TL measurement is combined with a preheat.
 #'
-#' @section Function version: 0.4.0
+#' @section Function version: 0.5.0
 #'
 #' @author Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne (France),
 #' Christoph Burow, Universtiy of Cologne (Germany)
@@ -67,74 +67,44 @@
   set
 ){
 
+  ##disaggregate object ... this makes it much faster below
+  ##we could also access via index, not number, but this is far to risky, as
+  ##every update in the BIN-file version will break the code here
+  METADATA <- as.list(object@METADATA)
+  DATA <- object@DATA
 
   # grep id of record -------------------------------------------------------
   ##if id is set, no input for pos and rund is nescessary
   if (missing(id)) {
-    id <- object@METADATA[object@METADATA[, "POSITION"] == pos &
-                            object@METADATA[, "SET"] == set &
-                            object@METADATA[, "RUN"] == run,
-                          "ID"]
+    id <- METADATA[METADATA[["POSITION"]] == pos &
+                     METADATA[["SET"]] == set &
+                     METADATA[["RUN"]] == run,
+                   "ID"]
 
   }
 
 
-  # Select values -----------------------------------------------------------
-
-  ##build matrix
-  if(object@METADATA[id,"NPOINTS"][1] != 0){
-
-    if(object@METADATA[id, "LTYPE"] == "TL" && as.numeric(object@METADATA[id, "VERSION"]) >=4){
-
-      temp.x <- c(
-        seq(
-          from = object@METADATA[["LOW"]][id],
-          to = object@METADATA[["AN_TEMP"]][id],
-          length.out = object@METADATA[["TOLDELAY"]][id]
-        ),
-        seq(
-          from = object@METADATA[["AN_TEMP"]][id],
-          to = object@METADATA[["AN_TEMP"]][id],
-          length.out = object@METADATA[["TOLON"]][id]
-        ),
-        seq(
-          from = object@METADATA[["AN_TEMP"]][id],
-          to = object@METADATA[["HIGH"]][id],
-          length.out = object@METADATA[["TOLOFF"]][id]
-        )
-      )
-
-    }else{
-
-      temp.x <- seq(
-        from = object@METADATA[["LOW"]][id],
-        to = object@METADATA[["HIGH"]][id],
-        length.out = object@METADATA[["NPOINTS"]][id]
-      )
-
-    }
-
-    temp.y <- unlist(object@DATA[id], use.names = FALSE)
-
-
-  }else{
-    temp.x <- NA
-    temp.y <- NA
-
-    warning("[.Risoe.BINfileData2RLum.Data.Curve()] NPOINTS was 0, RLum.Data.Curve-object with NA-values produced.")
-
-  }
-
-  ##convert info elements to list ... this procedure halfs the time needed in comparison to
-  ##to simply as.list(object@METADATA)
-  info <- lapply(1:length(names(object@METADATA)), function(x){.subset2(object@METADATA, x)[id]})
-  names(info) <- names(object@METADATA)
+  ##grep info elements
+  info <- lapply(1:length(names(METADATA)), function(x){METADATA[[x]][id]})
+  names(info) <- names(METADATA)
 
   # Build object ------------------------------------------------------------
   set_RLum(
     class = "RLum.Data.Curve",
-    recordType = as.character(object@METADATA[id, "LTYPE"]),
-    data = matrix(c(temp.x, temp.y), ncol = 2),
+    recordType = METADATA[["LTYPE"]][id],
+    data =  .create_RLumDataCurve_matrix(
+      DATA = DATA[[id]],
+      NPOINTS = METADATA[["NPOINTS"]][id],
+      VERSION = METADATA[["VERSION"]][id],
+      LTYPE = METADATA[["LTYPE"]][id],
+      LOW =  METADATA[["LOW"]][id],
+      HIGH =  METADATA[["HIGH"]][id],
+      AN_TEMP = METADATA[["AN_TEMP"]][id],
+      TOLDELAY =METADATA[["TOLDELAY"]][id],
+      TOLON = METADATA[["TOLON"]][id],
+      TOLOFF = METADATA[["TOLOFF"]][id]
+
+    ),
     info = info
   )
 

R/RcppExports.R
@@ -5,6 +5,10 @@
     .Call('Luminescence_analyse_IRSARRF_SRS', PACKAGE = 'Luminescence', values_regenerated_limited, values_natural_limited, n_MC)
 }
 
+.create_RLumDataCurve_matrix <- function(DATA, VERSION, NPOINTS, LTYPE, LOW, HIGH, AN_TEMP, TOLDELAY, TOLON, TOLOFF) {
+    .Call('Luminescence_create_RLumDataCurve_matrix', PACKAGE = 'Luminescence', DATA, VERSION, NPOINTS, LTYPE, LOW, HIGH, AN_TEMP, TOLDELAY, TOLON, TOLOFF)
+}
+
 .create_UID <- function() {
     .Call('Luminescence_create_UID', PACKAGE = 'Luminescence')
 }

RLum.BuildResults/Luminescence_0.7.0-Functions.html
@@ -65,7 +65,7 @@
 </td>
 <td class=cellinside>2016-06-27
 </td>
-<td class=cellinside>09:57:17
+<td class=cellinside>17:14:51
 
 </td>
 <td class=cellinside><br /> Norbert Mercier, IRAMAT-CRP2A, Universite Bordeaux Montaigne (France), Sebastian Kreutzer,<br /> IRAMAT-CRP2A, Universite Bordeaux Montaigne (France)  <br /> <br /> The underlying Bayesian model based on a contribution by Combes et al., 2015.<br />  R Luminescence Package Team
@@ -82,9 +82,9 @@
 </td>
 <td class=cellinside>0.1.0
 </td>
-<td class=cellinside>2016-06-24
+<td class=cellinside>2016-06-27
 </td>
-<td class=cellinside>19:29:29
+<td class=cellinside>17:14:51
 
 </td>
 <td class=cellinside><br /> Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne (France)<br />  R Luminescence Package Team
@@ -139,9 +139,9 @@
 </td>
 <td class=cellinside>0.0.1
 </td>
-<td class=cellinside>2016-06-22
+<td class=cellinside>2016-06-27
 </td>
-<td class=cellinside>12:18:40
+<td class=cellinside>17:14:51
 
 </td>
 <td class=cellinside><br /> Christoph Burow, University of Cologne (Germany)<br />  R Luminescence Package Team
@@ -574,9 +574,9 @@
 </td>
 <td class=cellinside>0.6.4
 </td>
-<td class=cellinside>2016-06-22
+<td class=cellinside>2016-06-27
 </td>
-<td class=cellinside>17:21:58
+<td class=cellinside>17:14:51
 
 </td>
 <td class=cellinside><br /> Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne<br /> (France)<br />  R Luminescence Package Team
@@ -669,9 +669,9 @@
 </td>
 <td class=cellinside>0.2.0
 </td>
-<td class=cellinside>2016-06-22
+<td class=cellinside>2016-06-27
 </td>
-<td class=cellinside>12:18:40
+<td class=cellinside>17:14:51
 
 </td>
 <td class=cellinside><br /> Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne (France), <br /> Michael Dietze, GFZ Potsdam (Germany)<br />  R Luminescence Package Team
@@ -1266,9 +1266,9 @@
 </td>
 <td class=cellinside>0.1.9
 </td>
-<td class=cellinside>2016-06-22
+<td class=cellinside>2016-06-27
 </td>
-<td class=cellinside>12:18:40
+<td class=cellinside>17:14:51
 
 </td>
 <td class=cellinside><br /> Michael Dietze, GFZ Potsdam (Germany),  Sebastian Kreutzer,<br /> IRAMAT-CRP2A, Universite Bordeaux Montaigne (France)  Inspired by a plot<br /> introduced by Galbraith & Green (1990)<br />  R Luminescence Package Team
@@ -1342,9 +1342,9 @@
 </td>
 <td class=cellinside>1.8.14
 </td>
-<td class=cellinside>2016-06-22
+<td class=cellinside>2016-06-27
 </td>
-<td class=cellinside>16:27:47
+<td class=cellinside>17:14:51
 
 </td>
 <td class=cellinside><br /> Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne<br /> (France),   Michael Dietze, GFZ Potsdam (Germany)<br />  R Luminescence Package Team
@@ -1361,9 +1361,9 @@
 </td>
 <td class=cellinside>0.4.4
 </td>
-<td class=cellinside>2016-06-22
+<td class=cellinside>2016-06-27
 </td>
-<td class=cellinside>12:18:40
+<td class=cellinside>17:14:51
 
 </td>
 <td class=cellinside><br /> Michael Dietze, GFZ Potsdam (Germany),   Sebastian Kreutzer,<br /> IRAMAT-CRP2A, Universite Bordeaux Montaigne (France)<br />  R Luminescence Package Team
@@ -1512,9 +1512,9 @@
 </td>
 <td class=cellinside>0.5.0
 </td>
-<td class=cellinside>2016-06-22
+<td class=cellinside>2016-06-27
 </td>
-<td class=cellinside>12:18:40
+<td class=cellinside>17:14:51
 
 </td>
 <td class=cellinside><br /> Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne<br /> (France)<br />  R Luminescence Package Team
@@ -1588,9 +1588,9 @@
 </td>
 <td class=cellinside>0.0.1
 </td>
-<td class=cellinside>2016-06-22
+<td class=cellinside>2016-06-27
 </td>
-<td class=cellinside>12:18:40
+<td class=cellinside>17:14:51
 
 </td>
 <td class=cellinside><br /> Christoph Burow, University of Cologne (Germany)<br />  R Luminescence Package Team
@@ -1645,9 +1645,9 @@
 </td>
 <td class=cellinside>0.0.1
 </td>
-<td class=cellinside>2016-06-22
+<td class=cellinside>2016-06-27
 </td>
-<td class=cellinside>12:18:40
+<td class=cellinside>17:14:51
 
 </td>
 <td class=cellinside><br /> Christoph Burow, University of Cologne (Germany)<br />  R Luminescence Package Team

RLum.BuildResults/Luminescence_0.7.0-Functions.tex
@@ -1,20 +1,20 @@
 % latex table generated in R 3.4.0 by xtable 1.8-2 package
-% Mon Jun 27 10:00:09 2016
+% Tue Jun 28 12:46:24 2016
 \begin{table}[ht]
 \centering
 \begin{tabular}{rlllllll}
   \hline
  & Name & Title & Description & Version & m.Date & m.Time & Author \\ 
   \hline
-1 & analyse\_baSAR & Bayesian models (baSAR) applied on luminescence data & $<$br /$>$ This function allows the application of Bayesian models on luminescence data, measured$<$br /$>$ with the single-aliquot regenerative-dose (SAR, Murray and Wintle, 2000) protocol. In particular,$<$br /$>$ it follows the idea proposed by Combes et al., 2015 of using an hierarchical model for estimating$<$br /$>$ a central equivalent dose from a set of luminescence measurements. This function is (I) the adaption$<$br /$>$ of this approach for the R environment and (II) an extension and a technical refinement of the$<$br /$>$ published code. $<$br /$>$ & 0.1.18 & 2016-06-27 & 09:57:17
+1 & analyse\_baSAR & Bayesian models (baSAR) applied on luminescence data & $<$br /$>$ This function allows the application of Bayesian models on luminescence data, measured$<$br /$>$ with the single-aliquot regenerative-dose (SAR, Murray and Wintle, 2000) protocol. In particular,$<$br /$>$ it follows the idea proposed by Combes et al., 2015 of using an hierarchical model for estimating$<$br /$>$ a central equivalent dose from a set of luminescence measurements. This function is (I) the adaption$<$br /$>$ of this approach for the R environment and (II) an extension and a technical refinement of the$<$br /$>$ published code. $<$br /$>$ & 0.1.18 & 2016-06-27 & 17:14:51
  & $<$br /$>$ Norbert Mercier, IRAMAT-CRP2A, Universite Bordeaux Montaigne (France), Sebastian Kreutzer,$<$br /$>$ IRAMAT-CRP2A, Universite Bordeaux Montaigne (France)  $<$br /$>$ $<$br /$>$ The underlying Bayesian model based on a contribution by Combes et al., 2015.$<$br /$>$  R Luminescence Package Team \\ 
-  2 & analyse\_FadingMeasurement & Analyse fading measurements and returns the fading rate per decade (g-value) & $<$br /$>$ The function analysis fading measurements and returns a fading rate including an error estimation.$<$br /$>$ The function is not limited to standard fading measurements, as can be seen, e.g., Huntely and$<$br /$>$ Lamothe 2001.$<$br /$>$ & 0.1.0 & 2016-06-24 & 19:29:29
+  2 & analyse\_FadingMeasurement & Analyse fading measurements and returns the fading rate per decade (g-value) & $<$br /$>$ The function analysis fading measurements and returns a fading rate including an error estimation.$<$br /$>$ The function is not limited to standard fading measurements, as can be seen, e.g., Huntely and$<$br /$>$ Lamothe 2001.$<$br /$>$ & 0.1.0 & 2016-06-27 & 17:14:51
  & $<$br /$>$ Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne (France)$<$br /$>$  R Luminescence Package Team \\ 
   3 & analyse\_IRSAR.RF & Analyse IRSAR RF measurements & $<$br /$>$ Function to analyse IRSAR RF measurements on K-feldspar samples, performed$<$br /$>$ using the protocol according to Erfurt et al. (2003) and beyond.$<$br /$>$ & 0.6.9 & 2016-05-27 & 10:40:45
  & $<$br /$>$ Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne (France)$<$br /$>$  R Luminescence Package Team \\ 
   4 & analyse\_pIRIRSequence & Analyse post-IR IRSL sequences & $<$br /$>$ The function performs an analysis of post-IR IRSL sequences including curve$<$br /$>$ fitting on  RLum.Analysis  objects.$<$br /$>$ & 0.2.0 & 2016-01-18 & 15:07:46
  & $<$br /$>$ Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne$<$br /$>$ (France)$<$br /$>$  R Luminescence Package Team \\ 
-  5 & analyse\_portableOSL & Analyse portable CW-OSL measurements & $<$br /$>$ The function analyses CW-OSL curve data produced by a SUERC portable OSL reader and$<$br /$>$ produces a combined plot of OSL/IRSL signal intensities, OSL/IRSL depletion ratios$<$br /$>$ and the IRSL/OSL ratio.$<$br /$>$ & 0.0.1 & 2016-06-22 & 12:18:40
+  5 & analyse\_portableOSL & Analyse portable CW-OSL measurements & $<$br /$>$ The function analyses CW-OSL curve data produced by a SUERC portable OSL reader and$<$br /$>$ produces a combined plot of OSL/IRSL signal intensities, OSL/IRSL depletion ratios$<$br /$>$ and the IRSL/OSL ratio.$<$br /$>$ & 0.0.1 & 2016-06-27 & 17:14:51
  & $<$br /$>$ Christoph Burow, University of Cologne (Germany)$<$br /$>$  R Luminescence Package Team \\ 
   6 & analyse\_SAR.CWOSL & Analyse SAR CW-OSL measurements & $<$br /$>$ The function performs a SAR CW-OSL analysis on an$<$br /$>$ RLum.Analysis  object including growth curve fitting.$<$br /$>$ & 0.7.5 & 2016-06-21 & 18:08:35
  & $<$br /$>$ Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne$<$br /$>$ (France)$<$br /$>$  R Luminescence Package Team \\ 
@@ -58,7 +58,7 @@
  & $<$br /$>$ Christoph Burow, University of Cologne (Germany)   Based on a$<$br /$>$ rewritten S script of Rex Galbraith, 2010  $<$br /$>$  R Luminescence Package Team \\ 
   27 & calc\_MinDose & Apply the (un-)logged minimum age model (MAM) after Galbraith et al. (1999)$<$br /$>$ to a given De distribution & $<$br /$>$ Function to fit the (un-)logged three or four parameter minimum dose model$<$br /$>$ (MAM-3/4) to De data.$<$br /$>$ & 0.4.3 & 2016-05-24 & 12:14:20
  & $<$br /$>$ Christoph Burow, University of Cologne (Germany)   Based on a$<$br /$>$ rewritten S script of Rex Galbraith, 2010   The bootstrap approach is$<$br /$>$ based on a rewritten MATLAB script of Alastair Cunningham.   Alastair$<$br /$>$ Cunningham is thanked for his help in implementing and cross-checking the$<$br /$>$ code.$<$br /$>$  R Luminescence Package Team \\ 
-  28 & calc\_OSLLxTxRatio & Calculate Lx/Tx ratio for CW-OSL curves & $<$br /$>$ Calculate Lx/Tx ratios from a given set of CW-OSL curves assuming late light background subtraction.$<$br /$>$ & 0.6.4 & 2016-06-22 & 17:21:58
+  28 & calc\_OSLLxTxRatio & Calculate Lx/Tx ratio for CW-OSL curves & $<$br /$>$ Calculate Lx/Tx ratios from a given set of CW-OSL curves assuming late light background subtraction.$<$br /$>$ & 0.6.4 & 2016-06-27 & 17:14:51
  & $<$br /$>$ Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne$<$br /$>$ (France)$<$br /$>$  R Luminescence Package Team \\ 
   29 & calc\_SourceDoseRate & Calculation of the source dose rate via the date of measurement & $<$br /$>$ Calculating the dose rate of the irradiation source via the date of$<$br /$>$ measurement based on: source calibration date, source dose rate, dose rate$<$br /$>$ error. The function returns a data.frame that provides the input argument$<$br /$>$ dose\_rate for the function  Second2Gray .$<$br /$>$ & 0.3.0 & 2015-11-29 & 17:27:48
  & $<$br /$>$ Margret C. Fuchs, HZDR, Helmholtz-Institute Freiberg for Resource Technology (Germany),$<$br /$>$  Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne (France)$<$br /$>$  R Luminescence Package Team \\ 
@@ -68,7 +68,7 @@
  & $<$br /$>$ Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne (France)$<$br /$>$  R Luminescence Package Team \\ 
   32 & calc\_TLLxTxRatio & Calculate the Lx/Tx ratio for a given set of TL curves [beta version] & $<$br /$>$ Calculate Lx/Tx ratio for a given set of TL curves.$<$br /$>$ & 0.3.0 & 2015-11-29 & 17:27:48
  & $<$br /$>$ Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne$<$br /$>$ (France), Christoph Schmidt, University of Bayreuth (Germany)$<$br /$>$  R Luminescence Package Team \\ 
-  33 & calc\_WodaFuchs2008 & Obtain the equivalent dose using the approach proposed by Woda and Fuchs 2008 & $<$br /$>$ The description section$<$br /$>$ & 0.2.0 & 2016-06-22 & 12:18:40
+  33 & calc\_WodaFuchs2008 & Obtain the equivalent dose using the approach proposed by Woda and Fuchs 2008 & $<$br /$>$ The description section$<$br /$>$ & 0.2.0 & 2016-06-27 & 17:14:51
  & $<$br /$>$ Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne (France), $<$br /$>$ Michael Dietze, GFZ Potsdam (Germany)$<$br /$>$  R Luminescence Package Team \\ 
   34 & CW2pHMi & Transform a CW-OSL curve into a pHM-OSL curve via interpolation under$<$br /$>$ hyperbolic modulation conditions & $<$br /$>$ This function transforms a conventionally measured continuous-wave (CW)$<$br /$>$ OSL-curve to a pseudo hyperbolic modulated (pHM) curve under hyperbolic$<$br /$>$ modulation conditions using the interpolation procedure described by Bos \&$<$br /$>$ Wallinga (2012).$<$br /$>$ & 0.2.2 & 2015-11-29 & 17:27:48
  & $<$br /$>$ Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne$<$br /$>$ (France)   Based on comments and suggestions from:  Adrie J.J. Bos,$<$br /$>$ Delft University of Technology, The Netherlands $<$br /$>$  R Luminescence Package Team \\ 
@@ -121,7 +121,7 @@
  & $<$br /$>$ Johannes Friedrich, University of Bayreuth (Germany), $<$br /$>$ Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaige (France),  $<$br /$>$  R Luminescence Package Team \\ 
   64 & names\_RLum & S4-names function for RLum S4 class objects & $<$br /$>$ Function calls object-specific names functions for RLum S4 class objects.$<$br /$>$ & 0.1.0 & 2015-11-29 & 17:27:48
  & $<$br /$>$ Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne$<$br /$>$ (France)$<$br /$>$  R Luminescence Package Team \\ 
-  65 & plot\_AbanicoPlot & Function to create an Abanico Plot. & $<$br /$>$ A plot is produced which allows comprehensive presentation of data precision$<$br /$>$ and its dispersion around a central value as well as illustration of a$<$br /$>$ kernel density estimate, histogram and/or dot plot of the dose values.$<$br /$>$ & 0.1.9 & 2016-06-22 & 12:18:40
+  65 & plot\_AbanicoPlot & Function to create an Abanico Plot. & $<$br /$>$ A plot is produced which allows comprehensive presentation of data precision$<$br /$>$ and its dispersion around a central value as well as illustration of a$<$br /$>$ kernel density estimate, histogram and/or dot plot of the dose values.$<$br /$>$ & 0.1.9 & 2016-06-27 & 17:14:51
  & $<$br /$>$ Michael Dietze, GFZ Potsdam (Germany),  Sebastian Kreutzer,$<$br /$>$ IRAMAT-CRP2A, Universite Bordeaux Montaigne (France)  Inspired by a plot$<$br /$>$ introduced by Galbraith \& Green (1990)$<$br /$>$  R Luminescence Package Team \\ 
   66 & plot\_DetPlot & Create De(t) plot & $<$br /$>$ Plots the equivalent dose (De) in dependency of the chosen signal integral (cf. Bailey et al., 2003).$<$br /$>$ The function is simply passing several arguments to the function  plot  and the used$<$br /$>$ analysis functions and runs it in a loop. Example:  legend.pos  for legend position,$<$br /$>$ legend  for legend text. $<$br /$>$ & 0.1.0 & 2016-05-19 & 23:48:19
  & $<$br /$>$ Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne (France)$<$br /$>$  R Luminescence Package Team \\ 
@@ -129,9 +129,9 @@
  & $<$br /$>$ Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne$<$br /$>$ (France), Michael Dietze, GFZ Potsdam (Germany)$<$br /$>$  R Luminescence Package Team \\ 
   68 & plot\_FilterCombinations & Plot filter combinations along with net transmission window & $<$br /$>$ The function allows to plot transmission windows for different filters. Missing data for specific$<$br /$>$ wavelenghts are automatically interpolated for the given filter data using the function  approx .$<$br /$>$ With that a standardised output is reached and a net transmission window can be shown. $<$br /$>$ & 0.1.0 & 2016-05-02 & 09:36:06
  & $<$br /$>$ Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montagine (France) $<$br /$>$  R Luminescence Package Team \\ 
-  69 & plot\_GrowthCurve & Fit and plot a growth curve for luminescence data (Lx/Tx against dose) & $<$br /$>$ A dose response curve is produced for luminescence measurements using a$<$br /$>$ regenerative protocol.$<$br /$>$ & 1.8.14 & 2016-06-22 & 16:27:47
+  69 & plot\_GrowthCurve & Fit and plot a growth curve for luminescence data (Lx/Tx against dose) & $<$br /$>$ A dose response curve is produced for luminescence measurements using a$<$br /$>$ regenerative protocol.$<$br /$>$ & 1.8.14 & 2016-06-27 & 17:14:51
  & $<$br /$>$ Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne$<$br /$>$ (France),   Michael Dietze, GFZ Potsdam (Germany)$<$br /$>$  R Luminescence Package Team \\ 
-  70 & plot\_Histogram & Plot a histogram with separate error plot & $<$br /$>$ Function plots a predefined histogram with an accompanying error plot as$<$br /$>$ suggested by Rex Galbraith at the UK LED in Oxford 2010.$<$br /$>$ & 0.4.4 & 2016-06-22 & 12:18:40
+  70 & plot\_Histogram & Plot a histogram with separate error plot & $<$br /$>$ Function plots a predefined histogram with an accompanying error plot as$<$br /$>$ suggested by Rex Galbraith at the UK LED in Oxford 2010.$<$br /$>$ & 0.4.4 & 2016-06-27 & 17:14:51
  & $<$br /$>$ Michael Dietze, GFZ Potsdam (Germany),   Sebastian Kreutzer,$<$br /$>$ IRAMAT-CRP2A, Universite Bordeaux Montaigne (France)$<$br /$>$  R Luminescence Package Team \\ 
   71 & plot\_KDE & Plot kernel density estimate with statistics & $<$br /$>$ Plot a kernel density estimate of measurement values in combination with the$<$br /$>$ actual values and associated error bars in ascending order. If enabled, the$<$br /$>$ boxplot will show the usual distribution parameters (median as$<$br /$>$ bold line, box delimited by the first and third quartile, whiskers defined$<$br /$>$ by the extremes and outliers shown as points) and also the mean and$<$br /$>$ standard deviation as pale bold line and pale polygon, respectively.$<$br /$>$ & 3.5.2 & 2016-06-22 & 12:08:51
  & $<$br /$>$ Michael Dietze, GFZ Potsdam (Germany),  Sebastian Kreutzer,$<$br /$>$ IRAMAT-CRP2A, Universite Bordeaux Montaigne$<$br /$>$  R Luminescence Package Team \\ 
@@ -146,7 +146,7 @@
  & $<$br /$>$ Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne$<$br /$>$ (France)$<$br /$>$  R Luminescence Package Team \\ 
   77 & plot\_RLum.Data.Image & Plot function for an  RLum.Data.Image  S4 class object & $<$br /$>$ The function provides a standardised plot output for image data of an$<$br /$>$ RLum.Data.Image S4 class object, mainly using the plot functions$<$br /$>$ provided by the  raster  package.$<$br /$>$ & 0.1 & 2015-11-29 & 17:27:48
  & $<$br /$>$ Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne$<$br /$>$ (France)$<$br /$>$  R Luminescence Package Team \\ 
-  78 & plot\_RLum.Data.Spectrum & Plot function for an RLum.Data.Spectrum S4 class object & $<$br /$>$ The function provides a standardised plot output for spectrum data of an$<$br /$>$ RLum.Data.Spectrum S4 class object$<$br /$>$ & 0.5.0 & 2016-06-22 & 12:18:40
+  78 & plot\_RLum.Data.Spectrum & Plot function for an RLum.Data.Spectrum S4 class object & $<$br /$>$ The function provides a standardised plot output for spectrum data of an$<$br /$>$ RLum.Data.Spectrum S4 class object$<$br /$>$ & 0.5.0 & 2016-06-27 & 17:14:51
  & $<$br /$>$ Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne$<$br /$>$ (France)$<$br /$>$  R Luminescence Package Team \\ 
   79 & plot\_RLum & General plot function for RLum S4 class objects & $<$br /$>$ Function calls object specific plot functions for RLum S4 class objects.$<$br /$>$ & 0.4.2 & 2016-05-02 & 09:36:06
  & $<$br /$>$ Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne$<$br /$>$ (France)$<$br /$>$  R Luminescence Package Team \\ 
@@ -154,13 +154,13 @@
  & $<$br /$>$ Christoph Burow, University of Cologne (Germany), Sebastian Kreutzer, IRAMAT-CRP2A,$<$br /$>$ Universite Bordeaux Montaigne (France)$<$br /$>$  R Luminescence Package Team \\ 
   81 & plot\_ViolinPlot & Create a violin plot & $<$br /$>$ Draws a kernal densiy plot in combination with a boxplot in its middle. The shape of the violin$<$br /$>$ is constructed using a mirrored density curve. This plot is especially designed for cases$<$br /$>$ where the individual errors are zero or to small to be visualised. The idea for this plot is$<$br /$>$ based on the the 'volcano plot' in the ggplot2 package by Hadely Wickham and Winston Chang.$<$br /$>$ The general idea for the Violin Plot seems to be introduced by Hintze and Nelson (1998).$<$br /$>$ & 0.1.2 & 2016-05-17 & 13:27:04
  & $<$br /$>$ Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne (France)$<$br /$>$  R Luminescence Package Team \\ 
-  82 & PSL2Risoe.BINfileData & Convert portable OSL data to an Risoe.BINfileData object & $<$br /$>$ Converts an  RLum.Analysis  object produced by the function  read\_PSL2R()  to$<$br /$>$ an  Risoe.BINfileData  object  (BETA) .$<$br /$>$ & 0.0.1 & 2016-06-22 & 12:18:40
+  82 & PSL2Risoe.BINfileData & Convert portable OSL data to an Risoe.BINfileData object & $<$br /$>$ Converts an  RLum.Analysis  object produced by the function  read\_PSL2R()  to$<$br /$>$ an  Risoe.BINfileData  object  (BETA) .$<$br /$>$ & 0.0.1 & 2016-06-27 & 17:14:51
  & $<$br /$>$ Christoph Burow, University of Cologne (Germany)$<$br /$>$  R Luminescence Package Team \\ 
   83 & read\_BIN2R & Import Risoe BIN-file into R & $<$br /$>$ Import a *.bin or a *.binx file produced by a Risoe DA15 and DA20 TL/OSL$<$br /$>$ reader into R.$<$br /$>$ & 0.15.0 & 2016-06-13 & 21:17:19
  & $<$br /$>$ Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne$<$br /$>$ (France), Margret C. Fuchs, HZDR Freiberg, (Germany)$<$br /$>$  R Luminescence Package Team \\ 
   84 & read\_Daybreak2R & Import Daybreak ASCII dato into R & $<$br /$>$ Import a *.txt (ASCII) file produced by a Daybreak reader into R.$<$br /$>$ & 0.2.1 & 2016-05-02 & 09:36:06
  & $<$br /$>$ Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne$<$br /$>$ (France)  Based on a suggestion by Willian Amidon and Andrew Louis Gorin.$<$br /$>$  R Luminescence Package Team \\ 
-  85 & read\_PSL2R & Import PSL files to R & $<$br /$>$ Imports PSL files produced by a SUERC portable OSL reader into R  (BETA) .$<$br /$>$ & 0.0.1 & 2016-06-22 & 12:18:40
+  85 & read\_PSL2R & Import PSL files to R & $<$br /$>$ Imports PSL files produced by a SUERC portable OSL reader into R  (BETA) .$<$br /$>$ & 0.0.1 & 2016-06-27 & 17:14:51
  & $<$br /$>$ Christoph Burow, University of Cologne (Germany)$<$br /$>$  R Luminescence Package Team \\ 
   86 & read\_SPE2R & Import Princeton Intruments (TM) SPE-file into R & $<$br /$>$ Function imports Princeton Instruments (TM) SPE-files into R environment and$<$br /$>$ provides  RLum  objects as output.$<$br /$>$ & 0.1.0 & 2016-05-02 & 09:42:32
  & $<$br /$>$ Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne$<$br /$>$ (France)$<$br /$>$  R Luminescence Package Team \\ 

commit 41cb06beee4f1f25f58d232c90f755b149a5152e

Make .Risoe.BINfileData2RLum.Data.Curve() work without the id argument.
If id is not passed as an argument, the function tries to resolve it by
using the pos, run and set arguments, but this error would occur:

  > .Risoe.BINfileData2RLum.Data.Curve(CWOSL.SAR.Data,
                                       pos = 1, set = 2, run = 1)
  Error in METADATA[METADATA[["POSITION"]] == pos & METADATA[["SET"]] == :
    incorrect number of dimensions

As METADATA is converted from data.frame to a list a few lines before, it
cannot be accessed any longer with the data.frame indexing, but is should
use the standard list accessor.

This was an ancient bug introduced in bb8cf69, but totally harmless since
it's in an internal function that is never called withou the idt argument.

R/Risoe.BINfileData2RLum.Data.Curve.R
@@ -76,11 +76,9 @@
   # grep id of record -------------------------------------------------------
   ##if id is set, no input for pos and run is necessary
   if (missing(id)) {
-    id <- METADATA[METADATA[["POSITION"]] == pos &
-                     METADATA[["SET"]] == set &
-                     METADATA[["RUN"]] == run,
-                   "ID"]
-
+    id <- METADATA$ID[METADATA[["POSITION"]] == pos &
+                      METADATA[["SET"]] == set &
+                      METADATA[["RUN"]] == run]
   }
 
   ##grep info elements

tests/testthat/test_Risoe.BINfileData2RLum.Data.Curve.R
@@ -1,0 +1,18 @@
+test_that("check functionality", {
+  testthat::skip_on_cran()
+
+  data(ExampleData.BINfileData, envir = environment())
+
+  res <- .Risoe.BINfileData2RLum.Data.Curve(CWOSL.SAR.Data, id = 1)
+  expect_s4_class(res, "RLum.Data.Curve")
+  expect_length(res@data, 500)
+  expect_length(res@info, 44)
+  expect_equal(res@originator, ".Risoe.BINfileData2RLum.Data.Curve")
+
+  res1 <- .Risoe.BINfileData2RLum.Data.Curve(CWOSL.SAR.Data,
+                                             pos = 1, set = 2, run = 1)
+  expect_s4_class(res1, "RLum.Data.Curve")
+  expect_equal(res1@data, res@data)
+  expect_equal(res1@info, res@info)
+  expect_equal(res1@originator, res@originator)
+})