41e15cd4838a85d4ba8c62ccdff1b6fd3ef40246
RLumSK <sebastian.kreutzer@uni-heidelberg.de>
2024-09-15 21:51:55 +0200

    get_RLum() + fx behaviour of null.rm = TRUE; it was not removing empty list elements + ad tests + ad NEWS

--- tests/testthat/test_get_RLum.R
+++ tests/testthat/test_get_RLum.R
@@ -37,6 +37,12 @@ test_that("check get_RLum on a list and NULL", {
   res <- get_RLum(list(temp, temp_RLumAnalysis), null.rm = TRUE)
   expect_length(res, 1)
 
+  ##check list of such objects
+  l <- list(object, object)
+  l[[1]]@records[[1]]@recordType <- "IRSL"
+  expect_length(suppressWarnings(get_RLum(l, recordType = "IRSL", null.rm = FALSE)), 2)
+  expect_length(suppressWarnings(get_RLum(l, recordType = "IRSL", null.rm = TRUE)), 0)
+
   ##check class argument
   a <- list(set_RLum("RLum.Results"), set_RLum("RLum.Analysis", records = list(set_RLum("RLum.Data.Curve"))))
   expect_type(get_RLum(a, class = "test", drop = FALSE), "list")
