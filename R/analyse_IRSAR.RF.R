R-Lum/Luminescence

commit f0d0752753893eeb3606683d8fb334f7886933e4

Start using the .listify() helper function.

R/analyse_IRSAR.RF.R
@@ -436,45 +436,27 @@ analyse_IRSAR.RF<- function(
 
   # SELF CALL -----------------------------------------------------------------------------------
   if(is.list(object)){
-    ##extent the list of arguments if set
+    ## expand input arguments
+    rep.length <- length(object)
 
-    ##sequence_structure
-    sequence_structure <- rep(list(sequence_structure), length = length(object))
-
-    ##RF_nat.lim
-    RF_nat.lim <- rep(list(RF_nat.lim), length = length(object))
-
-    ##RF_reg.lim
-    RF_reg.lim <- rep(list(RF_reg.lim), length = length(object))
-
-    ##method
-    method <- rep(list(method), length = length(object))
-
-    ##method.control
-    method.control <- rep(list(method.control), length = length(object))
+    sequence_structure <- .listify(sequence_structure, rep.length)
+    RF_nat.lim <- .listify(RF_nat.lim, rep.length)
+    RF_reg.lim <- .listify(RF_reg.lim, rep.length)
+    method <- .listify(method, rep.length)
+    method.control <- .listify(method.control, rep.length)
+    n.MC <- .listify(n.MC, rep.length)
 
     ##test_parameters
     if(is(test_parameters[[1]], "list")){
-      test_parameters <- rep(test_parameters, length = length(object))
+      test_parameters <- rep(test_parameters, rep.length)
 
     }else{
-     test_parameters <- rep(list(test_parameters), length = length(object))
-
+     test_parameters <- rep(list(test_parameters), rep.length)
     }
-
-    ##n.MC
-    n.MC <- rep(list(n.MC), length = length(object))
 
     ##main
     if("main"%in% names(list(...))){
-
-      if(is(list(...)$main, "list")){
-        temp_main <- rep(list(...)$main, length = length(object))
-
-      }else{
-        temp_main <- rep(list(list(...)$main), length = length(object))
-
-      }
+      temp_main <- .listify(list(...)$main, rep.length)
 
     }else{
       if(object[[1]]@originator == "read_RF2R"){
@@ -482,7 +464,6 @@ analyse_IRSAR.RF<- function(
       } else {
         temp_main <- as.list(paste0("ALQ #",1:length(object)))
       }
-
     }
 
 

R/analyse_Al2O3C_ITC.R
@@ -125,18 +125,10 @@ analyse_Al2O3C_ITC <- function(
            function(x) .validate_class(x, "RLum.Analysis",
                                        name = "All elements of 'object'"))
 
-    ##expand input arguments
-    if(!is.null(signal_integral)){
-      signal_integral <- rep(list(signal_integral, length = length(object)))
-    }
-
-    ##dose points
-    if(is(dose_points, "list")){
-      dose.points <- rep(dose_points, length = length(object))
-
-    }else{
-      dose_points <- rep(list(dose_points), length = length(object))
-    }
+    ## expand input arguments
+    rep.length <- length(object)
+    signal_integral <- .listify(signal_integral, rep.length)
+    dose_points <- .listify(dose_points, rep.length)
 
     ##method_control
     ##verbose
@@ -252,7 +244,6 @@ analyse_Al2O3C_ITC <- function(
     fit.method = method_control_settings$fit.method,
     verbose = FALSE
   )
-
 
   ##output
   if(verbose){

R/analyse_Al2O3C_Measurement.R
@@ -157,44 +157,18 @@ analyse_Al2O3C_Measurement <- function(
            function(x) .validate_class(x, "RLum.Analysis",
                                        name = "All elements of 'object'"))
 
-    ##expand input arguments
+    ## expand input arguments
+    rep.length <- length(object)
+
     if(!is.null(signal_integral)){
-      signal_integral <- rep(list(signal_integral), length = length(object))
+      signal_integral <- .listify(list(signal_integral), rep.length)
     }
 
-    ##dose points
-    if(is(dose_points, "list")){
-      dose_points <- rep(dose_points, length = length(object))
-
-    }else{
-      dose_points <- rep(list(dose_points), length = length(object))
-    }
-
-    ##irradiation time correction
-    if(is(irradiation_time_correction, "list")){
-      irradiation_time_correction <- rep(irradiation_time_correction, length = length(object))
-
-    }else{
-      irradiation_time_correction <- rep(list(irradiation_time_correction), length = length(object))
-    }
-
-    ##cross talk correction
-    if(is( cross_talk_correction, "list")){
-      cross_talk_correction <- rep( cross_talk_correction, length = length(object))
-
-    }else{
-      cross_talk_correction <- rep(list( cross_talk_correction), length = length(object))
-    }
-
-    ##test_parameters
-    if(is(test_parameters[[1]], "list")){
-      test_parameters <- rep(test_parameters, length = length(object))
-
-    }else{
-      test_parameters <- rep(list(test_parameters), length = length(object))
-    }
-
-    ##verbose
+    dose_points <- .listify(dose_points, rep.length)
+    irradiation_time_correction <- .listify(irradiation_time_correction,
+                                            rep.length)
+    cross_talk_correction <- .listify(cross_talk_correction, rep.length)
+    test_parameters <- .listify(test_parameters, rep.length)
 
     ##plot
     if(is(plot, "logical")){

R/analyse_SAR.CWOSL.R
@@ -281,16 +281,10 @@ if(is.list(object)){
 
   ##handle main separately
   if("main"%in% names(list(...))){
-    if(inherits(list(...)$main, "list")){
-      main <- rep(list(...)$main,length = length(object))
-
-    }else{
-      main <- rep(as.list(list(...)$main),length = length(object))
-    }
+    main <- .listify(list(...)$main, length = length(object))
 
   }else{
     main <- as.list(paste0("ALQ #",1:length(object)))
-
   }
 
   results <- merge_RLum(lapply(1:length(object), function(x){

R/analyse_baSAR.R
@@ -1163,66 +1163,25 @@ analyse_baSAR <- function(
       object.file_name[is.duplicated] <- NULL
     }
 
-  # Expand parameter list -----------------------------------------------------------------------
+  ## Expand input arguments -------------------------------------------------
 
-  ##test_parameter = source_doserate
-  if(!is.null(source_doserate)){
-    if(is(source_doserate, "list")){
-      source_doserate <- rep(source_doserate, length = length(fileBIN.list))
-    }else{
-      source_doserate <- rep(list(source_doserate), length = length(fileBIN.list))
-    }
-  }else{
+  rep.length <- length(fileBIN.list)
+
+  if (is.null(source_doserate)) {
     .throw_error("'source_doserate' is missing, but the current ",
                  "implementation expects dose values in Gy")
   }
+  source_doserate <- .listify(source_doserate, rep.length)
+  sigmab <- .listify(sigmab, rep.length)
+  sig0 <- .listify(sig0, rep.length)
+  signal.integral <- .listify(signal.integral, rep.length)
+  background.integral <- .listify(background.integral, rep.length)
 
-  ##sigmab
-  if(is(sigmab, "list")){
-    sigmab <- rep(sigmab, length = length(fileBIN.list))
-    }else{
-    sigmab <- rep(list(sigmab), length = length(fileBIN.list))
-    }
-
-  ##sig0
-  if(is(sig0, "list")){
-    sig0 <- rep(sig0, length = length(fileBIN.list))
-  }else{
-    sig0 <- rep(list(sig0), length = length(fileBIN.list))
-  }
-
-  ##test_parameter = signal.integral
-  if(is(signal.integral, "list")){
-    signal.integral <- rep(signal.integral, length = length(fileBIN.list))
-  }else{
-    signal.integral <- rep(list(signal.integral), length = length(fileBIN.list))
-  }
-
-  ##test_parameter = signal.integral.Tx
   if (!is.null(signal.integral.Tx)) {
-    if (is(signal.integral.Tx, "list")) {
-      signal.integral.Tx <- rep(signal.integral.Tx, length = length(fileBIN.list))
-    } else{
-      signal.integral.Tx <- rep(list(signal.integral.Tx), length = length(fileBIN.list))
-    }
+    signal.integral.Tx <- .listify(signal.integral.Tx, rep.length)
   }
-
-  ##test_parameter = background.integral
-  if(is(background.integral, "list")){
-    background.integral <- rep(background.integral, length = length(fileBIN.list))
-  }else{
-    background.integral <- rep(list(background.integral), length = length(fileBIN.list))
-  }
-
-  ##test_parameter = background.integral.Tx
   if (!is.null(background.integral.Tx)) {
-    if (is(background.integral.Tx, "list")) {
-      background.integral.Tx <-
-        rep(background.integral.Tx, length = length(fileBIN.list))
-    } else{
-      background.integral.Tx <-
-        rep(list(background.integral.Tx), length = length(fileBIN.list))
-    }
+    background.integral.Tx <- .listify(background.integral.Tx, rep.length)
   }
 
   # Read CSV file -----------------------------------------------------------

R/analyse_pIRIRSequence.R
@@ -198,40 +198,22 @@ analyse_pIRIRSequence <- function(
       .throw_warning("'signal.integral.max' missing, set to 2")
     }
 
-    ##now we have to extend everything to allow list of arguments ... this is just consequent
-    signal.integral.min <- rep(list(signal.integral.min), length = length(object))
-    signal.integral.max <- rep(list(signal.integral.max), length = length(object))
-    background.integral.min <- rep(list(background.integral.min), length = length(object))
-    background.integral.max <- rep(list(background.integral.max), length = length(object))
-    sequence.structure <- rep(list(sequence.structure), length = length(object))
+   ## expand input arguments
+   rep.length <- length(object)
 
-    if(!is.null(dose.points)){
+   signal.integral.min <- .listify(signal.integral.min, rep.length)
+   signal.integral.max <- .listify(signal.integral.max, rep.length)
+   background.integral.min <- .listify(background.integral.min, rep.length)
+   background.integral.max <- .listify(background.integral.max, rep.length)
+   sequence.structure <- .listify(sequence.structure, rep.length)
+   dose.points <- .listify(dose.points, rep.length)
 
-      if(is(dose.points, "list")){
-        dose.points <- rep(dose.points, length = length(object))
+   if ("main" %in% names(list(...))) {
+     main_list <- .listify(list(...)$main, rep.length)
+   }
 
-      }else{
-        dose.points <- rep(list(dose.points), length = length(object))
-
-      }
-
-    }else{
-      dose.points <- rep(list(NULL), length(object))
-
-    }
-
-    ##main
-    if("main" %in% names(list(...))){
-      main_list <- rep(list(...)$main, length.out = length(object))
-
-      if(!inherits(main_list, "list")){
-        main_list <- as.list(main_list)
-
-      }
-    }
-
-    ##run analysis
-    temp <- lapply(1:length(object), function(x){
+   ## run analysis
+   temp <- lapply(1:length(object), function(x) {
       analyse_pIRIRSequence(object[[x]],
                         signal.integral.min = signal.integral.min[[x]],
                         signal.integral.max = signal.integral.max[[x]],
@@ -243,7 +225,6 @@ analyse_pIRIRSequence <- function(
                         plot.single = plot.single,
                         main = ifelse("main"%in% names(list(...)), main_list[[x]], paste0("ALQ #",x)),
                         ...)
-
     })
 
     ##combine everything to one RLum.Results object as this as what was written ... only
@@ -257,7 +238,6 @@ analyse_pIRIRSequence <- function(
       return(NULL)
     else
       return(results)
-
   }
 
 # General Integrity Checks ---------------------------------------------------
@@ -451,12 +431,10 @@ analyse_pIRIRSequence <- function(
     temp.IRSL.layout.vector <- c(temp.IRSL.layout.vector.first,
                                  temp.IRSL.layout.vector.middle,
                                  temp.IRSL.layout.vector.last)
-
   }else{
 
     temp.IRSL.layout.vector <- c(temp.IRSL.layout.vector.first,
                                  temp.IRSL.layout.vector.last)
-
   }
 
   ##get layout information
@@ -484,7 +462,6 @@ analyse_pIRIRSequence <- function(
 
   ## show the regions that have been allocated to each plot for debug
   #layout.show(nf)
-
   }
 
   ##(1) INFO PLOT
@@ -523,7 +500,6 @@ analyse_pIRIRSequence <- function(
       temp.signal.integral.max <- signal.integral.max
       temp.background.integral.min <- background.integral.min
       temp.background.integral.max <- background.integral.max
-
     }
 
     ##(c) call analysis sequence and plot

R/extract_IrradiationTimes.R
@@ -150,19 +150,11 @@ extract_IrradiationTimes <- function(
       .throw_warning("argument 'file.BINX' is not supported in self-call mode.")
     }
 
-    ##extend arguments
-      ##extent recordType
-      if(is(recordType, "list")){
-        recordType <-
-          rep(recordType, length = length(object))
+    ## expand input arguments
+    recordType <- .listify(recordType, length(object))
 
-      }else{
-        recordType <-
-          rep(list(recordType), length = length(object))
-      }
-
-      ##run function
-      results <- lapply(1:length(object), function(x) {
+    ## run function
+    results <- lapply(1:length(object), function(x) {
         extract_IrradiationTimes(
           object = object[[x]],
           recordType = recordType[[x]],

R/fit_EmissionSpectra.R
@@ -261,9 +261,8 @@ fit_EmissionSpectra <- function(
       mtext <- names(object)
 
     }else{
-      mtext <- as.list(rep(args_list$mtext, length(object)))
+      mtext <- .listify(args_list$mtext, length(object))
       args_list$mtext <- NULL
-
     }
 
     ##run over the list

commit afae3aed8008dfe91bb0715f8e686b6e6eb0c4c0

Don't listify the method.control argument when the object is a list().
As method.control is already given as a list and it's unlikely we want to
support applying different algorithmic parameters to different aliquots,
in the self-call we pass it directly, without further listifying it.

This is a regression that was introduced in f0d075275.

R/analyse_IRSAR.RF.R
@@ -443,7 +443,6 @@ analyse_IRSAR.RF<- function(
     RF_nat.lim <- .listify(RF_nat.lim, rep.length)
     RF_reg.lim <- .listify(RF_reg.lim, rep.length)
     method <- .listify(method, rep.length)
-    method.control <- .listify(method.control, rep.length)
     n.MC <- .listify(n.MC, rep.length)
 
     ##test_parameters
@@ -476,7 +475,7 @@ analyse_IRSAR.RF<- function(
         RF_nat.lim = RF_nat.lim[[x]],
         RF_reg.lim = RF_reg.lim[[x]],
         method = method[[x]],
-        method.control = method.control[[x]],
+        method.control = method.control,
         test_parameters = test_parameters[[x]],
         n.MC = n.MC[[x]],
         txtProgressBar = txtProgressBar,

tests/testthat/test_analyse_IRSAR.RF.R
@@ -164,6 +164,10 @@ test_that("test edge cases", {
 test_that("regression tests", {
   testthat::skip_on_cran()
 
+  ## issue 372
+  expect_silent(analyse_IRSAR.RF(list(IRSAR.RF.Data), plot = FALSE,
+                                 method.control = list(maxiter = 10)))
+
   ## issue 382
   expect_silent(analyse_IRSAR.RF(list(IRSAR.RF.Data), plot = FALSE))
 })