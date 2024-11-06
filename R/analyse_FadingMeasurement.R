3ed812fda46718c942b62b188f7b6468b9f3b448
Sebastian Kreutzer <RLumSK@users.noreply.github.com>
2024-11-06 12:12:32 +0100

    Update analyse_FadingMeasurement.R
    I made some changes that work, please check through whether we have to tackle more if it.

--- R/analyse_FadingMeasurement.R
+++ R/analyse_FadingMeasurement.R
@@ -660,7 +660,7 @@ analyse_FadingMeasurement <- function(
             xlim = plot_settings$xlim,
             log = plot_settings$log,
             legend.pos = "outside",
-            main = expression(bquote(paste(L[x], " - curves"))),
+            main = bquote(L[x] ~ "- curve"),
             mtext = plot_settings$mtext
           )
 
@@ -686,7 +686,7 @@ analyse_FadingMeasurement <- function(
             xlab = plot_settings$xlab,
             log = plot_settings$log,
             legend.pos = "outside",
-            main = expression(bquote(paste(T[x], " - curves"))),
+            main = bquote(T[x] ~ "- curve"),
             mtext = plot_settings$mtext
           )
 
