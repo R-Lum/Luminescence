663f791150a668fad30719fac9cc9af5a06b9d46
Marco Colombo <marco.colombo@uni-heidelberg.de>
2024-11-14 12:31:51 +0100

    Complete coverage for calc_CobbleDoseRate.R and add snapshot tests.

--- tests/testthat/test_calc_CobbleDoseRate.R
+++ tests/testthat/test_calc_CobbleDoseRate.R
@@ -1,12 +1,10 @@
-test_that("basic checks", {
+## load data
+data(ExampleData.CobbleData, envir = environment())
+df <- ExampleData.CobbleData
+
+test_that("input validation", {
   testthat::skip_on_cran()
 
-  ## simple run with example data
-  data("ExampleData.CobbleData", envir = environment())
-  expect_s4_class(calc_CobbleDoseRate(ExampleData.CobbleData), "RLum.Results")
-
-  ## break the function
-  df <- ExampleData.CobbleData
   df$Distance[[14]] <- 50000
   expect_error(calc_CobbleDoseRate("error"),
                "'input' should be of class 'data.frame'")
@@ -15,4 +13,34 @@ test_that("basic checks", {
 
   expect_error(calc_CobbleDoseRate(ExampleData.CobbleData, conversion = "error"),
                "'conversion' should be one of 'Guerinetal2011', 'Cresswelletal2018'")
+})
+
+test_that("snapshot tests", {
+  testthat::skip_on_cran()
+
+  snapshot.tolerance <- 1.5e-6
+
+  expect_snapshot_RLum(calc_CobbleDoseRate(df),
+                       tolerance = snapshot.tolerance)
+  expect_snapshot_RLum(calc_CobbleDoseRate(df, conv = "Cresswelletal2018"),
+                       tolerance = snapshot.tolerance)
+  expect_snapshot_RLum(calc_CobbleDoseRate(df, conv = "Liritzisetal2013"),
+                       tolerance = snapshot.tolerance)
+  expect_snapshot_RLum(calc_CobbleDoseRate(df, conv = "AdamiecAitken1998"),
+                       tolerance = snapshot.tolerance)
+})
+
+test_that("more coverage", {
+  testthat::skip_on_cran()
+
+  df$CobbleDiameter[1] <- 25
+  expect_s4_class(calc_CobbleDoseRate(df),
+                  "RLum.Results")
+
+  df$DistanceError[1] <- 0
+  df$CobbleDiameter[1] <- 2
+  df$Thickness <- 10
+  df <- df[1:9, ]
+  expect_s4_class(calc_CobbleDoseRate(df),
+                  "RLum.Results")
 })
