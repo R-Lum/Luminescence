R-Lum/Luminescence

commit 84235c14737c663872194c7a61d172101f388f2d

Improve validation against an empty matrix.

R/calc_FastRatio.R
@@ -126,7 +126,8 @@ calc_FastRatio <- function(object,
   .set_function_name("calc_FastRatio")
   on.exit(.unset_function_name(), add = TRUE)
 
-  ## Input verification -----------------------------------------------------
+  ## Integrity checks - -----------------------------------------------------
+
   .validate_class(object, c("RLum.Analysis", "RLum.Results", "RLum.Data.Curve",
                             "data.frame", "matrix"))
   .validate_not_empty(object)
@@ -158,6 +159,10 @@ calc_FastRatio <- function(object,
 
   if (inherits(object, "RLum.Results"))
     object <- get_RLum(object, "data")
+
+  if (all(is.na(object))) {
+    .throw_error("'object' contains no data")
+  }
 
   if (!inherits(object, "list"))
     object <-list(object)

tests/testthat/test_calc_FastRatio.R
@@ -11,6 +11,8 @@ test_that("input validation", {
                "'object' cannot be an empty data.frame")
   expect_error(calc_FastRatio(matrix(nrow = 1, ncol = 0)),
                "'object' cannot be an empty matrix")
+  expect_error(calc_FastRatio(matrix()),
+               "'object' contains no data")
 
   expect_error(calc_FastRatio(obj, Ch_L1 = NULL),
                "'Ch_L1' should be a positive integer scalar")

R/convert_Wavelength2Energy.R
@@ -195,6 +195,11 @@ convert_Wavelength2Energy <- function(
     return(object)
 
   }else if(inherits(object, "matrix") || inherits(object, "data.frame")){
+
+    if (ncol(object) < 2) {
+      .throw_error("'object' should have at least two columns")
+    }
+
     temp <- as.matrix(object[,2:ncol(object)])
 
     ##set rownames

R/fit_EmissionSpectra.R
@@ -294,6 +294,9 @@ fit_EmissionSpectra <- function(
 
   # Start main core -----------------------------------------------------------------------------
 
+  if (ncol(object) < 2) {
+    .throw_error("'object' should have at least two columns")
+  }
   input_scale <- .validate_args(input_scale, c("wavelength", "energy"),
                                 null.ok = TRUE)
 

R/fit_OSLLifeTimes.R
@@ -272,14 +272,16 @@ if(inherits(object, "list") || inherits(object, "RLum.Analysis")){
      .throw_error("recordType '", object@recordType,
                   "' not supported for input object")
 
-    df <- as.data.frame(object@data)
-
-  }else if(inherits(object, "data.frame")){
-    df <- object[,1:2]
+    object <- as.data.frame(object@data)
 
   } else if(inherits(object, "matrix")){
-    df <- as.data.frame(object[,1:2])
+    object <- as.data.frame(object)
   }
+
+  if (ncol(object) < 2) {
+    .throw_error("'object' should have at least two columns")
+  }
+  df <- object[, 1:2]
 
   ##remove NA values, whatever it is worth for
   if (anyNA(df)) {

R/fit_SurfaceExposure.R
@@ -283,6 +283,9 @@ fit_SurfaceExposure <- function(
 
   # Exit if data type is invalid
   .validate_class(data, "data.frame")
+  if (ncol(data) < 2) {
+    .throw_error("'data' should have at least two columns")
+  }
 
   # Check which parameters have been provided
   if (!is.null(age) && anyNA(age)) age <- NULL
@@ -591,5 +594,3 @@ fit_SurfaceExposure <- function(
   ## EXIT ----
   return(results)
 }
-
-

tests/testthat/test_convert_Wavelength2Energy.R
@@ -5,6 +5,8 @@ test_that("input validation", {
                "'object' should be of class 'RLum.Data.Spectrum', 'data.frame'")
   expect_error(convert_Wavelength2Energy(data.frame()),
                "'object' cannot be an empty data.frame")
+  expect_error(convert_Wavelength2Energy(matrix()),
+               "'object' should have at least two columns")
   expect_error(convert_Wavelength2Energy(set_RLum("RLum.Data.Spectrum")),
                "'object' contains no data")
 })

tests/testthat/test_fit_EmissionSpectra.R
@@ -12,6 +12,8 @@ test_that("input validation", {
   expect_error(fit_EmissionSpectra(list(TL.Spectrum, "fail")),
                "[fit_EmissionSpectra()] List elements of different class detected",
                fixed = TRUE)
+  expect_error(fit_EmissionSpectra(matrix()),
+               "'object' should have at least two columns")
 
   ## input scale
   expect_error(fit_EmissionSpectra(TL.Spectrum, input_scale = "error"),

commit 864bde7a2f92d598111858db98908cf90be57ea4

Better check against empty matrix in calc_FastRatio().
This fixes this warning that is raised when the input to the function
is an RLum.Data.Curve object:

   is.na() applied to non-(list or vector) of type 'S4'

This is a regression from 84235c14.

R/calc_FastRatio.R
@@ -160,8 +160,8 @@ calc_FastRatio <- function(object,
   if (inherits(object, "RLum.Results"))
     object <- get_RLum(object, "data")
 
-  if (all(is.na(object))) {
-    .throw_error("'object' contains no data")
+  if (ncol(object) < 2) {
+    .throw_error("'object' should have at least two columns")
   }
 
   if (!inherits(object, "list"))

tests/testthat/test_calc_FastRatio.R
@@ -12,7 +12,7 @@ test_that("input validation", {
   expect_error(calc_FastRatio(matrix(nrow = 1, ncol = 0)),
                "'object' cannot be an empty matrix")
   expect_error(calc_FastRatio(matrix()),
-               "'object' contains no data")
+               "'object' should have at least two columns")
 
   expect_error(calc_FastRatio(obj, Ch_L1 = NULL),
                "'Ch_L1' should be a positive integer scalar")
@@ -98,6 +98,11 @@ test_that("check functionality", {
                  "L3 contains more counts (566) than L2 (562)",
                  fixed = TRUE)
   })
+
+  ## RLum.Data.Curve object
+  curve <- set_RLum("RLum.Data.Curve", data = as.matrix(ExampleData.CW_OSL_Curve))
+  expect_s4_class(calc_FastRatio(curve, plot = FALSE, verbose = FALSE),
+                  "RLum.Results")
 
   results <- get_RLum(temp)
   expect_equal(round(results$fast.ratio, digits = 3), 405.122)