d7967cda2eb1d6cc102b14833a1f313f31758c6d
RLumSK <sebastian.kreutzer@uni-heidelberg.de>
2024-12-26 20:37:29 +0100

    Correct skip to 4.4.0

--- tests/testthat/test_plot_AbanicoPlot.R
+++ tests/testthat/test_plot_AbanicoPlot.R
@@ -307,7 +307,7 @@ test_that("more coverage", {
 test_that("Test graphical snapshot", {
   testthat::skip_on_cran()
   testthat::skip_if_not_installed("vdiffr")
-  testthat::skip_if_not(getRversion() >= "4.3.0")
+  testthat::skip_if_not(getRversion() >= "4.4.0")
 
   SW({
     vdiffr::expect_doppelganger(
