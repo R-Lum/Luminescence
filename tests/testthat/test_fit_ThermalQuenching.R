39c2f6d8bf5285cf84d66a3226cb17c7f5c3db9d
Marco Colombo <marco.colombo@uni-heidelberg.de>
2025-01-22 15:40:16 +0100

    Small cleanup in test.
    [skip ci]

--- tests/testthat/test_fit_ThermalQuenching.R
+++ tests/testthat/test_fit_ThermalQuenching.R
@@ -3,19 +3,14 @@ data <- data.frame(
   T = c(25, 40, 50, 60, 70, 80, 90, 100, 110),
   V = c(0.06, 0.058, 0.052, 0.051, 0.041, 0.034, 0.035, 0.033, 0.032),
   V_X = c(0.012, 0.009, 0.008, 0.008, 0.007, 0.006, 0.005, 0.005, 0.004))
-
-data_list <- list(data, data)
 data_NA <- data
 data_NA[1,] <- NA
 
 test_that("input validation", {
   testthat::skip_on_cran()
 
-  ##trigger errors
   expect_error(fit_ThermalQuenching(data = "test"),
                "'data' should be of class 'data.frame' or a 'list' of such objects")
-
-  ##simple run with error
   expect_error(fit_ThermalQuenching(
     data = data[,1:2],
     n.MC = NULL),
@@ -50,8 +45,8 @@ test_that("check functionality", {
     n.MC = NULL)),
     "Error: Fitting failed, NULL returned")
 
-  # ##switch off weights
   SW({
+  ## switch off weights
   expect_s4_class(fit_ThermalQuenching(
     data = data,
     method_control = list(weights = NULL),
@@ -59,7 +54,7 @@ test_that("check functionality", {
 
   ##simple list run
   expect_s4_class(fit_ThermalQuenching(
-    data = data_list,
+    data = list(data, data),
     n.MC = NULL), class = "RLum.Results")
 
   ##simple run without plot etc
