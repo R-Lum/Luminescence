64d9671786a4638b75585ff457c51fe3abac1df4
RLumSK <sebastian.kreutzer@uni-heidelberg.de>
2025-02-07 18:18:35 +0100

    plot_MoranScatterplot()" + pl code polish + fx move parameters to ...

--- R/plot_MoranScatterplot.R
+++ R/plot_MoranScatterplot.R
@@ -14,32 +14,24 @@
 #' provided for [calc_MoransI]). If `NULL` (default), this is constructed
 #' automatically by the internal function `.get_Neighbours`.
 #'
-#' @param show_legend [logical] (*with default*): whether a legend explaining
-#' the different lines should be plotted (defaults to `TRUE`).
-#'
 #' @param str_y_def [character] (*with default*) Calculation of y position. Defaults to `"mean_neighbours"`
 #' which is the plain mean of all neighbour values and the most illustrative. The other option is `"weighted_sum"`,
 #' which means the sum of the border weights times the neighbour values, which is actually closer
 #' to the way Moran's I is by default calculated in this package.
 #'
-#' @param pch [character], [integer] (*with default*): Accepts options
-#' `"show_location_ids"` (plots grain location id's), `"show_n_neighbours"`
-#' (indicates numbers of neighbours) and the normal base plot `pch` options.
-#' @param cex FIXME(mcol)
-#' @param xlab FIXME(mcol)
-#' @param ylab FIXME(mcol)
-#' @param log FIXME(mcol)
-#'
 #' @param ... Other parameters to be forwarded to the base R plot functions.
+#' `legend` (`TRUE/FALSE`) to entable/disable the legend.
 #' Note that `xlab` (x axis label), `ylab` (y axis label) and `cex` (scaling
 #' value) are given default values. Because of sometimes large value
 #' differences, `log = "x"`, `log = "y"` and `log = "xy"`are supported.
 #' In case of negative values and logarithmic plotting, values are increased
 #' so the smallest value to plot is 1. Summary elements such as means, least
 #' square line etc. will still be based on the linear case.
+#' `pch` accepts options "show_location_ids"` (plots grain location id's), `"show_n_neighbours"`
+#' (indicates numbers of neighbours) and the normal base plot `pch` options.
 #'
 #' @details Note that this function plots on the y-axis the mean of the neighbours, while the function
-#' `calc_MoransI()` by default will for its global calculation weight every border the same. So, grain locations
+#' [calc_MoransI] by default will for its global calculation weight every border the same. So, grain locations
 #' with 1, 2 or 3 neighbours will appear higher on the y-axis than their influence on Moran's I justify -- apart
 #' from scaling, this explains a part of the differences of Moran's scatter plots between different packages.
 #' Also note that island' grain locations (=those not bordering other grains) are left out of these plots but
@@ -58,27 +50,30 @@
 #'
 #' @md
 #' @export
-plot_MoranScatterplot <- function(object,
-                                  df_neighbours = NULL,
-                                  show_legend = TRUE,
-                                  str_y_def = "mean_neighbours", # , mean_neighbours, weighted_sum
-                                  pch  = 20, #"show_location_ids", "show_n_neighbours", pch-value
-                                  cex  = 0.8,
-                                  xlab = "Value central grain",
-                                  ylab = ifelse(str_y_def == "weighted_sum",
-                                                yes = "Weighted sum neighbouring grains",
-                                                no = "Plain mean neighbour grains"),
-                                  log = "",
-                                  ...
+plot_MoranScatterplot <- function(
+    object,
+    df_neighbours = NULL,
+    str_y_def = "mean_neighbours", # , mean_neighbours, weighted_sum
+    ...
 ) {
   .set_function_name("plot_MoranScatterplot")
   on.exit(.unset_function_name(), add = TRUE)
 
   ## Validate input arguments; set variables  -----------------------
-
   .validate_class(object, c("RLum.Results", "numeric", "integer"))
   ## To add:
   #  - should contain a numerical vector of length 100
+
+  ## get ... arguments
+  plot_settings <- modifyList(
+    x = list(
+      legend = TRUE,
+      pch = 20,
+      cex = 0.8,
+      xlab = "Value central grain",
+      ylab = ifelse(str_y_def == "weighted_sum", "Weighted sum neighbouring grains", "Plain mean neighbour grains"),
+      log = ""),
+    val = list(...))
 
   if (is.null(df_neighbours)) {
     df_neighbours <- .get_Neighbours(object)
@@ -88,40 +83,32 @@ plot_MoranScatterplot <- function(object,
       .throw_error("'df_neighbours' should be a data frame with 3 columns")
   }
 
-  .validate_class(show_legend, "logical")
   .validate_args(str_y_def, c("mean_neighbours", "weighted_sum"))
 
   if(is.numeric(object))
-  {
     vn_values <- object
-  } else
-  {
+  else
     vn_values <- get_RLum(object)
-  }
 
-
-  vs_points_appearance <- pch
+  vs_points_appearance <- plot_settings$pch
 
   show_location_ids <- FALSE
   show_n_neighbours <- FALSE
-  if(pch == "show_location_ids")
-  {
+  if(plot_settings$pch == "show_location_ids")
     show_location_ids <- TRUE
-  }
-  else if (pch == "show_n_neighbours") {
+  else if (plot_settings$pch == "show_n_neighbours")
     show_n_neighbours <- TRUE
-  }
 
-  vs_log <- log  # because we need the function "log" later
+  vs_log <- plot_settings$log  # because we need the function "log" later
 
-  df_moran_plot <- data.frame(x = vn_values,
-                              y = NA,
-                              grain_id = NA,
-                              n_neighbours = NA)
+  df_moran_plot <- data.frame(
+    x = vn_values,
+    y = NA,
+    grain_id = NA,
+    n_neighbours = NA)
 
   vn_locations <- unique(c(df_neighbours$location, df_neighbours$neighbour))
-  for(i in 1:length(vn_locations) )
-  {
+  for(i in 1:length(vn_locations) ) {
     n_location <- vn_locations[i]
     ## Find the neighbouring locations according to df_neighbour
     ## For a given n_location, in which row of df_neighbour are neighbouring locations provided?
@@ -147,15 +134,14 @@ plot_MoranScatterplot <- function(object,
     )
 
 
-    if(str_y_def == "mean_neighbours")
-    {
+    if(str_y_def == "mean_neighbours") {
       n_mean_of_neighbours <- mean(
         df_moran_plot$x[df_neighbour_and_weights$adj_pos]*
           df_neighbour_and_weights$weight, na.rm = TRUE)
 
       df_moran_plot$y[n_location]  <- n_mean_of_neighbours
-    }
-    else if(str_y_def == "weighted_sum") {
+
+    } else if(str_y_def == "weighted_sum") {
       n_weighted_sum_neighbours <- sum(
         df_moran_plot$x[df_neighbour_and_weights$adj_pos]*
           df_neighbour_and_weights$weight, na.rm = TRUE) ## NOG AANPASSEN
@@ -180,11 +166,9 @@ plot_MoranScatterplot <- function(object,
 
   ## If we want a log plot, make sure that all relevant values are 1 or more (per axis)
   if (grepl("x", vs_log, fixed = TRUE) && min(df_moran_plot$x) < 1) {
-    if (min(df_moran_plot$x) < 0)
-    {
+    if (min(df_moran_plot$x) < 0) {
       df_moran_plot$x <- df_moran_plot$x - min(df_moran_plot$x) + 1
-    } else
-    {
+    } else {
       df_moran_plot$x <- df_moran_plot$x + (1 - min(df_moran_plot$x))
     }
     .throw_warning("x-axis values rescaled because of log transform ",
@@ -192,11 +176,10 @@ plot_MoranScatterplot <- function(object,
   }
 
   if (grepl("y", vs_log, fixed = TRUE) && min(df_moran_plot$y) < 1) {
-    if (min(df_moran_plot$y) < 0)
-    {
+    if (min(df_moran_plot$y) < 0) {
       df_moran_plot$y <- df_moran_plot$y - min(df_moran_plot$y) + 1
-    } else
-    {
+
+    } else {
       df_moran_plot$y <- df_moran_plot$y + (1 - min(df_moran_plot$y))
     }
     .throw_warning("y-axis values rescaled because of log transform ",
@@ -208,29 +191,28 @@ plot_MoranScatterplot <- function(object,
                 vs_points_appearance == "show_n_neighbours") {
     NA # Numbers will be added later
   } else {
-    pch
+    plot_settings$pch
   }
 
   plot(df_moran_plot[,1:2],
        pch = vs_pch,
        log = vs_log,
-       xlab = xlab,
-       ylab = ylab,
-       cex = cex,
+       xlab = plot_settings$xlab,
+       ylab = plot_settings$ylab,
+       cex = plot_settings$cex,
        ...
   )
 
-  if(vs_points_appearance == "show_location_ids")
-  {
+  if(vs_points_appearance == "show_location_ids") {
     text(x = df_moran_plot[,1:2],
          labels = df_moran_plot$grain_id,
-         cex = cex)
+         cex = plot_settings$cex)
   }
-  if(vs_points_appearance == "show_n_neighbours")
-  {
+
+  if(vs_points_appearance == "show_n_neighbours") {
     text(x = df_moran_plot[,1:2],
          labels = df_moran_plot$n_neighbours,
-         cex = cex)
+         cex = plot_settings$cex)
   }
 
 
@@ -247,30 +229,27 @@ plot_MoranScatterplot <- function(object,
   lm_moransI <- lm(y ~  x, data = df_moran_plot)
   abline(a = coef(lm_moransI)[1], b = coef(lm_moransI)[2], col = "orange", untf=TRUE)
 
-  if (show_legend) {
+  if (plot_settings$legend[1]) {
     vs_legend <- c("means",  "least square")
     vs_cols   <- c("black",  "orange")
     vs_lty    <- c("dotted", "solid")
     vs_pch    <- c(NA,       NA)
 
-    if(str_y_def == "mean_neighbours")
-    {
+    if(str_y_def == "mean_neighbours") {
       vs_legend <- c(vs_legend, "1:1 line")
       vs_cols   <- c(vs_cols,   "black")
       vs_lty    <- c(vs_lty,    "dotdash")
       vs_pch    <- c(vs_pch,    NA)
     }
 
-    if(vs_points_appearance == "show_location_ids")
-    {
+    if(vs_points_appearance == "show_location_ids") {
       vs_legend <- c(vs_legend, "location ids")
       vs_cols   <- c(vs_cols,   "black")
       vs_lty    <- c(vs_lty,    NA)
       vs_pch    <- c(vs_pch,    "#")
     }
 
-    if(vs_points_appearance == "show_n_neighbours")
-    {
+    if(vs_points_appearance == "show_n_neighbours") {
       vs_legend <- c(vs_legend, "numb. of neighb.")
       vs_cols   <- c(vs_cols,   "black")
       vs_lty    <- c(vs_lty,    NA)
