R-Lum/Luminescence

commit 9f5a918d1992a2818cc1790cb094f3a9e21234d6

Increase coverage in plot_RLum.Analysis.R.

R/plot_RLum.Analysis.R
@@ -181,8 +181,14 @@ plot_RLum.Analysis <- function(
   ##try to find optimal parameters, this is however, a little bit stupid, but
   ##better than without any presetting
   .validate_class(combine, "logical")
-  if(combine)
+  if (combine) {
+    sapply(object@records, function(x) {
+      if (!inherits(x, "RLum.Data.Curve")) {
+        .throw_error("'combine' is valid only for 'RLum.Data.Curve' objects")
+      }
+    })
     n.plots <- length(unique(as.character(structure_RLum(object)$recordType)))
+  }
   else
     n.plots <- length_RLum(object)
 
@@ -411,34 +417,21 @@ plot_RLum.Analysis <- function(
     ##(2) NORMAL (combine == TRUE)
     ##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     ##(1) check RLum objects in the set
-    object.list <- object@records
-
-    sapply(object.list, function(o){
-      if(!inherits(o, "RLum.Data.Curve")){
-        .throw_error("Using 'combine' is limited to 'RLum.Data.Curve' objects")
-      }
-    })
 
     ##account for different curve types, combine similar
     temp.object.structure  <- structure_RLum(object)
     temp.recordType <- as.character(unique(temp.object.structure$recordType))
 
     ##change graphic settings
+    par.default <- par()[c("cex", "mfrow")]
     if (!plot_singlePanels) {
-      par.default <- par()[c("cex", "mfrow")]
-
       if(!missing(ncols) & !missing(nrows)){
         par(mfrow = c(nrows, ncols))
       }
-
-      ##this 2nd par request is needed as setting mfrow resets the par settings ... this might
-      ##not be wanted
-      par(cex = plot.settings$cex[1])
-
-    }else{
-      par.default <- par()[c("cex")]
-      par(cex = plot.settings$cex)
     }
+    ## this 2nd par request is needed as setting mfrow resets the par
+    ## settings ... this might not be wanted
+    par(cex = plot.settings$cex)
 
     ##expand plot settings list
     ##expand list

tests/testthat/test_plot_RLum.Analysis.R
@@ -1,3 +1,4 @@
+## load data
 data(ExampleData.BINfileData, envir = environment())
 data(ExampleData.XSYG, envir = environment())
 
@@ -25,7 +26,7 @@ test_that("input validation", {
       set_RLum("RLum.Analysis", records = list(c1@records[[1]],
                                                set_RLum("RLum.Data.Image"))),
       combine = TRUE),
-      "Only 'RLum.Data.Curve' objects are allowed")
+      "'combine' is valid only for 'RLum.Data.Curve' objects")
 
   ## this generates multiple warnings
   warnings <- capture_warnings(plot_RLum.Analysis(c1, col = 2,

commit 1ea44d38ecda1b69994c2cb5abff9f72650251db

Fix regression in plot generated by analyse_FadingMeasurement().
The issue seems to be that setting mfrow resets the par settings (as the
pre-existing comment stated) even in the case when mfrow is not actually
changed. Therefore, we cannot have a shared par.default for the two cases
of plot_singlePanels, but each must define its own.

This fixes a regression introduced in 9f5a918.

R/plot_RLum.Analysis.R
@@ -427,11 +427,13 @@ plot_RLum.Analysis <- function(
     temp.recordType <- as.character(unique(temp.object.structure$recordType))
 
     ##change graphic settings
-    par.default <- par()[c("cex", "mfrow")]
     if (!plot_singlePanels) {
+      par.default <- par()[c("cex", "mfrow")]
       if(!missing(ncols) & !missing(nrows)){
         par(mfrow = c(nrows, ncols))
       }
+    } else {
+      par.default <- par()["cex"]
     }
     ## this 2nd par request is needed as setting mfrow resets the par
     ## settings ... this might not be wanted