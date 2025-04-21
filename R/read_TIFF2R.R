7d6424155fb1b440936838a8c6bc3116f17f1e17
RLumSK <sebastian.kreutzer@uni-heidelberg.de>
2025-04-21 15:10:23 +0200

    Extend read_TIFF2R(): + ad support for self-call + ad new argument for merging data + ad tests + ad NEWS + up docu and NEWS + up build number

--- R/read_TIFF2R.R
+++ R/read_TIFF2R.R
@@ -3,10 +3,14 @@
 #'@description Simple wrapper around [tiff::readTIFF] to import TIFF images
 #'and TIFF image stacks to be further processed within the package `'Luminescence'`
 #'
-#'@param file [character] (**required**): file name
+#'@param file [character] (**required**): file name, can be of type [list] for automated
+#'processing of many images
 #'
-#' @param verbose [logical] (*with default*): enable/disable output to the
-#' terminal.
+#'@param merge2stack [logical] (*with default*): if `file` is a [list] it merges
+#'the individual images to one image stack
+#'
+#'@param verbose [logical] (*with default*): enable/disable output to the
+#'terminal
 #'
 #'@param ... not in use, for compatibility reasons only
 #'
@@ -14,7 +18,7 @@
 #'
 #'@author Sebastian Kreutzer, Institute of Geography, Heidelberg University (Germany)
 #'
-#'@section Function version: 0.1.2
+#'@section Function version: 0.2.0
 #'
 #'@seealso [tiff::readTIFF], [RLum.Data.Image-class]
 #'
@@ -22,24 +26,42 @@
 #'
 #'@examples
 #'
-#'\dontrun{
-#'file <- file.choose()
-#'image <- read_TIFF2R(file)
+#'## use system file
+#'file <- system.file("extdata", "TIFFfile.tif", package = "Luminescence")
 #'
-#'}
+#'## import image
+#'image <- read_TIFF2R(file)
 #'
 #'@md
 #'@export
 read_TIFF2R <- function(
   file,
+  merge2stack = FALSE,
   verbose = TRUE,
   ...
 ) {
   .set_function_name("read_TIFF2R")
   on.exit(.unset_function_name(), add = TRUE)
 
-  ## Integrity checks -------------------------------------------------------
+  # Self call ---------------------------------------------------------------
+  if(inherits(file, "list") || length(file) > 1) {
+    .validate_class(merge2stack, "logical")
 
+    ## read list
+    tmp <- lapply(file, read_TIFF2R, verbose = verbose)
+
+    if(merge2stack[1]) {
+      tmp <- lapply(tmp, function(x) x@data)
+      tmp <- array(unlist(tmp), dim = c(nrow(tmp[[1]]), ncol(tmp[[1]]), length(tmp)))
+
+      return(set_RLum(class = "RLum.Data.Image", data = tmp))
+    }
+
+    ## return the results
+    return(tmp)
+  }
+
+  ## Integrity checks -------------------------------------------------------
   .validate_class(file, "character")
   .validate_length(file, 1)
   .require_suggested_package("tiff", "Importing TIFF files")
@@ -65,3 +87,4 @@ read_TIFF2R <- function(
 # Return ------------------------------------------------------------------
   set_RLum(class = "RLum.Data.Image", data = temp@data)
 }
+
