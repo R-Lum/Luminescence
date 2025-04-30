R-Lum/Luminescence

commit 3e945706687929dc967e51cddf909f491ead0c49

Consolidate code in plot_AbanicoPlot().
The change in the graphical snapshot is only due to the fact that previously
the label was

   "n = 62 | in 2 sigma = 41.9 % "

and now is

   "n = 62 | in 2 sigma = 41.9 %"

R/plot_AbanicoPlot.R
@@ -1208,7 +1208,7 @@ plot_AbanicoPlot <- function(
   colnames(De.stats) <- c("n",
                           "mean",
                           "median",
-                          "kde.max",
+                          "kdemax",
                           "sd.abs",
                           "sd.rel",
                           "se.abs",
@@ -1255,193 +1255,65 @@ plot_AbanicoPlot <- function(
     }
   }
 
-  label.text = list(NA)
+  ## helper to generate an element of the statistical summary
+  .summary_line <- function(keyword, summary, val, label = keyword,
+                            percent = FALSE, sep = FALSE, digits = 2) {
+    ifelse(keyword %in% summary,
+           paste0(label, " = ", round(val, digits),
+                  if (percent) " %" else NULL, if (sep) " | " else "\n"),
+           "")
+  }
 
-  if(summary.pos[1] != "sub") {
-    n.rows <- length(summary)
+  ## initialize list with a dummy element, it will be removed afterwards
+  label.text <- list(NA)
 
-    for(i in 1:length(data)) {
-      stops <- paste(rep("\n", (i - 1) * n.rows), collapse = "")
+  is.sub <- summary.pos[1] == "sub"
+  stops <- NULL
+  for (i in 1:length(data)) {
+    if (!is.sub)
+      stops <- paste(rep("\n", (i - 1) * length(summary)), collapse = "")
 
-      summary.text <- character(0)
-
-      for(j in 1:length(summary)) {
-        summary.text <- c(summary.text,
-                          paste(
-                            "",
-                            ifelse("n" %in% summary[j] == TRUE,
-                                   paste("n = ",
-                                         De.stats[i,1],
-                                         "\n",
-                                         sep = ""),
-                                   ""),
-                            ifelse("mean" %in% summary[j] == TRUE,
-                                   paste("mean = ",
-                                         round(De.stats[i,2], 2),
-                                         "\n",
-                                         sep = ""),
-                                   ""),
-                            ifelse("median" %in% summary[j] == TRUE,
-                                   paste("median = ",
-                                         round(De.stats[i,3], 2),
-                                         "\n",
-                                         sep = ""),
-                                   ""),
-                            ifelse("kde.max" %in% summary[j] == TRUE,
-                                   paste("kdemax = ",
-                                         round(De.stats[i,4], 2),
-                                         " \n ",
-                                         sep = ""),
-                                   ""),
-                            ifelse("sd.abs" %in% summary[j] == TRUE,
-                                   paste("abs. sd = ",
-                                         round(De.stats[i,5], 2),
-                                         "\n",
-                                         sep = ""),
-                                   ""),
-                            ifelse("sd.rel" %in% summary[j] == TRUE,
-                                   paste("rel. sd = ",
-                                         round(De.stats[i,6], 2), " %",
-                                         "\n",
-                                         sep = ""),
-                                   ""),
-                            ifelse("se.abs" %in% summary[j] == TRUE,
-                                   paste("se = ",
-                                         round(De.stats[i,7], 2),
-                                         "\n",
-                                         sep = ""),
-                                   ""),
-                            ifelse("se.rel" %in% summary[j] == TRUE,
-                                   paste("rel. se = ",
-                                         round(De.stats[i,8], 2), " %",
-                                         "\n",
-                                         sep = ""),
-                                   ""),
-                            ifelse("skewness" %in% summary[j] == TRUE,
-                                   paste("skewness = ",
-                                         round(De.stats[i,11], 2),
-                                         "\n",
-                                         sep = ""),
-                                   ""),
-                            ifelse("kurtosis" %in% summary[j] == TRUE,
-                                   paste("kurtosis = ",
-                                         round(De.stats[i,12], 2),
-                                         "\n",
-                                         sep = ""),
-                                   ""),
-                            ifelse("in.2s" %in% summary[j] == TRUE,
-                                   paste("in 2 sigma = ",
-                                         round(sum(data[[i]][,7] > -2 &
-                                                     data[[i]][,7] < 2) /
-                                                 nrow(data[[i]]) * 100 , 1),
-                                         " %\n",
-                                         sep = ""),
-                                   ""),
-                            sep = ""))
-      }
-
-      summary.text <- paste(summary.text, collapse = "")
-
-      label.text[[length(label.text) + 1]] <- paste(stops,
-                                                    summary.text,
-                                                    stops,
-                                                    sep = "")
+    summary.text <- character(0)
+    for (j in 1:length(summary)) {
+      summary.text <-
+        c(summary.text,
+          .summary_line("n", summary[j], De.stats[i, 1], sep = is.sub),
+          .summary_line("mean", summary[j], De.stats[i, 2], sep = is.sub),
+          .summary_line("median", summary[j], De.stats[i, 3], sep = is.sub),
+          .summary_line("kdemax", summary[j], De.stats[i, 4], sep = is.sub),
+          .summary_line("sdabs", summary[j], De.stats[i, 5], sep = is.sub,
+                        label = "abs. sd"),
+          .summary_line("sdrel", summary[j], De.stats[i, 6], sep = is.sub,
+                        label = "rel. sd"),
+          .summary_line("se.abs", summary[j], De.stats[i, 7], sep = is.sub,
+                        label = "se"),
+          .summary_line("se.rel", summary[j], De.stats[i, 8], sep = is.sub,
+                        label = "rel. se", percent = TRUE),
+          .summary_line("skewness", summary[j], De.stats[i, 11], sep = is.sub),
+          .summary_line("kurtosis", summary[j], De.stats[i, 12], sep = is.sub),
+          .summary_line("in.2s", summary[j],
+                        sum(data[[i]][,7] > -2 & data[[i]][,7] < 2) /
+                        nrow(data[[i]]) * 100, sep = is.sub,
+                        label = "in 2 sigma", percent = TRUE, digits = 1))
     }
-  } else {
-    for(i in 1:length(data)) {
-      summary.text <- character(0)
-
-      for(j in 1:length(summary)) {
-        summary.text <- c(summary.text,
-                          ifelse("n" %in% summary[j] == TRUE,
-                                 paste("n = ",
-                                       De.stats[i,1],
-                                       " | ",
-                                       sep = ""),
-                                 ""),
-                          ifelse("mean" %in% summary[j] == TRUE,
-                                 paste("mean = ",
-                                       round(De.stats[i,2], 2),
-                                       " | ",
-                                       sep = ""),
-                                 ""),
-                          ifelse("median" %in% summary[j] == TRUE,
-                                 paste("median = ",
-                                       round(De.stats[i,3], 2),
-                                       " | ",
-                                       sep = ""),
-                                 ""),
-                          ifelse("kde.max" %in% summary[j] == TRUE,
-                                 paste("kdemax = ",
-                                       round(De.stats[i,4], 2),
-                                       " | ",
-                                       sep = ""),
-                                 ""),
-                          ifelse("sd.abs" %in% summary[j] == TRUE,
-                                 paste("abs. sd = ",
-                                       round(De.stats[i,5], 2),
-                                       " | ",
-                                       sep = ""),
-                                 ""),
-                          ifelse("sd.rel" %in% summary[j] == TRUE,
-                                 paste("rel. sd = ",
-                                       round(De.stats[i,6], 2), " %",
-                                       " | ",
-                                       sep = ""),
-                                 ""),
-                          ifelse("se.abs" %in% summary[j] == TRUE,
-                                 paste("abs. se = ",
-                                       round(De.stats[i,7], 2),
-                                       " | ",
-                                       sep = ""),
-                                 ""),
-                          ifelse("se.rel" %in% summary[j] == TRUE,
-                                 paste("rel. se = ",
-                                       round(De.stats[i,8], 2), " %",
-                                       " | ",
-                                       sep = ""),
-                                 ""),
-                          ifelse("skewness" %in% summary[j] == TRUE,
-                                 paste("skewness = ",
-                                       round(De.stats[i,11], 2),
-                                       " | ",
-                                       sep = ""),
-                                 ""),
-                          ifelse("kurtosis" %in% summary[j] == TRUE,
-                                 paste("kurtosis = ",
-                                       round(De.stats[i,12], 2),
-                                       " | ",
-                                       sep = ""),
-                                 ""),
-                          ifelse("in.2s" %in% summary[j] == TRUE,
-                                 paste("in 2 sigma = ",
-                                       round(sum(data[[i]][,7] > -2 &
-                                                   data[[i]][,7] < 2) /
-                                               nrow(data[[i]]) * 100 , 1),
-                                       " % |  ",
-                                       sep = ""),
-                                 "")
-        )
-      }
-
-      summary.text <- paste(summary.text, collapse = "")
-
-      label.text[[length(label.text) + 1]]  <- paste(
-        "  ",
-        summary.text,
-        sep = "")
-    }
-
-    ## remove outer vertical lines from string
-    for(i in 2:length(label.text)) {
-      label.text[[i]] <- substr(x = label.text[[i]],
-                                start = 3,
-                                stop = nchar(label.text[[i]]) - 3)
-    }
+    label.text[[length(label.text) + 1]] <- paste0(
+        if (is.sub) "" else stops,
+        paste(summary.text, collapse = ""),
+        stops)
   }
 
   ## remove dummy list element
   label.text[[1]] <- NULL
+
+  ## remove outer vertical lines from string1
+  if (is.sub) {
+    for (i in seq_along(label.text)) {
+      label.text[[i]] <- substr(x = label.text[[i]],
+                                start = 1,
+                                stop = nchar(label.text[[i]]) - 3)
+    }
+  }
+
   if(!rotate[1]) {
     ## convert keywords into summary placement coordinates
     coords <- .get_keyword_coordinates(summary.pos, limits.x, limits.y)

tests/testthat/_snaps/plot_AbanicoPlot/abanico-expected.svg
@@ -177,6 +177,6 @@
 <text x='566.84' y='555.12' text-anchor='middle' style='font-size: 12.00px; font-family: sans;' textLength='93.38px' lengthAdjust='spacingAndGlyphs'>Density (bw 0.15)</text>
 <polyline points='514.49,368.10 514.49,82.44 ' style='stroke-width: 0.75;' />
 <polygon points='64.80,189.23 501.39,82.44 619.20,82.44 619.20,368.10 501.39,368.10 ' style='stroke-width: 0.60; fill: none;' />
-<text x='342.00' y='47.52' text-anchor='middle' style='font-size: 10.00px; font-family: sans;' textLength='127.13px' lengthAdjust='spacingAndGlyphs'>n = 62 | in 2 sigma = 41.9 % </text>
+<text x='342.00' y='47.52' text-anchor='middle' style='font-size: 10.00px; font-family: sans;' textLength='124.36px' lengthAdjust='spacingAndGlyphs'>n = 62 | in 2 sigma = 41.9 %</text>
 </g>
 </svg>

commit 27db9c6e4bdd492d6c8a15ab6d6489207e0a5277

Fix summary labels in plot_AbanicoPlot().
This fixes a regression from 3e9457066.

R/plot_AbanicoPlot.R
@@ -1281,9 +1281,9 @@ plot_AbanicoPlot <- function(
           .summary_line("mean", summary[j], De.stats[i, 2], sep = is.sub),
           .summary_line("median", summary[j], De.stats[i, 3], sep = is.sub),
           .summary_line("kdemax", summary[j], De.stats[i, 4], sep = is.sub),
-          .summary_line("sdabs", summary[j], De.stats[i, 5], sep = is.sub,
+          .summary_line("sd.abs", summary[j], De.stats[i, 5], sep = is.sub,
                         label = "abs. sd"),
-          .summary_line("sdrel", summary[j], De.stats[i, 6], sep = is.sub,
+          .summary_line("sd.rel", summary[j], De.stats[i, 6], sep = is.sub,
                         label = "rel. sd"),
           .summary_line("se.abs", summary[j], De.stats[i, 7], sep = is.sub,
                         label = "se"),