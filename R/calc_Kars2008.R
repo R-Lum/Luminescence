2094ff9aee4c9377ab45ed1c2cd16f737d04dea0
Marco Colombo <marco.colombo@uni-heidelberg.de>
2025-06-13 12:49:20 +0200

    Remove defunct functions calc_Kars2008() and Analyse_SAR.OSLdata().

--- R/calc_Kars2008.R
+++
@@ -1,6 +0,0 @@
-calc_Kars2008 <- function(fit.method = "EXP", ...) {
-  # nocov start
-  .Defunct("calc_Huntley2006")
-  calc_Huntley2006(fit.method = fit.method, ...)
-  # nocov end
-}
--- R/calc_OSLLxTxRatio.R
+++ R/calc_OSLLxTxRatio.R
@@ -139,8 +139,7 @@
 #' @author
 #' Sebastian Kreutzer, Institute of Geography, Heidelberg University (Germany)
 #'
-#' @seealso [RLum.Data.Curve-class], [Analyse_SAR.OSLdata], [fit_DoseResponseCurve],
-#' [analyse_SAR.CWOSL]
+#' @seealso [RLum.Data.Curve-class], [fit_DoseResponseCurve], [analyse_SAR.CWOSL]
 #'
 #' @references Duller, G., 2018. Analyst v4.57 - User Manual.
 #' `https://users.aber.ac.uk/ggd`\cr
