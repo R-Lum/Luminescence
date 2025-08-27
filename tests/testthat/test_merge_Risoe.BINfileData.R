5381781018c1a4a4cb6db3df8db5995dc0a7b021
Marco Colombo <marco.colombo@uni-heidelberg.de>
2025-08-27 10:14:05 +0200

    Add snapshot tests for the merge_*() functions.

--- tests/testthat/test_merge_Risoe.BINfileData.R
+++ tests/testthat/test_merge_Risoe.BINfileData.R
@@ -1,13 +1,21 @@
-test_that("Test merging", {
+## load data
+data(ExampleData.BINfileData, envir = environment())
+o1 <- CWOSL.SAR.Data
+o2 <- CWOSL.SAR.Data
+
+test_that("input validation", {
   testthat::skip_on_cran()
 
-  ##expect error
   expect_error(merge_Risoe.BINfileData(input.objects = c("data", "data2")),
                "File 'data' does not exist")
   expect_error(merge_Risoe.BINfileData(input.objects = list("data", "data2")),
                "All elements of 'input.objects' should be of class 'Risoe.BINfileData'")
   expect_error(merge_Risoe.BINfileData(input.objects = c(FALSE, FALSE)),
                "'input.objects' should be of class 'character' or 'list'")
+})
+
+test_that("check functionality", {
+  testthat::skip_on_cran()
 
   ## nothing done
   input <- "data"
@@ -15,16 +23,20 @@ test_that("Test merging", {
                  "At least two input objects are needed, nothing done")
   expect_equal(res, input)
 
-  ## expect success
-  data(ExampleData.BINfileData, envir = environment())
-  object1 <- CWOSL.SAR.Data
-  object2 <- CWOSL.SAR.Data
-  expect_s4_class(merge_Risoe.BINfileData(c(object1, object2)), "Risoe.BINfileData")
-
+  ## write to an output file and read it back in
   binx <- system.file("extdata/BINfile_V8.binx", package = "Luminescence")
-  output.file <- tempfile()
+  output.file <- tempfile(fileext = ".binx")
   SW({
   merge_Risoe.BINfileData(c(binx, binx), output.file)
   })
   expect_true(file.exists(output.file))
+  expect_s4_class(new <- read_BIN2R(output.file, verbose = FALSE),
+                  "Risoe.BINfileData")
+  expect_length(new, 4)
+})
+
+test_that("snapshot tests", {
+  testthat::skip_on_cran()
+
+  expect_snapshot(merge_Risoe.BINfileData(c(o1, o2)))
 })
