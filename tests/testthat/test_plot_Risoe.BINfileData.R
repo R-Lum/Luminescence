9aeaa450d6b5ae64fbd537c9c5203278ee51867f
Marco Colombo <marco.colombo@uni-heidelberg.de>
2025-09-04 14:57:26 +0200

    Improve validation in plot_RisoeBINfileData().

--- tests/testthat/test_plot_Risoe.BINfileData.R
+++ tests/testthat/test_plot_Risoe.BINfileData.R
@@ -6,6 +6,12 @@ test_that("input validation", {
 
   expect_error(plot_Risoe.BINfileData("error"),
                "'data' should be of class 'Risoe.BINfileData'")
+  expect_error(plot_Risoe.BINfileData(CWOSL.SAR.Data, sorter = 1),
+               "'sorter' should be one of 'POSITION', 'RUN' or 'SET'")
+  expect_error(plot_Risoe.BINfileData(CWOSL.SAR.Data, sorter = c("SET", "RUN")),
+               "'sorter' should have length 1")
+  expect_error(plot_Risoe.BINfileData(CWOSL.SAR.Data, dose_rate = c(3, 4)),
+               "'dose_rate' should be a positive scalar")
   expect_error(plot_Risoe.BINfileData(CWOSL.SAR.Data, position = 1,
                                       curve.transformation = "error"),
                "'curve.transformation' should be one of 'CW2pLM', 'CW2pLMi'")
