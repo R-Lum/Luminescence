070068251ae4d9c299e56280a9b1f1387950d2e9
Marco Colombo <marco.colombo@uni-heidelberg.de>
2025-11-04 17:12:15 +0100

    Validate the frame.range argument of read_SPE2R().

--- tests/testthat/test_read_SPE2R.R
+++ tests/testthat/test_read_SPE2R.R
@@ -28,7 +28,12 @@ test_that("input validation", {
                  "Error: File does not exist, NULL returned")
   expect_error(read_SPE2R(local.file, output.object = "error"),
                "'output.object' should be one of 'RLum.Data.Image'")
-
+  expect_error(read_SPE2R(local.file, frame.range = NA),
+               "'frame.range' should be of class 'integer', 'numeric' or NULL")
+  expect_error(read_SPE2R(local.file, frame.range = c(2, NA)),
+               "'frame.range' should contain positive values")
+  expect_error(read_SPE2R(local.file, frame.range = c(0, 3)),
+               "'frame.range' should contain positive values")
   SW({
   expect_message(expect_null(read_SPE2R("http://httpbingo.org/status/404")),
                  "Error: File does not exist, NULL returned")
@@ -58,6 +63,7 @@ test_that("snapshot tests", {
 
   expect_snapshot_RLum(read_SPE2R(local.file,
                                   output.object = "RLum.Data.Spectrum",
+                                  frame.range = 20:40,
                                   verbose = FALSE),
                        tolerance = snapshot.tolerance)
   expect_snapshot_plain(read_SPE2R(local.file,
