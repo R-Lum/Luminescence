R-Lum/Luminescence

commit f29084fed509e2e07f0145d8841eec5a57cf5ddf

Reimplement .validate_logical_scalar() in terms of .validate_scalar().

R/internals_RLum.R
@@ -1273,10 +1273,17 @@ SW <- function(expr) {
 
 #' @title Validate scalar variables
 #'
+#' This function is meant to validate unbounded (integer) scalar variables.
+#' The `pos` and `log` arguments are not intended for direct use, they serve
+#' to facilitate the implementation of `.validate_positive_scalar()` and
+#' `.validate_logical_scalar()`.
+#'
 #' @param val [numeric] (**required**): value to validate.
 #' @param int [logical] (*with default*): whether the value has to be an
 #'        integer (`FALSE` by default).
 #' @param pos [logical] (*with default*): whether the value has to be positive
+#'        (`FALSE` by default).
+#' @param log [logical] (*with default*): whether the value has to be logical
 #'        (`FALSE` by default).
 #' @param null.ok [logical] (*with default*): whether a `NULL` value should be
 #'        considered valid (`FALSE` by default).
@@ -1289,16 +1296,17 @@ SW <- function(expr) {
 #' The validated value, unless the validation failed with an error thrown.
 #'
 #' @noRd
-.validate_scalar <- function(val, int = FALSE, pos = FALSE,
+.validate_scalar <- function(val, int = FALSE, pos = FALSE, log = FALSE,
                              null.ok = FALSE, name = NULL) {
   if (!missing(val) && is.null(val) && null.ok)
     return(NULL)
-  if (missing(val) || !is.numeric(val) || length(val) != 1 || is.na(val) ||
+  if (missing(val) || length(val) != 1 || is.na(val) ||
+      (!log && !is.numeric(val)) || (log && !is.logical(val)) ||
       (int && (is.infinite(val) || val != as.integer(val))) ||
       (pos && val <= 0)) {
     .throw_error(name %||% .first_argument(), " should be a single ",
-                 if (pos) "positive ", if (int) "integer ", "value",
-                 if (null.ok) " or NULL")
+                 if (pos) "positive ", if (int) "integer ", if (log) "logical ",
+                 "value", if (null.ok) " or NULL")
   }
   val
 }
@@ -1306,9 +1314,6 @@ SW <- function(expr) {
 #' @title Validate scalar variables expected to be positive
 #'
 #' @inheritParams .validate_scalar
-#'
-#' @return
-#' The validated value, unless the validation failed with an error thrown.
 #'
 #' @noRd
 .validate_positive_scalar <- function(val, int = FALSE, null.ok = FALSE,
@@ -1319,26 +1324,12 @@ SW <- function(expr) {
 
 #' @title Validate logical scalar variables
 #'
-#' @param val [numeric] (**required**): value to validate
-#' @param null.ok [logical] (*with default*): whether a `NULL` value should be
-#'        considered valid (`FALSE` by default)
-#' @param name [character] (*with default*): variable name to report in case
-#'        of error: if specified, it's reported as is; if not specified it's
-#'        inferred from the name of the variable tested and reported with
-#'        quotes.
-#'
-#' @return
-#' The validated value, unless the validation failed with an error thrown.
+#' @inheritParams .validate_scalar
 #'
 #' @noRd
 .validate_logical_scalar <- function(val, null.ok = FALSE, name = NULL) {
-  if (!missing(val) && is.null(val) && null.ok)
-    return(NULL)
-  if (missing(val) || !is.logical(val) || length(val) != 1 || is.na(val)) {
-    .throw_error(name %||% .first_argument(), " should be a single logical value",
-                 if (null.ok) " or NULL")
-  }
-  val
+  .validate_scalar(val, log = TRUE, null.ok = null.ok,
+                   name = name %||% .first_argument())
 }
 
 #' Check that a suggested package is installed

commit 826b66a7d70d6bdbfdfb356779cc17a8883ea9ef

.validate_scalar: Make it work correctly on 1-column data frames.
This fixes a regression from f29084fe.

R/internals_RLum.R
@@ -1285,7 +1285,7 @@ SW <- function(expr) {
                              null.ok = FALSE, name = NULL) {
   if (!missing(val) && is.null(val) && null.ok)
     return(NULL)
-  if (missing(val) || length(val) != 1 || is.na(val) ||
+  if (missing(val) || NROW(val) != 1 || is.na(val) ||
       (!log && !is.numeric(val)) || (log && !is.logical(val)) ||
       (int && (is.infinite(val) || val != as.integer(val))) ||
       (pos && val <= 0)) {

tests/testthat/test_internals.R
@@ -438,6 +438,8 @@ test_that("Test internals", {
                "'test' should be a single value")
   expect_error(.validate_scalar(iris),
                "'iris' should be a single value")
+  expect_error(.validate_scalar(iris[, 1, drop = FALSE]),
+               "'iris' should be a single value")
   expect_error(.validate_scalar(iris, null.ok = TRUE),
                "'iris' should be a single value or NULL")
   expect_error(.validate_scalar(-1:2, name = "'var'"),