R-Lum/Luminescence

commit 9055876c2813d15b84e98191b7628ca1860bdcda

plot_RadialPlot: Don't warn on negative lines when log.z = TRUE.

R/plot_RadialPlot.R
@@ -836,17 +836,23 @@ plot_RadialPlot <- function(
   if (!is.null(line)) {
     #line = line + De.add
 
-    if (log.z) line <- log(line)
+    if (log.z) {
+      if (any(line < 0)) {
+        line <- line[line >= 0]
+        .throw_message("Lines with negative value skipped due to 'log.z = TRUE'")
+      }
+      line <- log(line)
+    }
 
     line.coords <- NULL
-    for(i in 1:length(line)) {
+    for (i in seq_along(line)) {
       line.x <- c(limits.x[1], x.coord(line[i]))
       line.y <- c(0, y.coord(line[i], line.x[2]))
       line.coords[[i]] <- rbind(line.x, line.y)
     }
 
     if (is.null(line.col)) {
-      line.col <- seq(from = 1, to = length(line.coords))
+      line.col <- seq_along(length(line.coords))
     }
 
     if (is.null(line.label)) {

tests/testthat/test_plot_RadialPlot.R
@@ -141,6 +141,9 @@ test_that("check functionality", {
       log.z = FALSE),
     regexp = "Attention.*")
 
+  expect_message(plot_RadialPlot(df, line = -1),
+                 "Lines with negative value skipped due to 'log.z = TRUE'")
+
   ## trigger warning
   expect_warning(plot_RadialPlot(
       data = df,

commit 00b23ac64209a0138a141a57f59588801f0c1889

plot_RadialPlot: Assign line colour correctly.
This fixes a regression introduced in 9055876c, which caused all lines
after the first not to be drawn because their line.col was NULL.

R/plot_RadialPlot.R
@@ -853,7 +853,7 @@ plot_RadialPlot <- function(
     }
 
     if (is.null(line.col)) {
-      line.col <- seq_along(length(line.coords))
+      line.col <- seq_along(line.coords)
     }
 
     if (is.null(line.label)) {

tests/testthat/_snaps/plot_RadialPlot/summary-line-cex.svg
@@ -29,8 +29,8 @@
 <polyline points='115.20,226.50 541.94,226.50 ' style='stroke-width: 0.75; stroke-dasharray: 4.00,4.00;' />
 <polyline points='115.20,226.50 538.68,272.54 ' style='stroke-width: 0.75;' />
 <text x='524.28' y='273.17' text-anchor='end' style='font-size: 21.60px; font-family: sans;' textLength='30.03px' lengthAdjust='spacingAndGlyphs'>4.5</text>
-<polyline points='115.20,226.50 529.84,138.25 ' style='stroke-width: 0.75; stroke: none;' />
-<text x='515.44' y='138.89' text-anchor='end' style='font-size: 21.60px; font-family: sans;' textLength='30.03px' lengthAdjust='spacingAndGlyphs'>6.5</text>
+<polyline points='115.20,226.50 529.84,138.25 ' style='stroke-width: 0.75; stroke: #DF536B;' />
+<text x='515.44' y='138.89' text-anchor='end' style='font-size: 21.60px; fill: #DF536B; font-family: sans;' textLength='30.03px' lengthAdjust='spacingAndGlyphs'>6.5</text>
 <polygon points='515.14,356.67 515.32,356.23 515.51,355.80 515.69,355.36 515.88,354.92 516.06,354.48 516.24,354.04 516.43,353.60 516.61,353.16 516.79,352.72 516.97,352.28 517.15,351.84 517.33,351.39 517.51,350.95 517.69,350.51 517.87,350.06 518.05,349.62 518.23,349.17 518.40,348.72 518.58,348.28 518.76,347.83 518.93,347.38 519.11,346.93 519.28,346.48 519.46,346.03 519.63,345.58 519.80,345.13 519.98,344.68 520.15,344.23 520.32,343.78 520.49,343.32 520.66,342.87 520.83,342.41 521.00,341.96 521.17,341.50 521.34,341.05 521.51,340.59 521.68,340.13 521.84,339.67 522.01,339.22 522.18,338.76 522.34,338.30 522.51,337.84 522.67,337.38 522.83,336.91 523.00,336.45 523.16,335.99 523.32,335.53 523.48,335.06 523.64,334.60 523.81,334.14 523.96,333.67 524.12,333.20 524.28,332.74 524.44,332.27 524.60,331.80 524.76,331.34 524.91,330.87 525.07,330.40 525.22,329.93 525.38,329.46 525.53,328.99 525.69,328.52 525.84,328.05 525.99,327.57 526.14,327.10 526.29,326.63 526.44,326.15 526.59,325.68 526.74,325.21 526.89,324.73 527.04,324.25 527.19,323.78 527.33,323.30 527.48,322.82 527.63,322.35 527.77,321.87 527.92,321.39 528.06,320.91 528.20,320.43 528.35,319.95 528.49,319.47 528.63,318.99 528.77,318.51 528.91,318.02 529.05,317.54 529.19,317.06 529.32,316.57 529.46,316.09 529.60,315.60 529.73,315.12 529.87,314.63 530.00,314.15 530.14,313.66 530.27,313.17 530.40,312.69 530.54,312.20 530.67,311.71 530.80,311.22 530.93,310.73 531.06,310.24 531.19,309.75 531.32,309.26 531.44,308.77 531.57,308.28 531.70,307.79 531.82,307.29 531.95,306.80 532.07,306.31 532.19,305.81 532.32,305.32 532.44,304.82 532.56,304.33 532.68,303.83 532.80,303.34 532.92,302.84 533.04,302.34 533.15,301.85 533.27,301.35 533.39,300.85 533.50,300.35 533.62,299.85 533.73,299.35 533.84,298.85 533.96,298.35 534.07,297.85 534.18,297.35 534.29,296.85 534.40,296.35 534.51,295.85 534.62,295.34 534.73,294.84 534.83,294.34 534.94,293.83 535.04,293.33 535.15,292.82 535.25,292.32 535.36,291.81 535.46,291.31 535.56,290.80 535.66,290.30 535.76,289.79 535.86,289.28 535.96,288.78 536.06,288.27 536.15,287.76 536.25,287.25 536.35,286.74 536.44,286.23 536.53,285.72 536.63,285.21 536.72,284.70 536.81,284.19 536.90,283.68 536.99,283.17 537.08,282.66 537.17,282.15 537.26,281.63 537.35,281.12 537.43,280.61 537.52,280.10 537.60,279.58 537.69,279.07 537.77,278.55 537.85,278.04 537.93,277.53 538.02,277.01 538.10,276.50 538.18,275.98 538.25,275.46 538.33,274.95 538.41,274.43 538.48,273.92 538.56,273.40 538.63,272.88 538.71,272.36 538.78,271.85 538.85,271.33 538.92,270.81 538.99,270.29 539.06,269.77 539.13,269.25 539.20,268.73 539.27,268.21 539.33,267.69 539.40,267.17 539.47,266.65 539.53,266.13 539.59,265.61 539.65,265.09 539.72,264.57 539.78,264.05 539.84,263.53 539.90,263.01 539.95,262.48 540.01,261.96 540.07,261.44 540.12,260.92 540.18,260.39 540.23,259.87 540.29,259.35 540.34,258.82 540.39,258.30 540.44,257.77 540.49,257.25 540.54,256.73 540.59,256.20 540.64,255.68 540.68,255.15 540.73,254.63 540.77,254.10 540.82,253.58 540.86,253.05 540.90,252.53 540.95,252.00 540.99,251.47 541.03,250.95 541.07,250.42 541.10,249.90 541.14,249.37 541.18,248.84 541.21,248.32 541.25,247.79 541.28,247.26 541.32,246.73 541.35,246.21 541.38,245.68 541.41,245.15 541.44,244.62 541.47,244.10 541.50,243.57 541.52,243.04 541.55,242.51 541.58,241.98 541.60,241.46 541.62,240.93 541.65,240.40 541.67,239.87 541.69,239.34 541.71,238.81 541.73,238.28 541.75,237.76 541.77,237.23 541.78,236.70 541.80,236.17 541.81,235.64 541.83,235.11 541.84,234.58 541.86,234.05 541.87,233.52 541.88,232.99 541.89,232.46 541.90,231.93 541.91,231.40 541.91,230.88 541.92,230.35 541.93,229.82 541.93,229.29 541.94,228.76 541.94,228.23 541.94,227.70 541.94,227.17 541.94,226.64 541.94,226.11 541.94,225.58 541.94,225.05 541.94,224.52 541.93,223.99 541.93,223.46 541.92,222.93 541.92,222.40 541.91,221.87 541.90,221.34 541.89,220.81 541.88,220.28 541.87,219.76 541.86,219.23 541.85,218.70 541.84,218.17 541.82,217.64 541.81,217.11 541.79,216.58 541.78,216.05 541.76,215.52 541.74,214.99 541.72,214.46 541.70,213.94 541.68,213.41 541.66,212.88 541.64,212.35 541.61,211.82 541.59,211.29 541.56,210.77 541.54,210.24 541.51,209.71 541.48,209.18 541.45,208.65 541.42,208.13 541.39,207.60 541.36,207.07 541.33,206.54 541.30,206.02 541.27,205.49 541.23,204.96 541.20,204.43 541.16,203.91 541.12,203.38 541.08,202.85 541.05,202.33 541.01,201.80 540.97,201.28 540.93,200.75 540.88,200.22 540.84,199.70 540.80,199.17 540.75,198.65 540.71,198.12 540.66,197.60 540.61,197.07 540.57,196.55 540.52,196.02 540.47,195.50 540.42,194.98 540.37,194.45 540.31,193.93 540.26,193.41 540.21,192.88 540.15,192.36 540.10,191.84 540.04,191.31 539.98,190.79 539.93,190.27 539.87,189.75 539.81,189.23 539.75,188.70 539.69,188.18 539.62,187.66 539.56,187.14 539.50,186.62 539.43,186.10 539.37,185.58 539.30,185.06 539.24,184.54 539.17,184.02 539.10,183.50 539.03,182.98 538.96,182.46 538.89,181.94 538.82,181.43 538.74,180.91 538.67,180.39 538.60,179.87 538.52,179.36 538.45,178.84 538.37,178.32 538.29,177.81 538.22,177.29 538.14,176.78 538.06,176.26 537.98,175.74 537.89,175.23 537.81,174.72 537.73,174.20 537.65,173.69 537.56,173.17 537.48,172.66 537.39,172.15 537.30,171.63 537.22,171.12 537.13,170.61 537.04,170.10 536.95,169.59 536.86,169.08 536.77,168.57 536.68,168.05 536.58,167.54 536.49,167.04 536.39,166.53 536.30,166.02 536.20,165.51 536.11,165.00 536.01,164.49 535.91,163.98 535.81,163.48 535.71,162.97 535.61,162.46 535.51,161.96 535.41,161.45 535.30,160.95 535.20,160.44 535.10,159.94 534.99,159.43 534.89,158.93 534.78,158.42 534.67,157.92 534.56,157.42 534.46,156.92 534.35,156.41 534.24,155.91 534.13,155.41 534.01,154.91 533.90,154.41 533.79,153.91 533.68,153.41 533.56,152.91 533.45,152.41 533.33,151.91 533.21,151.42 533.10,150.92 532.98,150.42 532.86,149.92 532.74,149.43 532.62,148.93 532.50,148.44 532.38,147.94 532.26,147.45 532.13,146.95 532.01,146.46 531.88,145.97 531.76,145.47 531.63,144.98 531.51,144.49 531.38,144.00 531.25,143.51 531.12,143.02 531.00,142.53 530.87,142.04 530.74,141.55 530.60,141.06 530.47,140.57 530.34,140.08 530.21,139.60 530.07,139.11 529.94,138.62 529.80,138.14 529.67,137.65 529.53,137.17 529.39,136.68 529.26,136.20 529.12,135.71 528.98,135.23 528.84,134.75 528.70,134.27 528.56,133.78 528.42,133.30 528.28,132.82 528.13,132.34 527.99,131.86 527.84,131.38 527.70,130.91 527.55,130.43 527.41,129.95 527.26,129.47 527.12,129.00 526.97,128.52 526.82,128.04 526.67,127.57 526.52,127.10 526.37,126.62 526.22,126.15 526.07,125.68 525.92,125.20 525.76,124.73 525.61,124.26 525.46,123.79 525.30,123.32 525.15,122.85 524.99,122.38 524.84,121.91 524.68,121.44 524.52,120.97 524.36,120.51 524.21,120.04 524.05,119.58 523.89,119.11 523.73,118.64 523.57,118.18 523.40,117.72 523.24,117.25 523.08,116.79 522.92,116.33 522.75,115.87 522.59,115.41 522.42,114.95 522.26,114.49 522.09,114.03 521.93,113.57 521.76,113.11 521.59,112.65 521.43,112.19 521.26,111.74 521.09,111.28 520.92,110.83 520.75,110.37 520.58,109.92 520.41,109.46 520.24,109.01 520.06,108.56 519.89,108.11 519.72,107.65 921.60,107.65 921.60,356.67 ' style='stroke-width: 0.75; stroke: none; fill: #FFFFFF;' />
 <text x='227.15' y='50.88' style='font-size: 28.80px; font-family: sans;' textLength='20.81px' lengthAdjust='spacingAndGlyphs'>D</text>
 <text x='247.96' y='56.20' style='font-size: 20.16px; font-family: sans;' textLength='11.21px' lengthAdjust='spacingAndGlyphs'>e</text>