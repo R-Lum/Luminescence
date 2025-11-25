R-Lum/Luminescence

commit 0c90f9b9d399d18e4983aa5550caa8763b83bfc6

analyse_FadingMeasurement: Consolidate plotting code.

R/analyse_FadingMeasurement.R
@@ -8,9 +8,10 @@
 #' Kars et al. (2008).
 #'
 #' @details
-#' All provided output corresponds to the \eqn{tc} value obtained by this analysis. Additionally
-#' in the output object the g-value normalised to 2-days is provided. The output of this function
-#' can be passed to the function [calc_FadingCorr].
+#' All provided output corresponds to the \eqn{tc} value obtained by this
+#' analysis. Additionally, the g-value normalised to 2-days is provided in the
+#' output object. The output of this function can be passed to the function
+#' [calc_FadingCorr].
 #'
 #' **Fitting and error estimation**
 #'
@@ -49,13 +50,14 @@
 #'
 #' **Multiple aliquots & Lx/Tx normalisation**
 #'
-#' Be aware that this function will always normalise all \eqn{\frac{L_x}{T_x}} values
-#' by the \eqn{\frac{L_x}{T_x}} value of the
-#' prompt measurement of the first aliquot. This implicitly assumes that there are no systematic
+#' Be aware that this function will always normalise all \eqn{\frac{L_x}{T_x}}
+#' values by the \eqn{\frac{L_x}{T_x}} value of the prompt measurement of the
+#' first aliquot. This implicitly assumes that there are no systematic
 #' inter-aliquot variations in the \eqn{\frac{L_x}{T_x}} values.
 #' If deemed necessary to normalise the \eqn{\frac{L_x}{T_x}} values  of each
 #' aliquot by its individual prompt measurement please do so **before** running
-#' [analyse_FadingMeasurement] and provide the already normalised values for `object` instead.
+#' [analyse_FadingMeasurement] and provide the already normalised values for
+#' `object` instead.
 #'
 #' **Shine-down curve plots**
 #' Please note that the shine-down curve plots are for information only. As such
@@ -75,10 +77,10 @@
 #' the object originated from an XSYG file, also the irradiation steps must
 #' be preserved in the input object.**
 #'
-#' If data from multiple aliquots are provided please **see the details below** with regard to
-#' Lx/Tx normalisation. **The function assumes that all your measurements are related to
-#' one (comparable) sample. If you have to treat independent samples, you have use this function
-#' in a loop.**
+#' If data from multiple aliquots are provided please **see the details below**
+#' with regard to Lx/Tx normalisation. **The function assumes that all your
+#' measurements are related to one (comparable) sample. If you have to treat
+#' independent samples, you have use this function in a loop.**
 #'
 #' @param structure [character] (*with default*):
 #' the structure of the measurement data, one of `'Lx'` or `c('Lx','Tx')`.
@@ -144,7 +146,7 @@
 #' `call` \tab `call` \tab the original function call\cr
 #' }
 #'
-#' @section Function version: 0.1.24
+#' @section Function version: 0.1.25
 #'
 #' @author Sebastian Kreutzer, Institute of Geography, Heidelberg University (Germany) \cr
 #' Christoph Burow, University of Cologne (Germany)
@@ -672,160 +674,75 @@ analyse_FadingMeasurement <- function(
     if (is.logical(plot_singlePanels))
       plot_singlePanels <- 1:4
 
-    ## plot Lx-curves -----
+    ## compute integration limits for plots
     if (!is.null(object)) {
       if (length(structure) == 2) {
-
-        if (1 %in% plot_singlePanels) {
-          records <- object_clean[seq(1, length(object_clean), by = 2)]
-          plot_RLum(
-            set_RLum(class = "RLum.Analysis",
-                     records = records),
-            combine = length(records) > 1,
-            col = c(col[1:5], rep(
-              rgb(0, 0, 0, 0.3), abs(length(TIMESINCEIRR) - 5)
-            ))[1:length(records)],
-            records_max = 10,
-            plot_singlePanels = TRUE,
-            legend.text = c(paste(round(irradiation_times.unique, 1), "s")),
-            xlab = plot_settings$xlab,
-            xlim = plot_settings$xlim,
-            log = plot_settings$log,
-            legend.pos = "outside",
-            main = bquote(L[x] ~ "- curve"),
-            mtext = plot_settings$mtext
-          )
-
-          ##add integration limits
-          abline(v = c(
-            object_clean[[1]][range(signal.integral), 1],
-            object_clean[[1]][range(background.integral), 1]),
-            lty = c(2,2,2,2),
-            col = c("green", "green", "red", "red"))
-        }
-
-        # plot Tx-curves ----
-        if (2 %in% plot_singlePanels) {
-          records <- object_clean[seq(2, length(object_clean), by = 2)]
-          plot_RLum(
-            set_RLum(class = "RLum.Analysis",
-                     records = records),
-            combine = length(records) > 1,
-            records_max = 10,
-            plot_singlePanels = TRUE,
-            legend.text = paste(round(irradiation_times.unique, 1), "s"),
-            xlab = plot_settings$xlab,
-            log = plot_settings$log,
-            legend.pos = "outside",
-            main = bquote(T[x] ~ "- curve"),
-            mtext = plot_settings$mtext
-          )
-
-          if (is.null(list(...)$signal.integral.Tx)) {
-            ##add integration limits
-            abline(v = c(
-              object_clean[[1]][range(signal.integral), 1],
-              object_clean[[1]][range(background.integral), 1]),
-              lty = c(2,2,2,2),
-              col = c("green", "green", "red", "red"))
-
-          } else{
-            ##add integration limits
-            abline(
-              v = range(list(...)$signal.integral.Tx) *
-                max(as.matrix(object_clean[[1]][, 1])) /
-                nrow(as.matrix(object_clean[[1]])),
-              lty = 2,
-              col = "green"
-            )
-            abline(
-              v = range(list(...)$background.integral.Tx) *
-                max(as.matrix(object_clean[[1]][, 1])) /
-                nrow(as.matrix(object_clean[[1]])),
-              lty = 2,
-              col = "red"
-            )
-          }
-        }
-      } else{
-        if (1 %in% plot_singlePanels) {
-          plot_RLum(
-            set_RLum(class = "RLum.Analysis", records = object_clean),
-            combine = length(object_clean) > 1,
-            records_max = 10,
-            plot_singlePanels = TRUE,
-            legend.text = c(paste(round(irradiation_times.unique, 1), "s")),
-            legend.pos = "outside",
-            xlab = plot_settings$xlab,
-            log = plot_settings$log,
-            main = bquote(L[x] ~ " - curves"),
-            mtext = plot_settings$mtext
-          )
-
-          ##add integration limits
-          abline(
-            v = range(signal.integral) * max(as.matrix(object_clean[[1]][, 1])) /
-              nrow(as.matrix(object_clean[[1]])),
-            lty = 2,
-            col = "green"
-          )
-          abline(
-            v = range(background.integral) * max(as.matrix(object_clean[[1]][, 1])) /
-              nrow(as.matrix(object_clean[[1]])),
-            lty = 2,
-            col = "red"
-          )
-        }
-
-        ##empty Tx plot
-        if (2 %in% plot_singlePanels) {
-          plot(
-            NA,
-            NA,
-            xlim = c(0, 1),
-            ylim = c(0, 1),
-            xlab = "",
-            ylab = "",
-            axes = FALSE
-          )
-          text(x = 0.5,
-               y = 0.5,
-               labels = expression(paste("No ", T[x], " curves detected")))
-        }
+        int.limits.lx <- c(object_clean[[1]][range(signal.integral), 1],
+                           object_clean[[1]][range(background.integral), 1])
+      } else {
+        int.limits.lx <- c(range(signal.integral), range(background.integral)) *
+          max(as.matrix(object_clean[[1]][, 1])) /
+          nrow(as.matrix(object_clean[[1]]))
       }
-
-    }else{
-      if (1 %in% plot_singlePanels) {
-        ##empty Lx plot
-        plot(
-          NA,
-          NA,
-          xlim = c(0, 1),
-          ylim = c(0, 1),
-          xlab = "",
-          ylab = "",
-          axes = FALSE
-        )
-        text(x = 0.5,
-             y = 0.5,
-             labels = expression(paste("No ", L[x], " curves detected")))
+      if (is.null(list(...)$signal.integral.Tx)) {
+        int.limits.tx <- int.limits.lx
+      } else {
+        int.limits.tx <- c(range(list(...)$signal.integral.Tx),
+                           range(list(...)$background.integral.Tx)) *
+          max(as.matrix(object_clean[[1]][, 1])) /
+          nrow(as.matrix(object_clean[[1]]))
       }
+    }
 
-      if (2 %in% plot_singlePanels) {
-        ##empty Tx plot
-        plot(
-          NA,
-          NA,
-          xlim = c(0, 1),
-          ylim = c(0, 1),
-          xlab = "",
-          ylab = "",
-          axes = FALSE
-        )
-        text(x = 0.5,
-             y = 0.5,
-             labels = expression(paste("No ", T[x], " curves detected")))
-      }
+    ## plot Lx-curves -----
+    if (!is.null(object) && 1 %in% plot_singlePanels) {
+      records <- object_clean[seq(1, length(object_clean), by = length(structure))]
+      plot_RLum(
+          set_RLum(class = "RLum.Analysis",
+                   records = records),
+          combine = length(records) > 1,
+          records_max = 10,
+          plot_singlePanels = TRUE,
+          legend.text = c(paste(round(irradiation_times.unique, 1), "s")),
+          xlab = plot_settings$xlab,
+          xlim = plot_settings$xlim,
+          log = plot_settings$log,
+          legend.pos = "outside",
+          main = bquote(L[x] ~ "- curve"),
+          mtext = plot_settings$mtext
+      )
+      abline(v = int.limits.lx, lty = 2,
+             col = c("green", "green", "red", "red"))
+    } else {
+      ## empty Lx plot
+      shape::emptyplot()
+      text(x = 0.5, y = 0.5,
+           labels = expression(paste("No ", L[x], " curves detected")))
+    }
+
+    ## plot Tx-curves ----
+    if (!is.null(object) && length(structure) == 2 && 2 %in% plot_singlePanels) {
+      records <- object_clean[seq(2, length(object_clean), by = 2)]
+      plot_RLum(
+          set_RLum(class = "RLum.Analysis",
+                   records = records),
+          combine = length(records) > 1,
+          records_max = 10,
+          plot_singlePanels = TRUE,
+          legend.text = paste(round(irradiation_times.unique, 1), "s"),
+          xlab = plot_settings$xlab,
+          log = plot_settings$log,
+          legend.pos = "outside",
+          main = bquote(T[x] ~ "- curve"),
+          mtext = plot_settings$mtext
+      )
+      abline(v = int.limits.tx, lty = 2,
+             col = c("green", "green", "red", "red"))
+    } else {
+      ## empty Tx plot
+      shape::emptyplot()
+      text(x = 0.5, y = 0.5,
+           labels = expression(paste("No ", T[x], " curves detected")))
     }
 
     ## plot fading ----

man/analyse_FadingMeasurement.Rd
@@ -31,10 +31,10 @@ for the data analysis, i.e. only IRSL curves representing Lx (and Tx). If
 the object originated from an XSYG file, also the irradiation steps must
 be preserved in the input object.}
 
-If data from multiple aliquots are provided please \strong{see the details below} with regard to
-Lx/Tx normalisation. \strong{The function assumes that all your measurements are related to
-one (comparable) sample. If you have to treat independent samples, you have use this function
-in a loop.}}
+If data from multiple aliquots are provided please \strong{see the details below}
+with regard to Lx/Tx normalisation. \strong{The function assumes that all your
+measurements are related to one (comparable) sample. If you have to treat
+independent samples, you have use this function in a loop.}}
 
 \item{structure}{\link{character} (\emph{with default}):
 the structure of the measurement data, one of \code{'Lx'} or \code{c('Lx','Tx')}.}
@@ -108,9 +108,10 @@ Additionally, the density of recombination centres (rho') is estimated after
 Kars et al. (2008).
 }
 \details{
-All provided output corresponds to the \eqn{tc} value obtained by this analysis. Additionally
-in the output object the g-value normalised to 2-days is provided. The output of this function
-can be passed to the function \link{calc_FadingCorr}.
+All provided output corresponds to the \eqn{tc} value obtained by this
+analysis. Additionally, the g-value normalised to 2-days is provided in the
+output object. The output of this function can be passed to the function
+\link{calc_FadingCorr}.
 
 \strong{Fitting and error estimation}
 
@@ -149,13 +150,14 @@ procedure as for the g-value is applied (see above).
 
 \strong{Multiple aliquots & Lx/Tx normalisation}
 
-Be aware that this function will always normalise all \eqn{\frac{L_x}{T_x}} values
-by the \eqn{\frac{L_x}{T_x}} value of the
-prompt measurement of the first aliquot. This implicitly assumes that there are no systematic
+Be aware that this function will always normalise all \eqn{\frac{L_x}{T_x}}
+values by the \eqn{\frac{L_x}{T_x}} value of the prompt measurement of the
+first aliquot. This implicitly assumes that there are no systematic
 inter-aliquot variations in the \eqn{\frac{L_x}{T_x}} values.
 If deemed necessary to normalise the \eqn{\frac{L_x}{T_x}} values  of each
 aliquot by its individual prompt measurement please do so \strong{before} running
-\link{analyse_FadingMeasurement} and provide the already normalised values for \code{object} instead.
+\link{analyse_FadingMeasurement} and provide the already normalised values for
+\code{object} instead.
 
 \strong{Shine-down curve plots}
 Please note that the shine-down curve plots are for information only. As such
@@ -163,7 +165,7 @@ a maximum of five pause steps are plotted to avoid graphically overloaded plots.
 However, \emph{all} pause times are taken into consideration for the analysis.
 }
 \section{Function version}{
- 0.1.24
+ 0.1.25
 }
 
 \examples{

tests/testthat/_snaps/analyse_FadingMeasurement/panel-3.svg
@@ -18,6 +18,15 @@
   </clipPath>
 </defs>
 <g clip-path='url(#cpMC4wMHw3MjAuMDB8MC4wMHw1NzYuMDA=)'>
+</g>
+<defs>
+  <clipPath id='cpNTkuMDR8Njg5Ljc2fDU5LjA0fDUwMi41Ng=='>
+    <rect x='59.04' y='59.04' width='630.72' height='443.52' />
+  </clipPath>
+</defs>
+<g clip-path='url(#cpNTkuMDR8Njg5Ljc2fDU5LjA0fDUwMi41Ng==)'>
+</g>
+<g clip-path='url(#cpMC4wMHw3MjAuMDB8MC4wMHw1NzYuMDA=)'>
 <line x1='59.04' y1='445.07' x2='59.04' y2='116.53' style='stroke-width: 0.75;' />
 <line x1='59.04' y1='445.07' x2='51.84' y2='445.07' style='stroke-width: 0.75;' />
 <line x1='59.04' y1='362.93' x2='51.84' y2='362.93' style='stroke-width: 0.75;' />
@@ -64,11 +73,6 @@
 <text x='685.90' y='539.28' text-anchor='middle' style='font-size: 8.40px; font-family: sans;' textLength='16.34px' lengthAdjust='spacingAndGlyphs'>[3.4]</text>
 <text x='374.40' y='56.16' text-anchor='middle' style='font-size: 10.80px; font-family: sans;' textLength='228.51px' lengthAdjust='spacingAndGlyphs'>g-value: 5.18 ± 0.74 (%/decade) | tc = 3.78e+02</text>
 </g>
-<defs>
-  <clipPath id='cpNTkuMDR8Njg5Ljc2fDU5LjA0fDUwMi41Ng=='>
-    <rect x='59.04' y='59.04' width='630.72' height='443.52' />
-  </clipPath>
-</defs>
 <g clip-path='url(#cpNTkuMDR8Njg5Ljc2fDU5LjA0fDUwMi41Ng==)'>
 <polygon points='82.40,103.45 94.32,105.00 106.24,106.54 118.16,108.09 130.07,109.64 141.99,111.18 153.91,112.73 165.83,114.28 177.75,115.83 189.67,117.37 201.58,118.92 213.50,120.47 225.42,122.01 237.34,123.56 249.26,125.11 261.18,126.66 273.09,128.20 285.01,129.75 296.93,131.30 308.85,132.84 320.77,134.39 332.69,135.94 344.60,137.49 356.52,139.03 368.44,140.53 380.36,141.97 392.28,143.41 404.20,144.86 416.11,146.30 428.03,147.74 439.95,149.19 451.87,150.63 463.79,152.07 475.71,153.52 487.62,154.71 499.54,155.75 511.46,156.79 523.38,157.83 535.30,158.87 547.22,159.91 559.13,160.95 571.05,161.99 582.97,163.03 594.89,164.07 606.81,165.11 618.73,166.15 630.64,167.19 642.56,168.23 654.48,169.27 666.40,170.31 666.40,197.43 654.48,195.61 642.56,193.78 630.64,191.96 618.73,190.13 606.81,188.31 594.89,186.48 582.97,184.66 571.05,182.83 559.13,181.01 547.22,179.18 535.30,177.36 523.38,175.53 511.46,173.71 499.54,171.88 487.62,170.06 475.71,168.23 463.79,166.41 451.87,164.58 439.95,162.76 428.03,160.93 416.11,159.11 404.20,157.28 392.28,155.46 380.36,153.63 368.44,151.81 356.52,149.98 344.60,148.16 332.69,146.33 320.77,144.51 308.85,142.68 296.93,140.86 285.01,139.03 273.09,137.21 261.18,135.57 249.26,133.96 237.34,132.87 225.42,131.83 213.50,130.79 201.58,129.75 189.67,128.71 177.75,127.67 165.83,126.63 153.91,125.59 141.99,124.55 130.07,123.51 118.16,122.47 106.24,121.43 94.32,120.39 82.40,119.35 ' style='stroke-width: 0.75; stroke: none; fill: #000000; fill-opacity: 0.20;' />
 <polyline points='82.40,109.55 88.24,110.27 94.08,110.98 99.92,111.70 105.76,112.42 111.60,113.13 117.44,113.85 123.28,114.57 129.12,115.28 134.96,116.00 140.80,116.72 146.64,117.43 152.48,118.15 158.32,118.87 164.16,119.58 170.00,120.30 175.84,121.02 181.68,121.73 187.52,122.45 193.36,123.17 199.20,123.88 205.04,124.60 210.88,125.32 216.72,126.04 222.56,126.75 228.40,127.47 234.24,128.19 240.08,128.90 245.92,129.62 251.76,130.34 257.60,131.05 263.44,131.77 269.28,132.49 275.12,133.20 280.96,133.92 286.80,134.64 292.64,135.35 298.48,136.07 304.32,136.79 310.16,137.50 316.00,138.22 321.84,138.94 327.68,139.65 333.52,140.37 339.36,141.09 345.20,141.80 351.04,142.52 356.88,143.24 362.72,143.96 368.56,144.67 374.40,145.39 380.24,146.11 386.08,146.82 391.92,147.54 397.76,148.26 403.60,148.97 409.44,149.69 415.28,150.41 421.12,151.12 426.96,151.84 432.80,152.56 438.64,153.27 444.48,153.99 450.32,154.71 456.16,155.42 462.00,156.14 467.84,156.86 473.68,157.57 479.52,158.29 485.36,159.01 491.20,159.72 497.04,160.44 502.88,161.16 508.72,161.88 514.56,162.59 520.40,163.31 526.24,164.03 532.08,164.74 537.92,165.46 543.76,166.18 549.60,166.89 555.44,167.61 561.28,168.33 567.12,169.04 572.96,169.76 578.80,170.48 584.64,171.19 590.48,171.91 596.32,172.63 602.16,173.34 608.00,174.06 613.84,174.78 619.68,175.49 625.52,176.21 631.36,176.93 637.20,177.64 643.04,178.36 648.88,179.08 654.72,179.80 660.56,180.51 666.40,181.23 ' style='stroke-width: 1.12; stroke: #FF0000;' />

commit 8758420aa368cd6a8eaef8dc8cc3db0940851171

analyse_FadingMeasurement: Don't plot unrequested panels.
This fixes a regression introduced in 0c90f9b9. This was spotted after
running analyse_Thermochronometry(), so it will get a graphical snapshot
when that's finalised.

R/analyse_FadingMeasurement.R
@@ -713,7 +713,7 @@ analyse_FadingMeasurement <- function(
       )
       abline(v = int.limits.lx, lty = 2,
              col = c("green", "green", "red", "red"))
-    } else {
+    } else if (1 %in% plot_singlePanels) {
       ## empty Lx plot
       shape::emptyplot()
       text(x = 0.5, y = 0.5,
@@ -738,7 +738,7 @@ analyse_FadingMeasurement <- function(
       )
       abline(v = int.limits.tx, lty = 2,
              col = c("green", "green", "red", "red"))
-    } else {
+    } else if (2 %in% plot_singlePanels) {
       ## empty Tx plot
       shape::emptyplot()
       text(x = 0.5, y = 0.5,