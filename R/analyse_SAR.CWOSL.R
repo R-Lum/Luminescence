R-Lum/Luminescence

commit b43b274132776cb799f8e2ef78e2979fdcbd2856

Minor update for analyse_SAR_CWOSL() to better display the rejection criteria.

R/analyse_SAR.CWOSL.R
@@ -727,6 +727,7 @@ if(is.list(object)){
        rep(rejection.criteria$recycling.ratio, length(RecyclingRatio)),
        rep(rejection.criteria$recuperation.rate, length(Recuperation))) / 100
 
+
     ## compare a single value with a threshold (either can be NA)
     .status_from_threshold <- function(value, threshold) {
       if (is.na(threshold) || isTRUE(value <= threshold))
@@ -736,9 +737,18 @@ if(is.list(object)){
 
     ##RecyclingRatio
     temp.status.RecyclingRatio <- rep("OK", length(RecyclingRatio))
-    if (!anyNA(RecyclingRatio) && !is.na(rejection.criteria$recycling.ratio))
+    if (!anyNA(RecyclingRatio) && !is.na(rejection.criteria$recycling.ratio)) {
       temp.status.RecyclingRatio[abs(1 - RecyclingRatio) > (
         rejection.criteria$recycling.ratio / 100)] <- "FAILED"
+
+      ## set better ratio by given the absolute margin depending
+      ## on whether we are have values larger or smaller 1
+      if(RecyclingRatio > 1)
+        temp.threshold[1:length(RecyclingRatio)] <- temp.threshold[1:length(RecyclingRatio)] + 1
+      else
+        temp.threshold[1:length(RecyclingRatio)] <- 1 - temp.threshold[1:length(RecyclingRatio)]
+
+    }
 
     ##Recuperation
     temp.status.Recuperation <- sapply(Recuperation, function(value) {
@@ -1341,10 +1351,14 @@ if(is.list(object)){
     lines(x = c(0.1,1), y = rep(y_coord_l[i],2), lwd = 0.25)
   }
 
+  ## set labels
+  x$Value <- round(x$Value, 1)
+  x$Threshold <- round(x$Threshold, 2)
+  x$sign <- ifelse(x$Value > x$Threshold, ">=", "<=")
   text(
     x = 0.8,
     y = y_coord_l,
-    labels = paste0(round(x$Value, 1), " <> ", round(x$Threshold, 2)),
+    labels = paste(x$Value, x$sign, x$Threshold),
     cex = 0.6,
     adj = c(1, 1.5))
 

DESCRIPTION
@@ -1,7 +1,7 @@
 Package: Luminescence
 Type: Package
 Title: Comprehensive Luminescence Dating Data Analysis
-Version: 1.1.1.9000-1
+Version: 1.1.1.9000-2
 Date: 2025-11-25
 Authors@R: c(
     person("Sebastian", "Kreutzer", role = c("aut", "trl", "cre", "dtc"), email = "maintainer_luminescence@r-luminescence.org", comment = c(ORCID = "0000-0002-0734-2199")),

commit d81ed4d26674153b3cf6f3b8d9a5e0056eeb9046

Fix test failures in analyse_SAR.CWOSL().
These changes are required to fix the failures introduced in b43b2741.

The change regarding `if (RecyclingRatio > 1)` is a tentative fix to solve
a crash due to the condition in the if having length > 1.

Note that the analyse_pIRISequence() snapshot change indicates that more
fixes are required.

R/analyse_SAR.CWOSL.R
@@ -742,12 +742,11 @@ if(is.list(object)){
         rejection.criteria$recycling.ratio / 100)] <- "FAILED"
 
       ## set better ratio by given the absolute margin depending
-      ## on whether we are have values larger or smaller 1
-      if(RecyclingRatio > 1)
-        temp.threshold[1:length(RecyclingRatio)] <- temp.threshold[1:length(RecyclingRatio)] + 1
-      else
-        temp.threshold[1:length(RecyclingRatio)] <- 1 - temp.threshold[1:length(RecyclingRatio)]
-
+      ## on whether we have values larger or smaller than 1
+      idx.gt1 <- which(RecyclingRatio > 1)
+      temp.threshold[idx.gt1] <- temp.threshold[idx.gt1] + 1
+      idx.lt1 <- which(RecyclingRatio < 1)
+      temp.threshold[idx.lt1] <- 1 - temp.threshold[idx.lt1]
     }
 
     ##Recuperation

tests/testthat/_snaps/analyse_SAR.CWOSL/ch-tl-log-x.svg
@@ -743,11 +743,11 @@
 <polyline points='630.08,337.13 713.28,337.13 ' style='stroke-width: 0.19;' />
 <polyline points='630.08,301.18 713.28,301.18 ' style='stroke-width: 0.19;' />
 <polyline points='630.08,265.24 713.28,265.24 ' style='stroke-width: 0.19;' />
-<text x='694.79' y='413.48' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='19.46px' lengthAdjust='spacingAndGlyphs'>1.1 &lt;&gt; 0.1</text>
-<text x='694.79' y='377.54' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='15.86px' lengthAdjust='spacingAndGlyphs'>0 &lt;&gt; 0.1</text>
-<text x='694.79' y='341.59' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='15.86px' lengthAdjust='spacingAndGlyphs'>0 &lt;&gt; 0.1</text>
-<text x='694.79' y='305.64' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='15.86px' lengthAdjust='spacingAndGlyphs'>0 &lt;&gt; 0.1</text>
-<text x='694.79' y='269.70' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='30.27px' lengthAdjust='spacingAndGlyphs'>1760.7 &lt;&gt; 2550</text>
+<text x='694.79' y='413.48' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='19.46px' lengthAdjust='spacingAndGlyphs'>1.1 &lt;= 1.1</text>
+<text x='694.79' y='377.54' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='15.86px' lengthAdjust='spacingAndGlyphs'>0 &lt;= 0.1</text>
+<text x='694.79' y='341.59' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='15.86px' lengthAdjust='spacingAndGlyphs'>0 &lt;= 0.1</text>
+<text x='694.79' y='305.64' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='15.86px' lengthAdjust='spacingAndGlyphs'>0 &lt;= 0.1</text>
+<text x='694.79' y='269.70' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='30.27px' lengthAdjust='spacingAndGlyphs'>1760.7 &lt;= 2550</text>
 <circle cx='708.66' cy='427.00' r='2.11' style='stroke-width: 0.75; stroke: #61D04F; fill: #61D04F;' />
 <circle cx='708.66' cy='391.05' r='2.11' style='stroke-width: 0.75; stroke: #61D04F; fill: #61D04F;' />
 <circle cx='708.66' cy='355.10' r='2.11' style='stroke-width: 0.75; stroke: #61D04F; fill: #61D04F;' />

tests/testthat/_snaps/analyse_SAR.CWOSL/default.svg
@@ -777,11 +777,11 @@
 <polyline points='630.08,337.13 713.28,337.13 ' style='stroke-width: 0.19;' />
 <polyline points='630.08,301.18 713.28,301.18 ' style='stroke-width: 0.19;' />
 <polyline points='630.08,265.24 713.28,265.24 ' style='stroke-width: 0.19;' />
-<text x='694.79' y='413.48' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='19.46px' lengthAdjust='spacingAndGlyphs'>1.1 &lt;&gt; 0.1</text>
-<text x='694.79' y='377.54' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='15.86px' lengthAdjust='spacingAndGlyphs'>0 &lt;&gt; 0.1</text>
-<text x='694.79' y='341.59' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='15.86px' lengthAdjust='spacingAndGlyphs'>0 &lt;&gt; 0.1</text>
-<text x='694.79' y='305.64' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='15.86px' lengthAdjust='spacingAndGlyphs'>0 &lt;&gt; 0.1</text>
-<text x='694.79' y='269.70' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='30.27px' lengthAdjust='spacingAndGlyphs'>1668.3 &lt;&gt; 2550</text>
+<text x='694.79' y='413.48' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='19.46px' lengthAdjust='spacingAndGlyphs'>1.1 &lt;= 1.1</text>
+<text x='694.79' y='377.54' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='15.86px' lengthAdjust='spacingAndGlyphs'>0 &lt;= 0.1</text>
+<text x='694.79' y='341.59' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='15.86px' lengthAdjust='spacingAndGlyphs'>0 &lt;= 0.1</text>
+<text x='694.79' y='305.64' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='15.86px' lengthAdjust='spacingAndGlyphs'>0 &lt;= 0.1</text>
+<text x='694.79' y='269.70' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='30.27px' lengthAdjust='spacingAndGlyphs'>1668.3 &lt;= 2550</text>
 <circle cx='708.66' cy='427.00' r='2.11' style='stroke-width: 0.75; stroke: #61D04F; fill: #61D04F;' />
 <circle cx='708.66' cy='391.05' r='2.11' style='stroke-width: 0.75; stroke: #61D04F; fill: #61D04F;' />
 <circle cx='708.66' cy='355.10' r='2.11' style='stroke-width: 0.75; stroke: #61D04F; fill: #61D04F;' />

tests/testthat/_snaps/analyse_SAR.CWOSL/list-cex.svg
@@ -750,11 +750,11 @@
 <polyline points='629.73,348.71 710.66,348.71 ' style='stroke-width: 0.19;' />
 <polyline points='629.73,318.81 710.66,318.81 ' style='stroke-width: 0.19;' />
 <polyline points='629.73,288.91 710.66,288.91 ' style='stroke-width: 0.19;' />
-<text x='692.67' y='416.98' text-anchor='end' style='font-size: 8.21px; font-family: sans;' textLength='30.13px' lengthAdjust='spacingAndGlyphs'>1 &lt;&gt; 0.1</text>
-<text x='692.67' y='387.08' text-anchor='end' style='font-size: 8.21px; font-family: sans;' textLength='30.13px' lengthAdjust='spacingAndGlyphs'>0 &lt;&gt; 0.1</text>
-<text x='692.67' y='357.18' text-anchor='end' style='font-size: 8.21px; font-family: sans;' textLength='30.13px' lengthAdjust='spacingAndGlyphs'>0 &lt;&gt; 0.1</text>
-<text x='692.67' y='327.28' text-anchor='end' style='font-size: 8.21px; font-family: sans;' textLength='30.13px' lengthAdjust='spacingAndGlyphs'>0 &lt;&gt; 0.1</text>
-<text x='692.67' y='297.38' text-anchor='end' style='font-size: 8.21px; font-family: sans;' textLength='57.52px' lengthAdjust='spacingAndGlyphs'>1748.3 &lt;&gt; 2550</text>
+<text x='692.67' y='416.98' text-anchor='end' style='font-size: 8.21px; font-family: sans;' textLength='30.13px' lengthAdjust='spacingAndGlyphs'>1 &lt;= 1.1</text>
+<text x='692.67' y='387.08' text-anchor='end' style='font-size: 8.21px; font-family: sans;' textLength='30.13px' lengthAdjust='spacingAndGlyphs'>0 &lt;= 0.1</text>
+<text x='692.67' y='357.18' text-anchor='end' style='font-size: 8.21px; font-family: sans;' textLength='30.13px' lengthAdjust='spacingAndGlyphs'>0 &lt;= 0.1</text>
+<text x='692.67' y='327.28' text-anchor='end' style='font-size: 8.21px; font-family: sans;' textLength='30.13px' lengthAdjust='spacingAndGlyphs'>0 &lt;= 0.1</text>
+<text x='692.67' y='297.38' text-anchor='end' style='font-size: 8.21px; font-family: sans;' textLength='57.52px' lengthAdjust='spacingAndGlyphs'>1748.3 &lt;= 2550</text>
 <circle cx='706.16' cy='423.45' r='4.00' style='stroke-width: 0.75; stroke: #61D04F; fill: #61D04F;' />
 <circle cx='706.16' cy='393.56' r='4.00' style='stroke-width: 0.75; stroke: #61D04F; fill: #61D04F;' />
 <circle cx='706.16' cy='363.66' r='4.00' style='stroke-width: 0.75; stroke: #61D04F; fill: #61D04F;' />

tests/testthat/_snaps/analyse_SAR.CWOSL/no-tl-log-xy.svg
@@ -710,11 +710,11 @@
 <polyline points='630.08,337.13 713.28,337.13 ' style='stroke-width: 0.19;' />
 <polyline points='630.08,301.18 713.28,301.18 ' style='stroke-width: 0.19;' />
 <polyline points='630.08,265.24 713.28,265.24 ' style='stroke-width: 0.19;' />
-<text x='694.79' y='413.48' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='15.86px' lengthAdjust='spacingAndGlyphs'>1 &lt;&gt; 0.1</text>
-<text x='694.79' y='377.54' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='15.86px' lengthAdjust='spacingAndGlyphs'>0 &lt;&gt; 0.1</text>
-<text x='694.79' y='341.59' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='15.86px' lengthAdjust='spacingAndGlyphs'>0 &lt;&gt; 0.1</text>
-<text x='694.79' y='305.64' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='15.86px' lengthAdjust='spacingAndGlyphs'>0 &lt;&gt; 0.1</text>
-<text x='694.79' y='269.70' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='30.27px' lengthAdjust='spacingAndGlyphs'>1749.2 &lt;&gt; 2550</text>
+<text x='694.79' y='413.48' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='15.86px' lengthAdjust='spacingAndGlyphs'>1 &lt;= 1.1</text>
+<text x='694.79' y='377.54' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='15.86px' lengthAdjust='spacingAndGlyphs'>0 &lt;= 0.1</text>
+<text x='694.79' y='341.59' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='15.86px' lengthAdjust='spacingAndGlyphs'>0 &lt;= 0.1</text>
+<text x='694.79' y='305.64' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='15.86px' lengthAdjust='spacingAndGlyphs'>0 &lt;= 0.1</text>
+<text x='694.79' y='269.70' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='30.27px' lengthAdjust='spacingAndGlyphs'>1749.2 &lt;= 2550</text>
 <circle cx='708.66' cy='427.00' r='2.11' style='stroke-width: 0.75; stroke: #61D04F; fill: #61D04F;' />
 <circle cx='708.66' cy='391.05' r='2.11' style='stroke-width: 0.75; stroke: #61D04F; fill: #61D04F;' />
 <circle cx='708.66' cy='355.10' r='2.11' style='stroke-width: 0.75; stroke: #61D04F; fill: #61D04F;' />

tests/testthat/_snaps/analyse_SAR.CWOSL/onlylxtxtable.svg
@@ -403,9 +403,9 @@
 <polyline points='630.08,394.65 713.28,394.65 ' style='stroke-width: 0.19;' />
 <polyline points='630.08,329.94 713.28,329.94 ' style='stroke-width: 0.19;' />
 <polyline points='630.08,265.24 713.28,265.24 ' style='stroke-width: 0.19;' />
-<text x='694.79' y='399.10' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='15.86px' lengthAdjust='spacingAndGlyphs'>1 &lt;&gt; 0.1</text>
-<text x='694.79' y='334.40' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='15.86px' lengthAdjust='spacingAndGlyphs'>0 &lt;&gt; 0.1</text>
-<text x='694.79' y='269.70' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='15.86px' lengthAdjust='spacingAndGlyphs'>0 &lt;&gt; 0.1</text>
+<text x='694.79' y='399.10' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='15.86px' lengthAdjust='spacingAndGlyphs'>1 &lt;= 1.1</text>
+<text x='694.79' y='334.40' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='15.86px' lengthAdjust='spacingAndGlyphs'>0 &lt;= 0.1</text>
+<text x='694.79' y='269.70' text-anchor='end' style='font-size: 4.32px; font-family: sans;' textLength='15.86px' lengthAdjust='spacingAndGlyphs'>0 &lt;= 0.1</text>
 <circle cx='708.66' cy='427.00' r='2.11' style='stroke-width: 0.75; stroke: #61D04F; fill: #61D04F;' />
 <circle cx='708.66' cy='362.29' r='2.11' style='stroke-width: 0.75; stroke: #61D04F; fill: #61D04F;' />
 <circle cx='708.66' cy='297.59' r='2.11' style='stroke-width: 0.75; stroke: #61D04F; fill: #61D04F;' />