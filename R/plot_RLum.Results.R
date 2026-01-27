395ac05efb744342b3a856ed4976607daa842ce1
Marco Colombo <marco.colombo@uni-heidelberg.de>
2026-01-27 15:36:54 +0100

    Revert "plot_RLum.Results: Remove warning when printing a calc_MinDose() object."
    This reverts commit f4a92093188d59b0fa631f45eed8237aab90f91c because it
    caused failures in the calc_MinDose() tests.
    
    [skip ci]

--- R/plot_RLum.Results.R
+++ R/plot_RLum.Results.R
@@ -141,7 +141,7 @@ plot_RLum.Results<- function(
       tryCatch({
 
         xvals <- as.data.frame(profiles@profile[[i]]$par.vals)[[i]]
-        xlim <- range(xvals[xvals > 0, na.rm = TRUE])
+        xlim <- range(xvals[xvals > 0])
         suppressWarnings(
           bbmle::plot(profiles, which = i, xlab = "", xaxt = "n", xlim = xlim)
         )
