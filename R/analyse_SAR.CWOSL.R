R-Lum/Luminescence

commit fb4c4479a9e47520f5f2675aba0f479d1ba90faf

analyse_SAR.CWOSL: Clean up computation of recuperation rate.

R/analyse_SAR.CWOSL.R
@@ -736,16 +736,11 @@ analyse_SAR.CWOSL<- function(
 
   ## Recuperation Rate (capable of handling multiple type of recuperation values)
   if ("R0" %in% LnLxTnTx$Name) {
-    Recuperation <- LnLxTnTx$LxTx[LnLxTnTx$Name == "R0"] /
-      LnLxTnTx$LxTx[LnLxTnTx$Name == recuperation_reference]
-
-     ##transform and name
-     Recuperation <- t(setNames(
-        object = Recuperation,
-        nm = paste0(
-          "Recuperation rate (", recuperation_reference, ") ",
-          seq_along(Recuperation))
-      ))
+    R0 <- LnLxTnTx$LxTx[LnLxTnTx$Name == "R0"]
+    Rref <- LnLxTnTx$LxTx[LnLxTnTx$Name == recuperation_reference]
+    labels <- paste0("Recuperation rate (", recuperation_reference, ") ",
+                     seq_along(Recuperation))
+    Recuperation <- t(setNames(R0 / Rref, labels))
   }
 
     # Evaluate and Combine Rejection Criteria ---------------------------------

commit 5e402a4b40ce7c6123926f0f7bd6ca1280ba653f

analyse_SAR.CWOSL: Label correctly multiple recuperation rates in plot.
This fixes a regression from fb4c4479.

R/analyse_SAR.CWOSL.R
@@ -789,7 +789,7 @@ analyse_SAR.CWOSL<- function(
     R0 <- LnLxTnTx$LxTx[LnLxTnTx$Name == "R0"]
     Rref <- LnLxTnTx$LxTx[LnLxTnTx$Name == recuperation_reference]
     labels <- paste0("Recuperation rate (", recuperation_reference, ") ",
-                     seq_along(Recuperation))
+                     seq_along(R0))
     Recuperation <- t(setNames(R0 / Rref, labels))
   }
 

tests/testthat/_snaps/analyse_SAR.CWOSL/multiple-recuperation-rates.svg
@@ -847,6 +847,8 @@
 <text x='702.19' y='377.46' text-anchor='end' style='font-size: 6.48px; font-family: sans;' textLength='70.59px' lengthAdjust='spacingAndGlyphs'>Recycling ratio (R11/R4)</text>
 <text x='702.19' y='364.52' text-anchor='end' style='font-size: 6.48px; font-family: sans;' textLength='70.59px' lengthAdjust='spacingAndGlyphs'>Recycling ratio (R12/R1)</text>
 <text x='702.19' y='351.58' text-anchor='end' style='font-size: 6.48px; font-family: sans;' textLength='83.93px' lengthAdjust='spacingAndGlyphs'>Recuperation rate (Natural) 1</text>
+<text x='702.19' y='338.64' text-anchor='end' style='font-size: 6.48px; font-family: sans;' textLength='83.93px' lengthAdjust='spacingAndGlyphs'>Recuperation rate (Natural) 2</text>
+<text x='702.19' y='325.70' text-anchor='end' style='font-size: 6.48px; font-family: sans;' textLength='83.93px' lengthAdjust='spacingAndGlyphs'>Recuperation rate (Natural) 3</text>
 <text x='702.19' y='312.76' text-anchor='end' style='font-size: 6.48px; font-family: sans;' textLength='42.14px' lengthAdjust='spacingAndGlyphs'>Testdose error</text>
 <text x='702.19' y='299.82' text-anchor='end' style='font-size: 6.48px; font-family: sans;' textLength='57.63px' lengthAdjust='spacingAndGlyphs'>Signal-to-noise ratio</text>
 <text x='702.19' y='286.88' text-anchor='end' style='font-size: 6.48px; font-family: sans;' textLength='49.72px' lengthAdjust='spacingAndGlyphs'>Palaeodose error</text>