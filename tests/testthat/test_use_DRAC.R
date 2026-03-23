a0a0fe1be909784e8f1e3b31c7b8f86bfe05bfa5
Marco Colombo <marco.colombo@uni-heidelberg.de>
2026-03-23 11:46:31 +0100

    use_DRAC: Add support for the DRAC v1.3 input template.

--- tests/testthat/test_use_DRAC.R
+++ tests/testthat/test_use_DRAC.R
@@ -1,3 +1,6 @@
+template_v1.2 <- test_path("_data/DRAC_Input_Template_v1.2.csv")
+template_v1.3 <- test_path("_data/DRAC_Input_Template_v1.3.csv")
+
 test_that("input validation", {
   testthat::skip_on_cran()
 
@@ -17,14 +20,14 @@ test_that("input validation", {
                "File extension 'xls' is not supported, only 'csv' is valid")
 
   ## CSV file with the wrong header
-  fake <- data.table::fread(test_path("_data/DRAC_Input_Template.csv"))
+  fake <- data.table::fread(template_v1.2)
   fake[1, 1] <- "error"
   fake.csv <- tempfile(fileext = ".csv")
   data.table::fwrite(fake, file = fake.csv)
   expect_error(use_DRAC(fake.csv),
-               "you are not using the original DRAC v1.2 CSV template")
+               "you are not using the original DRAC v1.2 or v1.3 CSV template")
 
-  expect_error(use_DRAC(test_path("_data/DRAC_Input_Template.csv"), NA),
+  expect_error(use_DRAC(template_v1.2, NA),
                "'name' should be of class 'character' or NULL")
 
   ## exceed allowed limit
@@ -99,7 +102,7 @@ test_that("check functionality", {
   SW({
   ## CSV input
   expect_message(expect_s4_class(
-      use_DRAC(test_path("_data/DRAC_Input_Template.csv"),
+      use_DRAC(template_v1.2,
                verbose = TRUE),
       "RLum.Results"),
       "Reference for")
@@ -128,6 +131,10 @@ test_that("check functionality", {
 
 test_that("snapshot tests", {
   testthat::skip_on_cran()
+
+  SW({
+  expect_snapshot_output(use_DRAC(template_v1.3))
+  })
 
   ## communicate with insufficient input
   t <- template_DRAC(preset = "DRAC-example_quartz", notification = FALSE)
