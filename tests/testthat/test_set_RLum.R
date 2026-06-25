0fe7e470fb97db6f83122c0d57b7ea5e6896abdd
Marco Colombo <marco.colombo@uni-heidelberg.de>
2026-06-25 12:21:02 +0200

    test_set_RLum: Add originator tests.

--- tests/testthat/test_set_RLum.R
+++ tests/testthat/test_set_RLum.R
@@ -1,3 +1,6 @@
+## load data
+data(ExampleData.LxTxData, envir = environment())
+
 test_that("input validation", {
   testthat::skip_on_cran()
 
@@ -9,4 +12,21 @@ test_that("input validation", {
                "'class' should be of class 'character' and have length 1")
   expect_error(set_RLum(""),
                "'class' cannot be an empty character")
+})
+
+test_that("check functionality", {
+  testthat::skip_on_cran()
+
+  expect_equal(set_RLum("RLum.Data.Curve")@originator,
+               "eval_bare") # TODO(mcol): ideally it should be NA_character_
+  expect_equal(set_RLum("RLum.Data.Curve", originator = "test")@originator,
+               "test")
+  expect_equal(fit_DoseResponseCurve(LxTxData, verbose = FALSE)@originator,
+               "fit_DoseResponseCurve")
+
+  ## qualified calls
+  expect_equal(Luminescence::fit_DoseResponseCurve(LxTxData, verbose = FALSE)@originator,
+               "fit_DoseResponseCurve")
+  expect_equal(Luminescence:::fit_DoseResponseCurve(LxTxData, verbose = FALSE)@originator,
+               "fit_DoseResponseCurve")
 })
