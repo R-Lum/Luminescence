R-Lum/Luminescence

commit c4c182aac0599fecef740f03c86cdde614fb9e03

calc_Statistics: Don't crash if an error is 0.

R/calc_Statistics.R
@@ -94,8 +94,11 @@ calc_Statistics <- function(
   ## replace Na values in error by 0
   data[is.na(data[,2]),2] <- 0
 
-  if(sum(data[,2]) == 0) {
-    .throw_warning("All errors are NA or zero, automatically set to 10^-9")
+  ## replace zeros by a small value
+  if (any(data[, 2] == 0)) {
+    if (sum(data[, 2]) == 0) {
+      .throw_warning("All errors are NA or zero, automatically set to 10^-9")
+    }
     data[,2] <- rep(x = 10^-9, length(data[,2]))
   }
 

tests/testthat/test_calc_Statistics.R
@@ -55,4 +55,7 @@ test_that("snapshot tests", {
   expect_snapshot_plain(calc_Statistics(
       ExampleData.DeValues$BT998, n.MCM = 1000, digits = 2),
       tolerance = snapshot.tolerance)
+
+  expect_snapshot_plain(calc_Statistics(data.frame(1:10, 0:9)),
+                        tolerance = snapshot.tolerance)
 })

commit f729a2533ec5b8efa011665649492f3408e7af50

calc_Statistics: Don't ignore the error column if any error is 0.
This fixes a regression introduced in c4c182aa.

R/calc_Statistics.R
@@ -33,7 +33,7 @@
 #'
 #' @return Returns a list with weighted and unweighted statistic measures.
 #'
-#' @section Function version: 0.1.8
+#' @section Function version: 0.1.9
 #'
 #' @keywords datagen
 #'
@@ -88,19 +88,9 @@ calc_Statistics <- function(
   }
 
   ## handle error-free data sets
-  if(ncol(data) == 1) {
-    data <- cbind(data, rep(NA, length(data)))
-  }
-
-  ## replace Na values in error by 0
-  data[is.na(data[,2]),2] <- 0
-
-  ## replace zeros by a small value
-  if (any(data[, 2] == 0)) {
-    if (sum(data[, 2]) == 0) {
-      .throw_warning("All errors are NA or zero, automatically set to 10^-9")
-    }
-    data[,2] <- rep(x = 10^-9, length(data[,2]))
+  if (ncol(data) == 1 || all(data[, 2] == 0)) {
+    .throw_warning("All errors are NA or zero, automatically set to 10^-9")
+    data[, 2] <- 1e-9
   }
 
   ## deprecated names
@@ -122,7 +112,6 @@ calc_Statistics <- function(
   }
 
   S.weights <- S.weights / sum(S.weights)
-
   .validate_positive_scalar(digits, int = TRUE, null.ok = TRUE)
   .validate_positive_scalar(n.MCM, int = TRUE, null.ok = TRUE)
 

R/internals_RLum.R
@@ -230,6 +230,8 @@
   x <- x[ox]
   w <- w[ox]
   p <- cumsum(w) / sum(w)
+  if (any(is.nan(p)))
+    return(NA_real_)
   n <- sum(p < 0.5)
   if (p[n + 1] > 0.5)
     return(x[n + 1])

man/calc_Statistics.Rd
@@ -50,7 +50,7 @@ values. See Dietze et al. (2016, Quaternary Geochronology) and the function
 \link{plot_AbanicoPlot} for details.
 }
 \section{Function version}{
- 0.1.8
+ 0.1.9
 }
 
 \examples{

tests/testthat/test_calc_Statistics.R
@@ -34,6 +34,8 @@ test_that("input validation", {
                "'data' cannot be an empty data.frame")
   expect_error(calc_Statistics(data.frame(a = NA)),
                "'data' contains only NA values")
+  expect_error(calc_Statistics(data.frame(a = 1:10, b = NA)),
+               "'data' contains only NA values")
   expect_error(calc_Statistics(data = df, weight.calc = "error"),
                "'weight.calc' should be one of 'inverse_var', 'inverse_std'")
   expect_error(calc_Statistics(df, digits = 2.4),

tests/testthat/test_internals.R
@@ -81,6 +81,10 @@ test_that("Test internals", {
   ## .weighted.median() -----------------------------------------------------
   expect_equal(.weighted.median(1:10, w = rep(1, 10)),
                median(1:10))
+  expect_equal(.weighted.median(1:10, w = c(1:9, Inf)),
+               NA_real_)
+  expect_equal(.weighted.median(1:10, w = c(NaN, 2:10)),
+               NA_real_)
   expect_equal(.weighted.median(1:5, w = c(0.15, 0.1, 0.2, 0.3, 0.25)),
                4)
   expect_equal(.weighted.median(c(1:5, NA), w = c(0.15, 0.1, 0.2, 0.3, 0.25, 1)),