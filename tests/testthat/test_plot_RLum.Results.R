59e22bcbdb9fba8de053bbdf52f546ea860577b3
Marco Colombo <marco.colombo@uni-heidelberg.de>
2026-09-10 16:26:37 +0200

    test_plot_RLum.Results: Fix text expectations.
    This is required because of the changes introduced in 080114e9.

--- tests/testthat/test_plot_RLum.Results.R
+++ tests/testthat/test_plot_RLum.Results.R
@@ -83,7 +83,8 @@ test_that("check functionality", {
   data(ExampleData.RLum.Analysis, envir = environment())
   d8 <- analyse_IRSAR.RF(IRSAR.RF.Data, method = "VSLIDE", n.MC = 10,
                          plot = FALSE, verbose = FALSE)
-  expect_silent(plot_RLum.Results(d8))
+  expect_warning(plot_RLum.Results(d8),
+                 "Data set 1 contains a single point, its density curve cannot")
 
   ## no valid originator
   expect_silent(plot_RLum.Results(set_RLum("RLum.Results",
