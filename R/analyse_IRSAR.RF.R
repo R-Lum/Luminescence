R-Lum/Luminescence

commit f3eb84c287c2553e4673493406753538865fe27b

Uniform the cores argument and its validation (#1669)
This removes support for the multicore argument and introduces the
.validate_cores() helper function to consolidate some code.

R/analyse_IRSAR.RF.R
@@ -103,10 +103,6 @@
 #' the more time is spent in searching the global optimum. The default setting
 #' attempts to strike a balance between quality of the fit and computation
 #' speed.
-#' - `cores` ([numeric] or [character], default: `NULL`): number of cores
-#' allocated for a parallel processing of the Monte-Carlo runs. The default
-#' value corresponds to single-threaded computation; the recommended values is
-#' `"auto"`, which assigns all but two of the available cores.
 #'
 #' **Error estimation**
 #'
@@ -236,6 +232,12 @@
 #' e.g., `par(mfrow(...))`. If `TRUE` no residual plot
 #' is returned; it has no effect if `plot = FALSE`
 #'
+#' @param cores [integer], [numeric] (*with default*):
+#' number of cores allocated for parallel processing of the Monte-Carlo runs.
+#' The default value corresponds to single-threaded computation; the
+#' recommended values is `NULL`, which assigns all but two of the available
+#' logical CPU cores.
+#'
 #' @param ... further arguments that will be passed to the plot output.
 #' Currently supported arguments are `main`, `mtext`, `xlab`, `ylab`,
 #' `xlim`, `ylim`, `log`, `legend` (`TRUE/FALSE`),
@@ -332,7 +334,7 @@
 #' measurements (natural vs. regenerated signal), which is in contrast to the
 #' findings by Buylaert et al. (2012).
 #'
-#' @section Function version: 0.7.12
+#' @section Function version: 0.7.13
 #'
 #' @author Sebastian Kreutzer, F2.1 Geophysical Parametrisation/Regionalisation, LIAG - Institute for Applied Geophysics (Germany)
 #'
@@ -451,6 +453,7 @@ analyse_IRSAR.RF<- function(
   txtProgressBar = TRUE,
   plot = TRUE,
   plot_reduced = FALSE,
+  cores = 1,
   ...
 ) {
   .set_function_name("analyse_IRSAR.RF")
@@ -504,6 +507,7 @@ analyse_IRSAR.RF<- function(
         txtProgressBar = txtProgressBar,
         plot = plot,
         plot_reduced = plot_reduced,
+        cores = cores,
         main = temp_main[[x]],
         ...)
     })
@@ -662,8 +666,7 @@ analyse_IRSAR.RF<- function(
     show_fit = FALSE,
     n.MC = if(is.null(n.MC)) NULL else 1000,
     vslide_range = if(method[1] == "VSLIDE") "auto" else NULL,
-    num_slide_windows = 3,
-    cores = NULL
+    num_slide_windows = 3
   )
 
   ##modify list if necessary
@@ -1116,42 +1119,14 @@ analyse_IRSAR.RF<- function(
       })
 
       ##set parallel calculation if wanted
-      if (is.null(method_control.settings$cores)) {
-        cores <- 1
-
-      } else {
-        available.cores <- parallel::detectCores()
-        requested.cores <- method_control.settings$cores[1]
-
-        ##case 'auto'
-        if (requested.cores == "auto") {
-          cores <- max(available.cores - 2, 1) # nocov
-
-        } else if (is.numeric(requested.cores)) {
-          .validate_positive_scalar(requested.cores, int = TRUE,
-                                    name = "method_control.settings$cores")
-          if (requested.cores > available.cores) {
-            ##assign all they have, it is not our problem
-            # nocov start
-            .throw_warning("Number of cores limited to the maximum ",
-                           "available (", available.cores, ")")
-            # nocov end
-          }
-          cores <- min(requested.cores, available.cores)
-
-        }else{
-          .throw_message("Invalid value for control argument 'cores', ",
-                         "value set to 1")
-          cores <- 1
-        }
-
-        if (verbose)
-          .throw_message("Using ", cores, ifelse(cores == 1, " core", " cores"),
-                         " ...", error = FALSE)
+      cores <- .validate_cores(cores)
+      if (verbose) {
+        .throw_message("Using ", cores, ifelse(cores == 1, " core", " cores"),
+                       " ...", error = FALSE)
       }
 
       ## SINGLE CORE -----
-      if (cores[1] == 1) {
+      if (cores == 1) {
         if(txtProgressBar){
           ##progress bar
           cat("\n\t Run Monte Carlo loops for error estimation\n")

R/calc_Huntley2006.R
@@ -170,9 +170,11 @@
 #' when `fit.method = "SSE"`). More details can be found in
 #' [Luminescence::fit_DoseResponseCurve].
 #'
-#' @param cores [integer] (*with default*):
-#' The number of cores to use. This will be capped to the number of available
-#' cores if set to too high.
+#' @param cores [integer], [numeric] (*with default*):
+#' number of cores allocated for parallel processing of the Monte-Carlo runs.
+#' The default value corresponds to single-threaded computation; the
+#' recommended values is `NULL`, which assigns all but two of the available
+#' logical CPU cores.
 #'
 #' @param summary [logical] (*with default*):
 #' If `TRUE` (the default) various parameters provided by the user
@@ -220,7 +222,7 @@
 #' `args` \tab `list` \tab arguments of the original function call \cr
 #' }
 #'
-#' @section Function version: 0.4.7
+#' @section Function version: 0.4.8
 #'
 #' @author
 #' Georgina E. King, University of Lausanne (Switzerland) \cr
@@ -247,7 +249,6 @@
 #'
 #' King, G.E., Herman, F., Lambert, R., Valla, P.G., Guralnik, B., 2016.
 #' Multi-OSL-thermochronometry of feldspar. Quaternary Geochronology 33, 76-87. doi:10.1016/j.quageo.2016.01.004
-#'
 #'
 #' **Further reading**
 #'
@@ -410,8 +411,7 @@ calc_Huntley2006 <- function(
   .validate_class(readerDdot, "numeric", length = 2)
 
   ## set up the parallel cluster
-  .validate_positive_scalar(cores, int = TRUE)
-  cores <- min(cores, parallel::detectCores())
+  cores <- .validate_cores(cores)
   cl <- parallel::makeCluster(cores)
   on.exit(parallel::stopCluster(cl), add = TRUE)
 

R/calc_MinDose.R
@@ -67,12 +67,11 @@
 #'
 #' **Multicore support**
 #'
-#' Parallel processing can be enabled by setting `multicore = TRUE`. By default,
-#' the number of logical CPU cores is detected automatically, but it can be
-#' overridden with the `cores` argument. Multicore processing is supported
+#' Parallel processing can be enabled by setting the `cores` argument to a
+#' value greater than 1 (or to `NULL` to use all but two of the available
+#' logical CPU cores). Multicore processing is supported
 #' only when `bootstrap = TRUE`, and spawns one R process per core to compute
-#' MAM estimates for each of the `N * M` bootstrap replicates. Note that this
-#' feature is experimental and may not work on all systems. Performance gains
+#' MAM estimates for each of the `N * M` bootstrap replicates. Performance gains
 #' grow with the number of bootstrap replicates, but each additional core (R
 #' process) increases memory usage. When memory is insufficient, overall
 #' performance can degrade severely.
@@ -134,18 +133,17 @@
 #' @param plot [logical] (*with default*):
 #' enable/disable the plot output.
 #'
-#' @param multicore [logical] (*with default*):
-#' parallelize the computation of the bootstrap by creating a multicore cluster
-#' (only considered if `bootstrap = TRUE`). By default, it uses all available
-#' logical CPU cores, but this can be changed with the `cores` argument. Note
-#' that this feature is experimental and may not work on all systems.
+#' @param cores [integer], [numeric] (*with default*):
+#' number of cores allocated for parallel processing of the bootstrap step
+#' (only considered if `bootstrap = TRUE`). The default value corresponds to
+#' single-threaded computation; the recommended value is `NULL`, which assigns
+#' all but two of the available logical CPU cores.
 #'
 #' @param ... (*optional*) further arguments for bootstrapping
 #' (`bs.M`, `bs.N`, `bs.h`, `sigmab.sd`). See details for their usage.
 #' Further arguments are
 #' - `verbose`: enable/disable output to the terminal
 #' - `debug`: enable/disable extended console output
-#' - `cores`: number of cores to be used when `multicore=TRUE`
 #'
 #' @return Returns a plot (*optional*) and terminal output. In addition an
 #' [Luminescence::RLum.Results-class] object is returned containing the
@@ -173,7 +171,7 @@
 #' model with `debug=TRUE` which provides extended console output and
 #' forwards all internal warning messages.
 #'
-#' @section Function version: 0.6.0
+#' @section Function version: 0.6.1
 #'
 #' @author
 #' Christoph Burow, University of Cologne (Germany) \cr
@@ -333,7 +331,7 @@ calc_MinDose <- function(
   level = 0.95,
   log.output = FALSE,
   plot = TRUE,
-  multicore = FALSE,
+  cores = 1,
   ...
 ) {
   .set_function_name("calc_MinDose")
@@ -387,13 +385,18 @@ calc_MinDose <- function(
   .validate_logical_scalar(bootstrap)
   .validate_logical_scalar(log.output)
   .validate_logical_scalar(plot)
-  .validate_logical_scalar(multicore)
+  cores <- .validate_cores(cores)
 
   ##============================================================================##
   ## ... ARGUMENTS
   ##============================================================================##
 
   extraArgs <- list(...)
+
+  ## deprecated argument
+  if ("multicore" %in% ...names()) {
+    .deprecated("multicore", "cores", since = "1.3.1")
+  }
 
   ## check if this function is called by calc_MaxDose()
   if ("invert" %in% names(extraArgs)) {
@@ -451,14 +454,8 @@ calc_MinDose <- function(
     debug <- FALSE
   }
 
-  if (multicore && "cores" %in% names(extraArgs)) {
-    cores <- .validate_positive_scalar(extraArgs$cores,
-                                       int = TRUE, name = "'cores'")
-  } else {
-    cores <- ifelse(multicore, parallel::detectCores(), 1)
-    if (multicore && verbose)
-      message("Logical CPU cores detected: ", cores) # nocov
-  }
+  if (cores > 1 && verbose)
+    .throw_message("Logical CPU cores detected: ", cores, error = FALSE)
 
   ## remove non-positive values if using log-transformation
   if (log) {
@@ -814,7 +811,7 @@ calc_MinDose <- function(
               " cores, this may take a while...")
     }
 
-    if (multicore) {
+    if (cores > 1) {
       cl <- parallel::makeCluster(cores)
       mle <- parallel::parLapply(cl, replicates, Get_mle)
       parallel::stopCluster(cl)

R/internals_RLum.R
@@ -1586,9 +1586,8 @@ SW <- function(expr) {
 
 #' @title Validate the originator of an RLum object
 #'
-#' @param object [Luminescence::RLum-class] (**required**): object whose
-#' originator should be
-#'        checked.
+#' @param object [Luminescence::RLum-class] (**required**):
+#' object whose originator should be checked.
 #' @inheritParams .validate_args
 #'
 #' @return
@@ -1613,6 +1612,31 @@ SW <- function(expr) {
 #' @noRd
 .check_originator <- function(object, choices) {
   .hasSlot(object, "originator") && object@originator %in% choices
+}
+
+#' @title Validate and compute the number of cores to use
+#'
+#' @param cores [integer], [numeric] (**required**):
+#' Number of cores or `NULL`.
+#'
+#' @return
+#' An integer value corresponding to the minimum between the requested and the
+#' available cores, or the number of available cores - 2 if `cores = NULL`.
+#'
+#' @noRd
+.validate_cores <- function(cores) {
+  .validate_positive_scalar(cores, int = TRUE, null.ok = TRUE, name = "'cores'")
+  available.cores <- parallel::detectCores()
+
+  if (is.null(cores))
+    return(max(available.cores - 2, 1))
+
+  ## numeric input
+  if (cores > available.cores) {
+    .throw_warning("Number of cores limited to the maximum ",
+                   "available (", available.cores, ")")
+  }
+  min(cores, available.cores)
 }
 
 #' Validate a vector used for signal and background integrals

R/plot_DetPlot.R
@@ -74,10 +74,10 @@
 #' @param verbose [logical] (*with default*):
 #' enable/disable output to the terminal.
 #'
-#' @param multicore [logical] (*with default*):
-#' enable/disable multi core calculation if `object` is a [list] of
-#' [Luminescence::RLum.Analysis-class] objects. Can be an [integer] specifying
-#' the number of cores to use.
+#' @param cores [integer], [numeric] (*with default*):
+#' number of cores allocated for parallel processing when `object` is a [list]
+#' of objects. The default value (`NULL`) assigns all but two of the available
+#' logical CPU cores.
 #'
 #' @param plot [logical] (*with default*):
 #' enable/disable the plot output.
@@ -114,7 +114,7 @@
 #' should be checked carefully before running long calculations over hundreds
 #' of channels.
 #'
-#' @section Function version: 0.1.11
+#' @section Function version: 0.1.12
 #'
 #' @author Sebastian Kreutzer, F2.1 Geophysical Parametrisation/Regionalisation, LIAG - Institute for Applied Geophysics (Germany)
 #'
@@ -156,7 +156,7 @@ plot_DetPlot <- function(
   n.channels = NULL,
   show_ShineDownCurve = TRUE,
   respect_RC.Status = FALSE,
-  multicore = TRUE,
+  cores = NULL,
   verbose = TRUE,
   plot = TRUE,
   ...
@@ -166,6 +166,11 @@ plot_DetPlot <- function(
 
 # SELF CALL ---------------------------------------------------------------
   if(inherits(object, "list")) {
+    ## deprecated argument
+    if ("multicore" %in% ...names()) {
+      .deprecated("multicore", "cores", since = "1.3.1")
+    }
+
    ## remove all RLum.Analysis objects
    object <- .rm_nonRLum(x = object, class = "RLum.Analysis")
 
@@ -173,18 +178,13 @@ plot_DetPlot <- function(
    f_def <- sys.call(sys.parent(n = -1))
    args_default <- as.list(f_def)[-(1:2)]
 
-    ## detect cores
-    .validate_class(multicore, c("logical", "numeric", "integer"), length = 1)
-    cores <- if (isTRUE(multicore)) {
-               min(parallel::detectCores(), length(object))
-             } else {
-               max(multicore, 1)
-             }
-
     ## function that calls plot_DetPlot() on each element of the list
     nested.fun <- function(x) {
       do.call(plot_DetPlot, c(list(object = x), args_default))
     }
+
+    ## detect cores
+    cores <- .validate_cores(cores)
 
     if (cores > 1) {
       ## run in parallel

man/analyse_IRSAR.RF.Rd
@@ -16,6 +16,7 @@ analyse_IRSAR.RF(
   txtProgressBar = TRUE,
   plot = TRUE,
   plot_reduced = FALSE,
+  cores = 1,
   ...
 )
 }
@@ -75,6 +76,12 @@ enable/disable the plot output.}
 provides a reduced plot output if enabled to allow common R plot combinations,
 e.g., \code{par(mfrow(...))}. If \code{TRUE} no residual plot
 is returned; it has no effect if \code{plot = FALSE}}
+
+\item{cores}{\link{integer}, \link{numeric} (\emph{with default}):
+number of cores allocated for parallel processing of the Monte-Carlo runs.
+The default value corresponds to single-threaded computation; the
+recommended values is \code{NULL}, which assigns all but two of the available
+logical CPU cores.}
 
 \item{...}{further arguments that will be passed to the plot output.
 Currently supported arguments are \code{main}, \code{mtext}, \code{xlab}, \code{ylab},
@@ -274,10 +281,6 @@ windows tested when sliding: the higher the value (up to a maximum of 10),
 the more time is spent in searching the global optimum. The default setting
 attempts to strike a balance between quality of the fit and computation
 speed.
-\item \code{cores} (\link{numeric} or \link{character}, default: \code{NULL}): number of cores
-allocated for a parallel processing of the Monte-Carlo runs. The default
-value corresponds to single-threaded computation; the recommended values is
-\code{"auto"}, which assigns all but two of the available cores.
 }
 
 \strong{Error estimation}
@@ -354,7 +357,7 @@ measurements (natural vs. regenerated signal), which is in contrast to the
 findings by Buylaert et al. (2012).
 }
 \section{Function version}{
- 0.7.12
+ 0.7.13
 }
 
 \examples{

man/calc_Huntley2006.Rd
@@ -106,9 +106,11 @@ parameters \code{a}, \code{D0}, \code{c} and \code{d} in that order (parameter \
 when \code{fit.method = "SSE"}). More details can be found in
 \link{fit_DoseResponseCurve}.}
 
-\item{cores}{\link{integer} (\emph{with default}):
-The number of cores to use. This will be capped to the number of available
-cores if set to too high.}
+\item{cores}{\link{integer}, \link{numeric} (\emph{with default}):
+number of cores allocated for parallel processing of the Monte-Carlo runs.
+The default value corresponds to single-threaded computation; the
+recommended values is \code{NULL}, which assigns all but two of the available
+logical CPU cores.}
 
 \item{summary}{\link{logical} (\emph{with default}):
 If \code{TRUE} (the default) various parameters provided by the user
@@ -247,7 +249,7 @@ This function has BETA status, in particular for the GOK implementation. Please 
 your results carefully.
 }
 \section{Function version}{
- 0.4.7
+ 0.4.8
 }
 
 \examples{

man/calc_MinDose.Rd
@@ -14,7 +14,7 @@ calc_MinDose(
   level = 0.95,
   log.output = FALSE,
   plot = TRUE,
-  multicore = FALSE,
+  cores = 1,
   ...
 )
 }
@@ -62,11 +62,11 @@ values of the final parameter estimates and confidence intervals.}
 \item{plot}{\link{logical} (\emph{with default}):
 enable/disable the plot output.}
 
-\item{multicore}{\link{logical} (\emph{with default}):
-parallelize the computation of the bootstrap by creating a multicore cluster
-(only considered if \code{bootstrap = TRUE}). By default, it uses all available
-logical CPU cores, but this can be changed with the \code{cores} argument. Note
-that this feature is experimental and may not work on all systems.}
+\item{cores}{\link{integer}, \link{numeric} (\emph{with default}):
+number of cores allocated for parallel processing of the bootstrap step
+(only considered if \code{bootstrap = TRUE}). The default value corresponds to
+single-threaded computation; the recommended value is \code{NULL}, which assigns
+all but two of the available logical CPU cores.}
 
 \item{...}{(\emph{optional}) further arguments for bootstrapping
 (\code{bs.M}, \code{bs.N}, \code{bs.h}, \code{sigmab.sd}). See details for their usage.
@@ -74,7 +74,6 @@ Further arguments are
 \itemize{
 \item \code{verbose}: enable/disable output to the terminal
 \item \code{debug}: enable/disable extended console output
-\item \code{cores}: number of cores to be used when \code{multicore=TRUE}
 }}
 }
 \value{
@@ -161,12 +160,11 @@ with \code{bs.h}. By default, this is calculated as:
 
 \strong{Multicore support}
 
-Parallel processing can be enabled by setting \code{multicore = TRUE}. By default,
-the number of logical CPU cores is detected automatically, but it can be
-overridden with the \code{cores} argument. Multicore processing is supported
+Parallel processing can be enabled by setting the \code{cores} argument to a
+value greater than 1 (or to \code{NULL} to use all but two of the available
+logical CPU cores). Multicore processing is supported
 only when \code{bootstrap = TRUE}, and spawns one R process per core to compute
-MAM estimates for each of the \code{N * M} bootstrap replicates. Note that this
-feature is experimental and may not work on all systems. Performance gains
+MAM estimates for each of the \code{N * M} bootstrap replicates. Performance gains
 grow with the number of bootstrap replicates, but each additional core (R
 process) increases memory usage. When memory is insufficient, overall
 performance can degrade severely.
@@ -196,7 +194,7 @@ model with \code{debug=TRUE} which provides extended console output and
 forwards all internal warning messages.
 }
 \section{Function version}{
- 0.6.0
+ 0.6.1
 }
 
 \examples{

commit 0759fcb94f2968f4531cff4b9de2b49852b9d5c9

analyse_IRSAR.RF: Limit output about cores to the first iteration.
Fixes a regression from f3eb84c.

R/analyse_IRSAR.RF.R
@@ -509,6 +509,7 @@ analyse_IRSAR.RF<- function(
         plot_reduced = plot_reduced,
         cores = cores,
         main = temp_main[[x]],
+        .call.idx = x, # to suppress output about cores
         ...)
     })
 
@@ -717,6 +718,7 @@ analyse_IRSAR.RF<- function(
 
   ## control terminal output
   verbose <- extraArgs$verbose %||% TRUE
+  call.idx <- extraArgs$.call.idx %||% 1
 
   ## don't show the progress bar if not verbose
   if (!verbose)
@@ -1121,7 +1123,7 @@ analyse_IRSAR.RF<- function(
 
       ##set parallel calculation if wanted
       cores <- .validate_cores(cores)
-      if (verbose) {
+      if (verbose && call.idx == 1) {
         .throw_message("Using ", cores, ifelse(cores == 1, " core", " cores"),
                        " ...", error = FALSE)
       }