b9e02554cca664e24c8616b5c8b3790b7fead5ef
Marco Colombo <marco.colombo@uni-heidelberg.de>
2026-09-22 12:52:01 +0200

    spelling: Don't run spell check during coverage testing.
    This removes the following warning that appears on CI:
    
    Warning message:
     In spelling::spell_check_test(vignettes = TRUE, error = FALSE,
         skip_on_cran = TRUE) :
     Failed to find package source directory from:
       /tmp/RtmpYJmhKG/R_LIBS94e0f62c3ee01/Luminescence/Luminescence-tests
    
    [skip ci]

--- tests/spelling.R
+++ tests/spelling.R
@@ -1,4 +1,6 @@
-if (requireNamespace('spelling', quietly = TRUE))
+## run spelling during R CMD check but not during covr::pacakge_coverage()
+if (requireNamespace('spelling', quietly = TRUE) &&
+    file.exists('../00_pkg_src'))
   spelling::spell_check_test(
     vignettes = TRUE,
     error = FALSE,
